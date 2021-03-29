3b432c859c78fb02dddf57bb954292aab6666ccd
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-03-29 20:11:35 +0530

    DSA

---
+++ Python Problem Solving/10_Floyd's Triangle | Printing Numbers in Right Triangle Shape.py
@@ -0,0 +1,17 @@
+'''
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+'''
+
+n = int(input("type the number here: \n"))
+num = 1
+for row in range(1,n+1):
+    for col in range(1,row+1):
+        print(num, end=" ")
+        num = num +1
+    print()
+
