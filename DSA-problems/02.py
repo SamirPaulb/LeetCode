703b6a6b4d38311ee7b8b8d62b3dab5b4acf909c
Sagar Paul <paul.samir.2002@gmail.com>
2021-04-13 12:58:39 +0530

    ___

--- DSA-problems/02.py
+++ DSA-problems/02.py
@@ -1,18 +1,3 @@
-for i in range(1001):
-    num = i
-    result = 0
-    n = len(str(i))
-    while (i !=0):
-        digit = i%10
-        result = result + digit**n
-        i = i//10
-    if num == result:
-        print(result)
-    
-
-'''
-
-
 n = int(input("Enter upto which digit you want to find Angstrom number:  "))
 
 for i in range(1,n+1):
@@ -25,5 +10,5 @@ for i in range(1,n+1):
         print(i)
     
 
-'''
+
 
