db145fc9457161227492e0793cce81baf6f595e6
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-05-11 05:34:57 +0000

    Sync LeetCode submission - Count Primes (cpp)

---
+++ problems/count_primes/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
+
+        vector<int> is_prime(n / 2, true);
+        auto cnt = is_prime.size();
+        for (int i = 3; i * i < n; i += 2) {
+            if (!is_prime[i / 2]) {
+                continue;
+            }
+            for (int j = i * i; j < n; j += 2 * i) {
+                if (!is_prime[j / 2]) {
+                    continue;
+                }
+                --cnt;
+                is_prime[j / 2] = false;
+            }
+        }
+        return cnt;
+    }
+};
