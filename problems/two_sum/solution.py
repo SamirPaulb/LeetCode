ce83d8fc0272810a0bffaf289d2387064d08435d
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-05-22 20:53:51 +0000

    Sync LeetCode submission - Two Sum (python3)

---
+++ problems/two_sum/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
