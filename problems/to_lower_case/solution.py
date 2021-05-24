2590a63347d6b1d427a75f10ff2ccf0badf36401
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-05-24 14:32:22 +0000

    Sync LeetCode submission - To Lower Case (python3)

--- problems/to_lower_case/solution.py
+++ problems/to_lower_case/solution.py
@@ -1,5 +1,3 @@
 class Solution:
-    def toLowerCase(self, str: str) -> str:
-        def LowerC(ST):
-            return ST.lower()
-        return LowerC(str)
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
