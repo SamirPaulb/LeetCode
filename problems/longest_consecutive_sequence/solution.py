bc2186cbd1400a3b7281da630dd26ea7e8b49202
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-06-06 08:14:03 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (python3)

---
+++ problems/longest_consecutive_sequence/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestConsecutive(self, nums):
+        if not nums:
+            return 0
+
+        nums.sort()
+
+        longest_streak = 1
+        current_streak = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                if nums[i] == nums[i-1]+1:
+                    current_streak += 1
+                else:
+                    longest_streak = max(longest_streak, current_streak)
+                    current_streak = 1
+
+        return max(longest_streak, current_streak)
