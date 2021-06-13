ea6549ff4801e10ce87da7a9a5a43dca379a163f
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-06-13 17:54:06 +0530

    uodate

--- BST/01.py
+++ BST/01.py
@@ -5,26 +5,46 @@ class BST:
         self.rchild = None
     def insert(self,data):
         if self.key is None:
-            self.key = BST(data)
+            self.key = data
             return
         if self.key == data:
             return
-        if data < self.key:
-            if self.lchild:
-                self.lchild.insert(data)
-            else:
-                self.lchild = BST(data)
-            return 
-        else:
+        elif self.key < data:
             if self.rchild:
                 self.rchild.insert(data)
             else:
                 self.rchild = BST(data)
+            return
+        elif self.key > data:
+            if self.lchild:
+                self.lchild.insert(data)
+            else:
+                self.lchild = BST(data)
+            return
+    def search(self,data):
+        if self.key == data:
+            print("Node is Found")
+            return
+        if self.key < data:
+            if self.rchild:
+                self.rchild.search(data)
+            else:
+                print("Node is NOT found")
+            return
+        if self.key > data:
+            if self.lchild:
+                delf.lchild.search(data)
+            else:
+                print("Node is NOT found")
 
+
+        
 root = BST(10)
-ll = [2,4,53,5,3,2,4,434,232,42,242,23]
+ll = [1,2,33,23,12,33,12,12,31,12,222,333,456,6667,77777,51]
 for i in ll:
     root.insert(i)
+root.search(222)
+
     
 
         
