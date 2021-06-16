390562a35259c060cada9e7eae367f0da365f00e
Sagar Paul <paul.samir.2002@gmail.com>
2021-06-16 20:23:49 +0530

    BST

--- BST/01.py
+++ BST/01.py
@@ -3,6 +3,7 @@ class BST:
         self.key = key
         self.lchild = None
         self.rchild = None
+        
     def insert(self,data):
         if self.key is None:
             self.key = data
@@ -21,6 +22,7 @@ class BST:
             else:
                 self.lchild = BST(data)
             return
+
     def search(self,data):
         if self.key == data:
             print("Node is Found")
@@ -36,16 +38,85 @@ class BST:
                 delf.lchild.search(data)
             else:
                 print("Node is NOT found")
+    def preorder(self):
+        print(self.key, end=", ")
+        if self.lchild:
+            self.lchild.preorder()
+        if self.rchild:
+            self.rchild.preorder()
+    
+    def inorder(self):
+        if self.lchild:
+            self.lchild.inorder()
+        print(self.key, end=", ")
+        if self.rchild:
+            self.rchild.inorder()
+    
+    def postorder(self):
+        if self.lchild:
+            self.lchild.postorder()
+        if self.rchild:
+            self.rchild.postorder()
+        print(self.key, end=", ")
 
+    def delete(self,data):
+        if self.key is None:
+            print("BST is empty")
+        if data<self.key:
+            self.lchild = self.lchild.delete()
 
-        
-root = BST(10)
-ll = [1,2,33,23,12,33,12,12,31,12,222,333,456,6667,77777,51]
-for i in ll:
-    root.insert(i)
-root.search(222)
-
+    def delete(self,data):
+        if self.key is None:
+            print("Tree is Empty!")   
+        elif data > self.key:
+            if self.rchild:
+                self.rchild  = self.rchild.delete(data)
+            else:
+                print("Node is Not Present ")
+        elif data < self.key:
+            if self.lchild:
+                self.lchild = self.lchild.delete(data)
+            else:
+                print("Node is not present")
+        else:
+            if self.lchild is None:
+                temp = self.rchild
+                self = None
+                return temp
+            if self.rchild is None:
+                temp = self.lchild
+                self = None
+                return temp
+            node = self.rchild
+            while node.lchild:
+                node = node.lchild
+                self.key = node.key
+                self.rchild = self.rchild.delete(node.key)
+            return self
     
 
+
+
+    
+      
+root = BST(10)
+ll = [1,2,33,23,12,33,12,456,66, 51]
+for i in ll:
+    root.insert(i)
+print("Preorder")
+root.preorder()
+'''
+print()
+print("Inorder")
+root.inorder()
+
+print()
+print("Postorder")
+root.postorder()
+
+'''
+print("----------------------")
+root.delete(51)
+root.preorder()
         
 
