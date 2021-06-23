47594319ca720e27ee940b9e0c4298d613e6b85b
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-06-23 12:35:51 +0000

    Sync LeetCode submission - Reverse Linked List II (python3)

---
+++ problems/reverse_linked_list_ii/solution.py
@@ -0,0 +1,48 @@
+class Solution:
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+
+        if not head:
+            return None
+
+        left, right = head, head
+        stop = False
+        def recurseAndReverse(right, m, n):
+            nonlocal left, stop
+
+            # base case. Don't proceed any further
+            if n == 1:
+                return
+
+            # Keep moving the right pointer one step forward until (n == 1)
+            right = right.next
+
+            # Keep moving left pointer to the right until we reach the proper node
+            # from where the reversal is to start.
+            if m > 1:
+                left = left.next
+
+            # Recurse with m and n reduced.
+            recurseAndReverse(right, m - 1, n - 1)
+
+            # In case both the pointers cross each other or become equal, we
+            # stop i.e. don't swap data any further. We are done reversing at this
+            # point.
+            if left == right or right.next == left:
+                stop = True
+
+            # Until the boolean stop is false, swap data between the two pointers     
+            if not stop:
+                left.val, right.val = right.val, left.val
+
+                # Move left one step to the right.
+                # The right pointer moves one step back via backtracking.
+                left = left.next           
+
+        recurseAndReverse(right, m, n)
+        return head
