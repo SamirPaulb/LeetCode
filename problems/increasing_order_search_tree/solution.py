9ff45700575ac81371ce93deead94a1950f3627a
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-06-23 05:36:33 +0000

    Sync LeetCode submission - Increasing Order Search Tree (python3)

---
+++ problems/increasing_order_search_tree/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        def inorder(root):
+            if root == None:
+                return
+            inorder(root.left)
+            root.left = None
+            self.cur.right = root
+            self.cur = root
+            inorder(root.right)
+        ans = self.cur = TreeNode(None)
+        inorder(root)
+        return ans.right
