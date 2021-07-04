95856831f68d25b401877414678688fee1276397
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-07-04 20:11:42 +0000

    Sync LeetCode submission - Same Tree (java)

---
+++ problems/same_tree/solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        else if((p != null && q == null)
+                || (p == null && q != null)
+                || (p.val != q.val)) return false;
+        return isSameTree(p.left, q.left)
+            && isSameTree(p.right, q.right);
+    }
+}
