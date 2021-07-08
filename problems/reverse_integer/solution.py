f4d274501694a68198ccbc3dc01a7f8a8d01f86e
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-07-08 17:29:57 +0000

    Sync LeetCode submission - Reverse Integer (python3)

--- problems/reverse_integer/solution.py
+++ problems/reverse_integer/solution.py
@@ -1,20 +1,18 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        
-        s = 0
-        n = abs(x)
-        while n != 0:
-            s = s*10 + n%10
-            n = n//10
-
-        if x<0:
-            s = -(s)
-        if(s<(-2**31) or s>(2**31-1)):
-            return 0
-        return s
-       
-        
+        if x>=0:
+            s = str(x)
+            s = s[::-1]
+            a = int(s)
+            if a > 2**31:
+                return 0
+            return a
+        else:
+            x = (-1)*x
+            s = str(x)
+            s = s[::-1]
+            a = (-1)*int(s)
+            if a < -2**31 +1:
+                return 0
+            return a
             
-        
-        
-        # 123
