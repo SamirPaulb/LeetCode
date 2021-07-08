f6fd0349fa39bd8338cbf24ab88bb00595b7d6f6
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-07-08 17:09:10 +0000

    Sync LeetCode submission - Rotate Image (python3)

--- problems/rotate_image/solution.py
+++ problems/rotate_image/solution.py
@@ -3,18 +3,16 @@ class Solution:
         n = len(matrix[0])
         for i in range(n):
             for j in range(i, n):
-                matrix[i][j],matrix[j][i] = matrix[j][i], matrix[i][j]
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
         for i in range(n):
             a = 0
-            b = n-1
-            while a< b:
-                matrix[i][a], matrix[i][b] = matrix[i][b], matrix[i][a]
+            b = n - 1
+            while a < b:
+                matrix[i][a], matrix[i][b] =  matrix[i][b], matrix[i][a]
                 a += 1
                 b -= 1
                 
+        return matrix
                 
                 
-        
-                
-                
-        
