178fbfadfeb6c3d8fe57c83c4c4d084d40ecb1c5
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-07-09 13:56:31 +0000

    Sync LeetCode submission - Implement Queue using Stacks (python3)

---
+++ problems/implement_queue_using_stacks/solution.py
@@ -0,0 +1,39 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.queue.pop(0)
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return len(self.queue) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
