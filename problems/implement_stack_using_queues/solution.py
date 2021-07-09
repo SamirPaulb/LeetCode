1996b7c0ba7dea90e456d1ed458d91529aee7090
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-07-09 13:02:01 +0000

    Sync LeetCode submission - Implement Stack using Queues (python3)

---
+++ problems/implement_stack_using_queues/solution.py
@@ -0,0 +1,39 @@
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.stack.pop()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.stack[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return len(self.stack) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
