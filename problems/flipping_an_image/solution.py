8930466896fbb2f752546b331a351ec80bfc06b8
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-07-13 11:51:08 +0000

    Sync LeetCode submission - Flipping an Image (python3)

--- problems/flipping_an_image/solution.py
+++ problems/flipping_an_image/solution.py
@@ -1,14 +1,11 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        for  i in range(len(image)):
-            image[i].reverse()
-        for  i in range(len(image)):
+        for i in range(len(image)):
+            image[i] = image[i][::-1]
+        for i in range(len(image)):
             for j in range(len(image[i])):
                 if image[i][j] == 0:
                     image[i][j] = 1
                 elif image[i][j] == 1:
                     image[i][j] = 0
         return image
-                    
-            
-            
