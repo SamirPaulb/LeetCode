e0b270a24f6c0ba0194e160fbfc2a2e98674d33b
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-07-16 05:21:53 +0000

    Sync LeetCode submission - Move Zeroes (python3)

--- problems/move_zeroes/solution.py
+++ problems/move_zeroes/solution.py
@@ -1,12 +1,14 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         i = 0
-        n = len(nums)
+        n = len(nums) - 1
         while i < n:
             if nums[i] == 0:
-                nums.remove(nums[i])
-                nums.append(0)
+                a = nums[i]
+                nums.remove(a)
+                nums.append(a)
                 n -= 1
             else:
                 i += 1
+        return nums
         
