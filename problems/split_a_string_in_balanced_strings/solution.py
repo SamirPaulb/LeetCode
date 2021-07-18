e3c0b3496800140ebe2bce310230703d63885b6b
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-07-18 15:27:56 +0000

    Sync LeetCode submission - Split a String in Balanced Strings (python3)

--- problems/split_a_string_in_balanced_strings/solution.py
+++ problems/split_a_string_in_balanced_strings/solution.py
@@ -1,31 +1,24 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        
-        a = 0
+        ans = 0
+        s = list(s)
         stack = []
-        
-        stack.append(s[0])
-        i = 1
-        while i < len(s):
+        while len(s) != 0:
+            if len(stack) == 0:
+                stack.append(s[0])
+                s.pop(0)
+            elif stack[-1] =="L" and s[0] == "R":
+                stack.pop(-1)
+                s.pop(0)
+                if len(stack) == 0:
+                    ans += 1
+            elif stack[-1] == "R" and s[0] == "L":
+                stack.pop(-1)
+                s.pop(0)
+                if len(stack) == 0:
+                    ans += 1
+            else:
+                stack.append(s[0])
+                s.pop(0)
+        return ans
             
-            
-            if stack[len(stack)-1] != s[i]:
-                stack.pop()
-            
-            if len(stack) !=0:    
-                if stack[len(stack)-1] == s[i]:
-                    stack.append(s[i])
-
-                
-            if len(stack) ==0:
-                a = a+1
-                i = i+1
-                if i<len(s):
-                    stack.append(s[i])
-            i = i+1
-            
-        return a
-                
-                
-            
-        
