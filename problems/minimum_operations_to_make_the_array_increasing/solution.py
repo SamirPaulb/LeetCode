2c97957b55798a4d59227e0824e12384056583d8
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-07-19 08:38:22 +0000

    Sync LeetCode submission - Minimum Operations to Make the Array Increasing (python3)

--- problems/minimum_operations_to_make_the_array_increasing/solution.py
+++ problems/minimum_operations_to_make_the_array_increasing/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        s = 0
+        ans =0
         for i in range(len(nums)-1):
-            if nums[i] >= nums[i+1]:
-                a = nums[i+1]
-                nums[i+1] +=  nums[i] - nums[i+1] + 1
-                s = s + (nums[i+1] -a)
-            
-        return s
-            
-        
+            if nums[i] < nums[i+1]:
+                continue
+            else:
+                diff = nums[i] - nums[i+1] + 1
+                nums[i+1] += diff
+                ans += diff 
+        return ans
