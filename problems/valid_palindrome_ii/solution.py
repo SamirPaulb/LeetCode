3c07119de75cf4596c4ec1e94602c0c9e886a4a3
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-07-20 05:25:17 +0000

    Sync LeetCode submission - Valid Palindrome II (python3)

--- problems/valid_palindrome_ii/solution.py
+++ problems/valid_palindrome_ii/solution.py
@@ -1,20 +1,19 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def helper(s,i,j):
-            while i <j:
+        def varify(s,i,j):
+            while i < j:
                 if s[i] == s[j]:
                     i += 1
                     j -= 1
                 else:
                     return False
             return True
-
         i = 0
-        j = len(s)-1
-        while i<j:
+        j = len(s) - 1
+        while i < j:
             if s[i] == s[j]:
-                i +=1 
-                j -=1
+                i += 1
+                j -= 1
             else:
-                return helper(s,i+1, j) or helper(s, i, j-1)
+                return varify(s,i+1,j) or varify(s,i,j-1)
         return True
