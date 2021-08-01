ab95083928b4c09f2d5a95860016f9682efe474c
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-08-01 15:24:14 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (python3)

--- problems/best_time_to_buy_and_sell_stock_ii/solution.py
+++ problems/best_time_to_buy_and_sell_stock_ii/solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
+        ans = 0
         for i in range(len(prices)-1):
             if prices[i] < prices[i+1]:
-                profit += prices[i+1] - prices[i]
-        return profit
-    
+                ans += prices[i+1] - prices[i]
+        return ans
