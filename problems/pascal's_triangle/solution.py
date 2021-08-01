fbb6d81b3638d929908aa6eb1bad0a16953fa409
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-08-01 15:03:13 +0000

    Sync LeetCode submission - Pascal's Triangle (python3)

--- problems/pascal's_triangle/solution.py
+++ problems/pascal's_triangle/solution.py
@@ -1,17 +1,14 @@
 class Solution:
-    def generate(self, num_rows: int) -> List[List[int]]:
-        triangle = []
+    def generate(self, n: int) -> List[List[int]]:
+        a = []
+        for i in range(n):
+            a.append([0 for j in range(i+1)])
+        
+        for i in range(n):
+            a[i][0], a[i][-1] = 1, 1
 
-        for row_num in range(num_rows):
-            # The first and last row elements are always 1.
-            row = [None for _ in range(row_num + 1)]
-            row[0], row[-1] = 1, 1
-
-            # Each triangle element is equal to the sum of the elements
-            # above-and-to-the-left and above-and-to-the-right.
-            for j in range(1, len(row) - 1):
-                row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
-
-            triangle.append(row)
-
-        return triangle
+        for i in range(2,n):
+            for j in range(1,i):
+                a[i][j] = a[i-1][j-1] + a[i-1][j]
+                
+        return a
