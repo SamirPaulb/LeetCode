5c66bc816c5b9ccb4033eac12b8b22320a840cfb
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-08-01 12:38:58 +0000

    Sync LeetCode submission - Fibonacci Number (python3)

---
+++ problems/fibonacci_number/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.fib(n-1) + self.fib(n-2)
