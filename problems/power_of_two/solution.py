b68e4c8f16d7e8140770d627189f014cae176e55
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-08-12 06:09:54 +0000

    Sync LeetCode submission - Power of Two (python3)

--- problems/power_of_two/solution.py
+++ problems/power_of_two/solution.py
@@ -1,6 +1,12 @@
+from math import log
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n <= 0:
-            return False 
-            
-        return (n & (n-1)) == 0
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # Since error exist, can't use float.is_integer() to check
+        # So I choose to check it back
+        if n <= 0: return False
+        res = round(log(n, 2))
+        return 2**res == n
