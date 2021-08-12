030a647736bd33ffa8db73789637b27dd9f485e7
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-08-12 11:49:30 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

--- problems/merge_two_sorted_lists/solution.py
+++ problems/merge_two_sorted_lists/solution.py
@@ -4,21 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        s = ListNode(-1)
-        temp = s
-        while (l1 != None) and (l2 != None) :
-            if l1.val<= l2.val:
-                temp.next = l1
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        l = ListNode(-1)
+        ans = l
+        while l1 != None or l2 != None:
+            if l1 != None and l2 != None and l1.val <= l2.val:
+                l.next = l1
                 l1 = l1.next
-            elif l1.val >= l2.val:
-                temp.next = l2
+                l = l.next
+            elif l1 != None and l2 != None and l1.val > l2.val:
+                l.next = l2
                 l2 = l2.next
-            temp = temp.next
-        if l1 != None:
-            temp.next = l1
-        elif l2 != None:
-            temp.next = l2
-        
-        return s.next
-        
+                l = l.next
+            elif l1 == None:
+                l.next = l2
+                l2 = l2.next
+                l = l.next
+            elif l2 == None:
+                l.next = l1
+                l1 = l1.next
+                l = l.next
+        return ans.next
+            
