d440b8a7ef67dc23bd217762b31f7e7c612040b8
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-08-16 14:22:34 +0000

    Sync LeetCode submission - Clone Graph (python3)

---
+++ problems/clone_graph/solution.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        oldToNew = {}
+        
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+        
+        return dfs(node) if node else None
