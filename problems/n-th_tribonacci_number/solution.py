e82c19e4f67d7d461dfe06e8769b0821cf7258e6
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-08-22 05:43:45 +0000

    Sync LeetCode submission - N-th Tribonacci Number (python3)

--- problems/n-th_tribonacci_number/solution.py
+++ problems/n-th_tribonacci_number/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
         a = 0
         b = 1
         c = 1
         i = 3
         while i <= n:
-            tem = a + b + c
+            tmp = a + b + c
             a = b
-            b = c 
-            c = tem
+            b = c
+            c = tmp
             i += 1
-        if n == 0:
-            return 0
         return c
