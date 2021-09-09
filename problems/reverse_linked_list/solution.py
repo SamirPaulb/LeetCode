72d15d7fbf113c83d0b70f94d111cea662e37e04
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-09-09 13:51:34 +0000

    Sync LeetCode submission - Reverse Linked List (python3)

--- problems/reverse_linked_list/solution.py
+++ problems/reverse_linked_list/solution.py
@@ -1,17 +1,23 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+'''
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        c = head
-        n = None
-        p = None
-        while c is not None:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        c = head # c = Current Node
+        p = None # p = Previous Node
+        n = None # n = Next Node of Current
+        while c != None:
             n = c.next
             c.next = p
             p = c
             c = n
         return p
-    
+'''
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverseLinkedList(c, p, n):
+            if c == None: return p
+            n = c.next
+            c.next = p
+            p = c
+            c = n
+            return reverseLinkedList(c, p, n)
+        return reverseLinkedList(head, None, None)
