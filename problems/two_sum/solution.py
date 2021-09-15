400d5b5ae6bc4c5069efac9682b81267200693c6
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-09-15 06:52:54 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,6 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            leftover =  target - nums[i]
-            if (leftover in nums ) and (nums.index(leftover) != i):
-                return [i, nums.index(leftover)]
+        dic = {}
+        for i, ch in enumerate(nums):
+            n = target - ch
+            if n not in dic:
+                dic[ch] = i
+            else:
+                return [dic[n], i]
+        
