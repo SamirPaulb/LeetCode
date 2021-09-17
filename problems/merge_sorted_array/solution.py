129b4ce700b6bef98d8fa5d6ff4397a0c4821cbd
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-09-17 19:53:34 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

--- problems/merge_sorted_array/solution.py
+++ problems/merge_sorted_array/solution.py
@@ -1,6 +1,16 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         for i in range(len(nums2)):
-            nums1.remove(0)
-        nums1 += nums2
-        nums1.sort()
+            nums1.pop()
+        i = 0
+        while i < len(nums1):
+            if len(nums2) != 0 and nums1[i] > nums2[0]:
+                nums1.insert(i, nums2[0])
+                nums2.pop(0)
+            if len(nums2) != 0 and nums1[i] > nums2[0]:
+                i -= 1
+            i += 1
+        if nums2:
+            nums1 += nums2
+        
+# Time Complexity = O(n)
