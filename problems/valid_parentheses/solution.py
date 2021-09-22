1db5e78ecacaced22cb9d098d4dabc9203af620f
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-09-22 14:14:23 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- problems/valid_parentheses/solution.py
+++ problems/valid_parentheses/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        m1 = {"(" : 0, "{" : 1, "[" : 2}
-        m2 = {")" : 0, "}" : 1, "]" : 2}
         stack = []
-        for i in range(len(s)):
-            if ((len(stack) == 0 and s[i] in m2) 
-               or (s[i] in m2 and stack[-1] in m1 
-                   and m1[stack[-1]] != m2[s[i]])) :
+        s = list(s)
+        for i in s:
+            if len(stack) == 0 and (i == ")" or i == "]" or i == "}"):
                 return False
-            elif (s[i] in m2 and stack[-1] in m1 
-                   and m1[stack[-1]] == m2[s[i]]):
-                stack.pop(-1)
-            elif s[i] in m1:
-                stack.append(s[i])
-                
             
-        if len(stack) == 0:
-            return True
-        return False
+            elif len(stack) == 0:
+                stack.append(i)
+                
+            else:
+                if (stack[-1] == "(" and i == ")") or (stack[-1] == "{" and i == "}") or (stack[-1] == "[" and i == "]"):
+                    stack.pop()
+                else:
+                    stack.append(i)
+        print(stack)
+        return len(stack) == 0
+                
+                
