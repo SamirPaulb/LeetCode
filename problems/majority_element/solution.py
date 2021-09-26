b7a2a2353b1648eaca3c835ecb661119b283d6f2
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-09-26 13:58:08 +0000

    Sync LeetCode submission - Majority Element (python3)

--- problems/majority_element/solution.py
+++ problems/majority_element/solution.py
@@ -1,7 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        a = list(set(nums))
-        for i in a:
-            if nums.count(i) > len(nums)//2: 
-                print(i)
-                return i
+        me = -1   # me = Majority Element
+        count = 0      # count = Count of Majority Element
+        
+        for i in range(len(nums)):
+            if nums[i] == me:
+                count += 1
+            elif count > 0 and nums[i] != me:
+                count -= 1
+            
+            if count == 0:
+                me = nums[i]
+                count = 1
+        
+        print(me)
+        if nums.count(me) >= len(nums) // 2:
+            return me
+        return -1
