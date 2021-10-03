de89c43c795cf4b3a16e1ea8334c179eb33013a8
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-10-03 18:43:46 +0000

    Sync LeetCode submission - Maximum Product Subarray (python3)

--- problems/maximum_product_subarray/solution.py
+++ problems/maximum_product_subarray/solution.py
@@ -1,18 +1,18 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        cs = 1 # Current so far
-        ms = nums[0] # Masimum so far
+        cs = 1  # Current So Far
+        ms = nums[0] # Max So Far
         for i in nums:
             cs *= i
             ms = max(ms, cs)
             if cs == 0:
                 cs = 1
-        cs = 1
         
+        cs = 1
         for i in nums[::-1]:
             cs *= i
             ms = max(ms, cs)
             if cs == 0:
                 cs = 1
-               
+        
         return ms
