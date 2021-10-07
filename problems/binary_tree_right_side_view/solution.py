e20251944a6af8147f7b740c30f55aeeae19847f
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-10-07 03:56:48 +0000

    Sync LeetCode submission - Binary Tree Right Side View (python3)

--- problems/binary_tree_right_side_view/solution.py
+++ problems/binary_tree_right_side_view/solution.py
@@ -3,8 +3,10 @@ class Solution:
         # Implement Level Order Traversal and append right node of each level to the answer
         final_ans = []
         q = []
+        
+        if not root: return q
+        
         q.append(root)
-        if not root: return
         while q:
             tmp_ans = []
             for i in range(len(q)):
@@ -13,4 +15,5 @@ class Solution:
                 if node.left: q.append(node.left)
                 if node.right: q.append(node.right)
             final_ans.append(tmp_ans[-1])
+        
         return final_ans
