48fbc584fa8a9f4d30bf4fc77c0669ea9bf7e633
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-10-09 16:23:19 +0000

    Sync LeetCode submission - Construct Binary Tree from Preorder and Inorder Traversal (python3)

--- problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.py
+++ problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.py
@@ -1,30 +1,23 @@
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-
-        def array_to_tree(left, right):
-            nonlocal preorder_index
-            # if there are no elements to construct the tree
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        self.preorder_index = 0  # nonelocal variable => self.preorder_index = nonlocal preorder_index
+        def constractTree(left, right):
             if left > right: return None
-
-            # select the preorder_index element as the root and increment it
-            root_value = preorder[preorder_index]
-            root = TreeNode(root_value)
-
-
-            preorder_index += 1
-
-            # build left and right subtree
-            # excluding inorder_index_map[root_value] element because it's the root
-            root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
-            root.right = array_to_tree(inorder_index_map[root_value] + 1, right)
-
+            
+            rootValue = preorder[self.preorder_index]
+            root = TreeNode(rootValue)
+            
+            self.preorder_index += 1
+            
+            if left == right: return root
+            
+            root.left = constractTree(left, inorder_index[rootValue] - 1)
+            root.right = constractTree(inorder_index[rootValue] + 1, right)
+            
             return root
-
-        preorder_index = 0
-
-        # build a hashmap to store value -> its index relations
-        inorder_index_map = {}
-        for index, value in enumerate(inorder):
-            inorder_index_map[value] = index
-
-        return array_to_tree(0, len(preorder) - 1)
+        
+        inorder_index = {}
+        for i, element in enumerate(inorder):
+            inorder_index[element] = i
+        
+        return constractTree(0, len(preorder) - 1)
