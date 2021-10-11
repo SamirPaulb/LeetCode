63a186ae4d19c6c88c274db5fd49852282ca9e17
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-10-11 16:36:11 +0000

    Sync LeetCode submission - Subarray Sum Equals K (python3)

--- problems/subarray_sum_equals_k/solution.py
+++ problems/subarray_sum_equals_k/solution.py
@@ -1,24 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         count = 0
-        hashMap = {}
+        hashMap = {0:1}
         curSum = 0
         
-        if len(nums) == 0:
-            return 0
-        
-        hashMap[0] = 1
-        
-        for i in range(len(nums)):
-            curSum += nums[i]
-                
+        for i in nums:
+            curSum += i
             if (curSum - k) in hashMap:
                 count += hashMap[curSum - k]
-                
             if curSum in hashMap:
                 hashMap[curSum] += 1
             else:
                 hashMap[curSum] = 1
         
         return count
-                
