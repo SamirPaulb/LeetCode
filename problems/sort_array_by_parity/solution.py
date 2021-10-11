997a54e572e681239aefd1b6d4fa3882524e63c3
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-10-11 16:02:57 +0000

    Sync LeetCode submission - Sort Array By Parity (python3)

--- problems/sort_array_by_parity/solution.py
+++ problems/sort_array_by_parity/solution.py
@@ -1,5 +1,13 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        ans = [i for i in nums if i%2 == 0] + [i for i in nums if i %2!= 0]
-        return ans
+        i = 0
+        arr = []
+        while i < len(nums):
+            if nums[i] % 2 != 0:
+                arr.append(nums[i])
+                nums.pop(i)
+            else: i += 1
         
+        nums += arr
+        
+        return nums
