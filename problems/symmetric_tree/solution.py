a7fd9defec97fb84a817aa4827c4f298b8277597
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-03 18:20:50 +0000

    Sync LeetCode submission - Symmetric Tree (python3)

--- problems/symmetric_tree/solution.py
+++ problems/symmetric_tree/solution.py
@@ -1,16 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def check(l, r):
-            if l == None and r == None: return True
-            elif l != None and r == None: return False
-            elif l == None and r != None: return False
-            elif l.val != r.val: return False
-            return check(l.left, r.right) and check(l.right, r.left)
-        return check(root.left, root.right)
-    
+        if not root: return True
+        p, q = root.left, root.right
+        
+        def check(p, q):
+            if not p and not q: return True
+            elif (not p and q) or (p and not q) or (p.val != q.val): return False
+            return check(p.left, q.right) and check(p.right, q.left)
+        
+        return check(p, q)
