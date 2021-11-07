dae77378aa9dc0d5f622827ab838795c45cabd2e
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-11-07 15:49:24 +0000

    Sync LeetCode submission - Construct Binary Search Tree from Preorder Traversal (python3)

--- problems/construct_binary_search_tree_from_preorder_traversal/solution.py
+++ problems/construct_binary_search_tree_from_preorder_traversal/solution.py
@@ -8,15 +8,16 @@ class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
         root = TreeNode(preorder[0])
         stack = [root]
+        
         for i in preorder[1:]:
             newNode = TreeNode(i)
             if newNode.val < stack[-1].val:
                 stack[-1].left = newNode
             else:
-                while stack and newNode.val > stack[-1].val:
+                while stack and stack[-1].val < newNode.val:
                     parent = stack.pop()
                 parent.right = newNode
             stack.append(newNode)
+        
         return root
-    
-    
+                
