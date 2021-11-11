22ee1eba0b43ee8bec505f39f6e59c40159b3c29
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-11-11 14:44:08 +0000

    Sync LeetCode submission - Path Sum (python3)

--- problems/path_sum/solution.py
+++ problems/path_sum/solution.py
@@ -1,23 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
-        ans = []
-        def path(root,s):
-            if not root:
-                return
-            if root.left == None and root.right == None:
-                ans.append(s + root.val)
-            if root.left:
-                path(root.left, s = s + root.val)
-            if root.right:
-                path(root.right, s =  s + root.val)
-        path(root, 0)
-        for i in ans:
-            if i == targetSum:
-                return True
-        return False
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        arr = []
+        def solve(root, s): 
+            if not root: return s
+            if not root.left and not root.right:
+                s += root.val
+                arr.append(s)
+            solve(root.left, s + root.val)
+            solve(root.right, s + root.val)
+        
+        solve(root, 0)
+        return targetSum in arr
+            
