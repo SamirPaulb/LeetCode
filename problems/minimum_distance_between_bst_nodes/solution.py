ae930e6963aacfcc0515baa344e9e774a81b7375
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-11-13 08:19:29 +0000

    Sync LeetCode submission - Minimum Distance Between BST Nodes (python3)

--- problems/minimum_distance_between_bst_nodes/solution.py
+++ problems/minimum_distance_between_bst_nodes/solution.py
@@ -1,24 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def __init__(self):
-        self.arr = []
-    def minDiffInBST(self, root: TreeNode) -> int:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        self.ans, self.pre = float("inf"), float("inf")
         def inorder(root):
-            if root.left:
-                inorder(root.left)
-            self.arr.append(root.val)
-            if root.right:
-                inorder(root.right)
+            if not root: return
+            inorder(root.left)
+            if self.pre == float("inf"): self.pre = root.val
+            else:
+                self.ans = min(self.ans, root.val - self.pre)
+                self.pre = root.val
+            inorder(root.right)
+        
         inorder(root)
-        s = abs(self.arr[1] - self.arr[0])
-        for i in range(1,len(self.arr)-1):
-            if s > abs(self.arr[i] - self.arr[i+1]):
-                s = abs(self.arr[i] - self.arr[i+1])
-        return s
-        
-        
+        return self.ans
