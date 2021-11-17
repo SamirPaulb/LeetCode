3b0d8dd1c166e4fccb6ae7e84bb2f4bb36dc7440
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-17 05:23:24 +0000

    Sync LeetCode submission - 3Sum (python3)

--- problems/3sum/solution.py
+++ problems/3sum/solution.py
@@ -3,16 +3,16 @@ class Solution:
         nums.sort()
         res = []
         for i, ch in enumerate(nums):
-            if i > 0 and nums[i] == nums[i-1]: continue
-            l = i + 1
-            r = len(nums)-1
-            while l < r:  
+            if i > 0 and nums[i] == nums[i - 1]: continue # As nums is sorted if 2 consecutive elements are same and the next element is also greater so sum of these three can not be = 0
+            l, r = i + 1, len(nums) - 1
+            while l < r:
                 threeSum = ch + nums[l] + nums[r]
                 if threeSum > 0: r -= 1
                 elif threeSum < 0: l += 1
                 else:
                     res.append([ch, nums[l], nums[r]])
                     l += 1
-                    while nums[l] == nums[l-1] and l < r:
+                    while nums[l] == nums[l - 1] and l < r: # nums = [0,0,0,0]
                         l += 1
+                        
         return res
