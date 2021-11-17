7a8d4d40650c2ed03d335122dbb7f30df8fd32f0
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-17 07:13:09 +0000

    Sync LeetCode submission - 4Sum (python3)

--- problems/4sum/solution.py
+++ problems/4sum/solution.py
@@ -2,19 +2,21 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         res = []
-        for i in range(len(nums)-3):
-            if i > 0 and nums[i] == nums[i-1]: continue
-            # Now apply 3 Sum problem approach
-            for j in range(i+1, len(nums)-2):
-                if j > i+1 and nums[j] == nums[j-1]: continue
-                l, r = j+1, len(nums)-1
+        for i in range(len(nums) - 3):
+            if i > 0 and nums[i] == nums[i - 1]: continue
+            tar = target - nums[i]
+            # 3 Sum approach
+            for j in range(i + 1, len(nums) - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]: continue
+                l, r = j + 1, len(nums) - 1
                 while l < r:
-                    s = nums[i] + nums[j] + nums[l] + nums[r]
-                    if s < target: l += 1
-                    elif s > target: r -= 1
+                    threeSum = nums[j] + nums[l] + nums[r]
+                    if threeSum < tar: l += 1
+                    elif threeSum > tar: r -= 1
                     else:
                         res.append([nums[i], nums[j], nums[l], nums[r]])
                         l += 1
-                        while l < r and nums[l] == nums[l-1]:
-                            l += 1
+                        while nums[l] == nums[l - 1] and l < r: l += 1
+                            
         return res
+    
