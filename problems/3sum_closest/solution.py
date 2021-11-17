53b951ca1ba0422500e513236c8b3a4da2f43fc9
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-11-17 06:23:03 +0000

    Sync LeetCode submission - 3Sum Closest (python3)

--- problems/3sum_closest/solution.py
+++ problems/3sum_closest/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        diff = 2**31
-        ans = 0
         nums.sort()
+        ans = None
+        prevDiff = float("inf")
         for i, ch in enumerate(nums):
-            if i > 0 and nums[i] == nums[i-1]: continue
+            if i > 0 and nums[i] == nums[i - 1]: continue
             l = i + 1
-            r = len(nums)-1
+            r = len(nums) - 1
             while l < r:
                 threeSum = ch + nums[l] + nums[r]
-                if diff > abs(target - threeSum):
-                    diff = abs(target - threeSum)
+                if abs(target - threeSum) < prevDiff: 
+                    prevDiff = abs(target - threeSum)
                     ans = threeSum
-                if target < threeSum: r -= 1
-                elif target > threeSum: l += 1
-                else:
-                    return threeSum
                     
+                if threeSum < target: l += 1
+                elif threeSum > target: r -= 1
+                else: return threeSum
+                        
         return ans
