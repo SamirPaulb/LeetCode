4be1920ea7b2f0e0cf270f5d9d15065d8025a0b9
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-11-18 15:46:33 +0000

    Sync LeetCode submission - Longest Valid Parentheses (python3)

--- problems/longest_valid_parentheses/solution.py
+++ problems/longest_valid_parentheses/solution.py
@@ -1,23 +1,17 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        o = c = 0   # o = count of Open Parenthisis "(" , # c = count of close parenthisis ")"
-        ans = 0
-        for i in range(len(s)):
-            if s[i] == "(": o += 1
+        o, c, ans = 0, 0, 0
+        for i in s:
+            if i == "(": o += 1
             else: c += 1
+            if o == c: ans = max(ans, o + c)
+            if c > o: o = c = 0
             
-            if o == c: ans = max(ans, o+c)
-            elif c > o:  # while thaversing from start to end if count of closing parenthesis greter than opening parenthesis => NO valid parenthesis pair exist in that scope.
-                o = c = 0
-        
-        
         o = c = 0
-        for i in range(len(s)-1, -1, -1):
+        for  i in range(len(s)-1, -1, -1):
             if s[i] == "(": o += 1
             else: c += 1
-            
-            if o == c: ans = max(ans, o+c)
-            elif o > c:   # While traversing from end to start if count of opening parenthesis greter than count of closing parenthesis => NO valid parenthesis pair exist in that scope.
-                o = c = 0
+            if o == c: ans = max(ans, o + c)
+            if o > c: o = c = 0
         
         return ans
