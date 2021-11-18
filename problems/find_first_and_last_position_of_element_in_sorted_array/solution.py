dcc99f10933b35bcda2ea7ec936dcc0ad1aefe61
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-18 17:41:33 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (python3)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
@@ -1,21 +1,26 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if len(nums) == 0: return [-1,-1]
-        arr = []
-        def binary_search(nums, high, low, target):
-            if high >= low:
-                mid = (high + low) // 2
-                if target == nums[mid]:
-                    arr.append(mid)
-                if nums[mid] > target or ((mid-1)>=0 and nums[mid-1] == target):
-                    binary_search(nums, mid-1, low, target)
-                if nums[mid] < target or ((mid+1)<len(nums) and nums[mid+1] == target):
-                    binary_search(nums, high, mid+1, target)
-                
-            return arr
+        ans = [-1, -1]
+        start, end = 0, len(nums) - 1
+        a = None
+        while start <= end:
+            mid = (start + end) // 2 
+            if nums[mid] == target: 
+                a = mid
+                break
+            if target < nums[mid]: end = mid - 1
+            if target > nums[mid]: start = mid + 1
         
-        binary_search(nums, len(nums)-1, 0, target)
-        if len(arr) == 0: return [-1,-1]
+        if a == None: return ans
+        # To find the left limit fo target
+        i = a
+        while i >= 0 and nums[i] == target:
+            i -= 1
+        ans[0] = i + 1
+        # To find the right limit of target
+        i = a
+        while i < len(nums) and nums[i] == target:
+            i += 1
+        ans[1] = i - 1
         
-        return [min(arr), max(arr)]
-    
+        return ans
