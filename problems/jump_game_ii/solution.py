54774f3f0dc3a6c0af4eb743b481d9af8bdab2f6
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-20 16:47:09 +0000

    Sync LeetCode submission - Jump Game II (python3)

--- problems/jump_game_ii/solution.py
+++ problems/jump_game_ii/solution.py
@@ -1,14 +1,10 @@
-# https://www.youtube.com/watch?v=dJ7sWiOoK7g
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        l = r = 0
-        jumps = 0
-        
+        l, r = 0, 0   # left, right pointer for scaning l to r for maxReachable index
+        maxReachableIndex, jumps = 0, 0
         while r < len(nums) - 1:
-            maxReachableIndex = 0
-            for i in range(l, r+1):
+            for i in range(l, r + 1):
                 maxReachableIndex = max(maxReachableIndex, i + nums[i])
-            
             l = r + 1
             r = maxReachableIndex
             jumps += 1
