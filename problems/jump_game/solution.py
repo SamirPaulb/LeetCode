9d2930ca5d8f34190aac1bb47f9ea78b9138b76b
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-23 20:18:52 +0000

    Sync LeetCode submission - Jump Game (python3)

--- problems/jump_game/solution.py
+++ problems/jump_game/solution.py
@@ -1,10 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        i, reachable = 0, 0
-        while i <= reachable:
-            if reachable >= len(nums) - 1: return True
-            reachable = max(reachable, i + nums[i])
+        i, maxReachable = 0, 0
+        while i <= maxReachable:
+            if maxReachable >= len(nums) - 1: return True
+            maxReachable = max(maxReachable, i + nums[i])
             i += 1
         
         return False
-    
