0d4dd9e0298ed1f96d2d53933fad7b77604df53f
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-11-23 09:52:09 +0000

    Sync LeetCode submission - Generate Parentheses (python3)

--- problems/generate_parentheses/solution.py
+++ problems/generate_parentheses/solution.py
@@ -1,18 +1,19 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        stack = []
-        ans = []
+        stack, ans = [], []
+        openN, closeN = [], []
         def backtrack(openN, closeN):
             if openN == closeN == n:
                 ans.append("".join(stack))
                 return ans
             if openN < n:
                 stack.append("(")
-                backtrack(openN+1, closeN)
+                backtrack(openN + 1, closeN)
                 stack.pop()
-            if openN > closeN:
+            if closeN < openN:
                 stack.append(")")
-                backtrack(openN, closeN+1)
+                backtrack(openN, closeN + 1)
                 stack.pop()
+                
         backtrack(0, 0)
         return ans
