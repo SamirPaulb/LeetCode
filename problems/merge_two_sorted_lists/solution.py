ced43d858de1f2dabf4d4d1db73e21499098b8b3
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-24 19:04:24 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

--- problems/merge_two_sorted_lists/solution.py
+++ problems/merge_two_sorted_lists/solution.py
@@ -1,16 +1,14 @@
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         if not l1 or not l2: return l1 or l2
-        if l1.val <= l2.val: a, b = l1, l2
+        if l1.val < l2.val: a, b = l1, l2
         else: a, b = l2, l1
-        res = a
-        while a or b:
-            if not a.next: 
+        ans = a
+        while l1 or l2:
+            if not a.next or not b: 
                 a.next = b
-                return res
-            elif not b: return res
-            
-            if a.next.val <= b.val: 
+                return ans
+            elif a.next.val < b.val:
                 a = a.next
             else:
                 tmp = a.next
@@ -18,3 +16,5 @@ class Solution:
                 b = tmp
                 a = a.next
         
+        return ans
+            
