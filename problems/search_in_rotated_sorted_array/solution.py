5aa8247204a5f4cb5ef5e06c94982ecf7f6873ee
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-24 15:10:37 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (python3)

--- problems/search_in_rotated_sorted_array/solution.py
+++ problems/search_in_rotated_sorted_array/solution.py
@@ -4,15 +4,27 @@ class Solution:
         while start <= end:
             mid = (start + end) // 2
             if nums[mid] == target: return mid
-            if start == mid == end: return -1
-            if nums[start] <= nums[mid]:
+            if nums[start] <= nums[mid]: # Left side of Mid is SORTED
                 if nums[start] <= target < nums[mid]:
                     end = mid
                 else: start = mid + 1
-            else:
-                if nums[mid] <= target < nums[start]:
+            else: # Right side of Mid is SORTED
+                if nums[mid] <= target <= nums[end]:
                     start = mid + 1
-                else:
-                    end = mid
+                else: end = mid
         
         return -1
+    
+    
+    
+    '''
+[4,5,6,7,0,1,2]
+0
+[4,5,6,7,0,1,2]
+3
+[1]
+0
+[5,1,3]
+3
+    
+    '''
