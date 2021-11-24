fb528fe028f9106a3dcfad71b0d0bf150a36f76c
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-11-24 16:02:02 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (python3)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
@@ -1,26 +1,54 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         ans = [-1, -1]
-        start, end = 0, len(nums) - 1
-        a = None
-        while start <= end:
-            mid = (start + end) // 2 
-            if nums[mid] == target: 
-                a = mid
+        # Implementing Binary Search 3 times
+        # First binary search for locating any index of target.
+        # Second binary search for finding Left Boundary index if target.
+        # Third binary search for finding Right Boundary index of target.
+        found = False
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2 
+            if nums[mid] < target: l = mid + 1
+            elif nums[mid] > target: r = mid - 1
+            else: 
+                found = True
                 break
-            if target < nums[mid]: end = mid - 1
-            if target > nums[mid]: start = mid + 1
         
-        if a == None: return ans
-        # To find the left limit fo target
-        i = a
-        while i >= 0 and nums[i] == target:
-            i -= 1
-        ans[0] = i + 1
-        # To find the right limit of target
-        i = a
-        while i < len(nums) and nums[i] == target:
-            i += 1
-        ans[1] = i - 1
+        if not found: return ans
+        
+        # Finding Left Boundary index of Target.
+        l, r = 0, mid
+        while l <= r:
+            m = (l + r) // 2  
+            if nums[m] < target: l = m + 1
+            elif nums[m] > target: r = m 
+            else: 
+                if m == 0:
+                    ans[0] = m
+                    break
+                if m - 1 >= 0 and nums[m - 1] != target:
+                    ans[0] = m
+                    break
+                r = m - 1
+                
+        # Finding Right Boundary index of Target.
+        l, r = mid, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2 
+            if nums[m] < target: l = m + 1
+            elif nums[m] > target: r = m 
+            else: 
+                if m == len(nums) - 1:
+                    ans[1] = m
+                    break
+                if m + 1 < len(nums) and nums[m + 1] != target:
+                    ans[1] = m
+                    break
+                l = m + 1
         
         return ans
+                
+            
+            
+            
