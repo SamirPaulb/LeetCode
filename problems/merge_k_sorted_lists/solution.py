f418f18d6eeb12dc7c40ebbf33396de2a20e2657
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-25 08:49:39 +0000

    Sync LeetCode submission - Merge k Sorted Lists (python3)

--- problems/merge_k_sorted_lists/solution.py
+++ problems/merge_k_sorted_lists/solution.py
@@ -1,28 +1,19 @@
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists) == 0: return None
-        while len(lists) > 1:
-            lists[0] = self.merge2SortedLists(lists[0], lists[1])
-            lists.pop(1)
-        
-        return lists[0]
-    
-    def merge2SortedLists(self, l1, l2):
-        if not l1 or not l2: return l1 or l2
-        if l1.val <= l2.val: a, b = l1, l2
-        else: a, b = l2, l1
-        ans = a
-        while a or b:
-            if not a.next: 
-                a.next = b
-                return ans
-            if not b: return ans
-            if a.next.val <= b.val: a = a.next
-            else:
-                tmp = a.next
-                a.next = b
-                b = tmp
-                a = a.next
+        myHeap = []
+        for head in lists:
+            while head:
+                myHeap.append(head.val)
+                head = head.next
                 
+        heapq.heapify(myHeap)
         
+        ans = ListNode(-1)
+        a = ans
         
+        while myHeap:
+            a.next = ListNode(heapq.heappop(myHeap))
+            a = a.next
+        
+        return ans.next
