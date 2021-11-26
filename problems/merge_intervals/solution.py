a7fd993242d413d445ff60813ab233303be8f09f
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-26 16:44:54 +0000

    Sync LeetCode submission - Merge Intervals (python3)

--- problems/merge_intervals/solution.py
+++ problems/merge_intervals/solution.py
@@ -1,14 +1,23 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key=lambda X : X[0])  # Sort intervals 0-th column wise. 
-        #print(intervals)
-        l, r = 0, 1
-        while r < len(intervals):
-            if intervals[r][0] <= intervals[l][1]:
-                intervals[r] = [intervals[l][0], max(intervals[l][1], intervals[r][1])]
-                intervals.pop(l)
+        intervals = sorted(intervals, key=lambda x: x[0])
+        # print(intervals)
+        i = 0
+        while i < len(intervals) - 1:
+            a, b = intervals[i], intervals[i + 1]
+            if a[-1] >= b[0]:
+                intervals[i] = [a[0], max(a[-1], b[-1])]
+                intervals.pop(i + 1)
             else:
-                r += 1
-                l += 1
+                i += 1
         
         return intervals
+           
+
+'''
+[[1,3],[2,6],[8,10],[15,18]]
+[[1,4],[4,5]]
+[[1,4],[0,4]]
+[[1,4],[2,3]]
+[[2,3],[2,2],[3,3],[1,3],[5,7],[2,2],[4,6]]
+'''
