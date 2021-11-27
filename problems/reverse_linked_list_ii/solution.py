82eb6f4f45058ff0fda34a1ffdc83d4abf0ebed9
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-27 18:51:55 +0000

    Sync LeetCode submission - Reverse Linked List II (python3)

--- problems/reverse_linked_list_ii/solution.py
+++ problems/reverse_linked_list_ii/solution.py
@@ -1,48 +1,50 @@
+# using a for loop concept of https://leetcode.com/problems/reverse-nodes-in-k-group/
 class Solution:
-    def reverseBetween(self, head, m, n):
-        """
-        :type head: ListNode
-        :type m: int
-        :type n: int
-        :rtype: ListNode
-        """
-
-        if not head:
-            return None
-
-        left, right = head, head
-        stop = False
-        def recurseAndReverse(right, m, n):
-            nonlocal left, stop
-
-            # base case. Don't proceed any further
-            if n == 1:
-                return
-
-            # Keep moving the right pointer one step forward until (n == 1)
-            right = right.next
-
-            # Keep moving left pointer to the right until we reach the proper node
-            # from where the reversal is to start.
-            if m > 1:
-                left = left.next
-
-            # Recurse with m and n reduced.
-            recurseAndReverse(right, m - 1, n - 1)
-
-            # In case both the pointers cross each other or become equal, we
-            # stop i.e. don't swap data any further. We are done reversing at this
-            # point.
-            if left == right or right.next == left:
-                stop = True
-
-            # Until the boolean stop is false, swap data between the two pointers     
-            if not stop:
-                left.val, right.val = right.val, left.val
-
-                # Move left one step to the right.
-                # The right pointer moves one step back via backtracking.
-                left = left.next           
-
-        recurseAndReverse(right, m, n)
-        return head
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head or not head.next: return head
+        dummy = ListNode(-1) # creating dummy node if by chance left == head
+        dummy.next = head
+        
+        if left == right: return dummy.next # left and right are in same node in the LinkedList
+        
+        pre = dummy
+        cur = pre.next
+        nex = cur.next
+        
+        # Move these three pointers to left
+        n = 1
+        while n < left:
+            pre = pre.next
+            cur = cur.next
+            nex = nex.next
+            n += 1
+        
+        # Now play with link pointers and reverse
+        for i in range(right - left):
+            cur.next = nex.next
+            nex.next = pre.next
+            pre.next = nex
+            nex = cur.next
+        
+        return dummy.next
+    
+    
+    
+''' Test Cases:
+[1,2,3,4,5]
+2
+4
+[5]
+1
+1
+[3,5]
+1
+1
+[3,5]
+2
+2
+[3,5]
+1
+2
+'''
+            
