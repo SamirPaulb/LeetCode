384ad72682331a3aac28de3800d63cbd47bceecf
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-11-28 10:46:11 +0000

    Sync LeetCode submission - Remove Duplicate Letters (python3)

--- problems/remove_duplicate_letters/solution.py
+++ problems/remove_duplicate_letters/solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        last_index = {}
+        # searching elements from a Hash Set takes O(N) time
+        visited = set() # Hash Set
+        stack = []
+        lastIndex = {ch: i for i, ch in enumerate(s)}
+        
         for i, ch in enumerate(s):
-            last_index[ch] = i
-        cur_result = []
-        #print(last_index)
-        for i, ch in enumerate(s):
-            if ch not in cur_result:
-                while cur_result and ch < cur_result[-1] and i < last_index[cur_result[-1]]:
-                    cur_result.pop()
-                cur_result.append(ch)
-        return "".join(cur_result)
-    
+            if ch not in visited:
+                while stack and stack[-1] > ch and lastIndex[stack[-1]] > i:
+                    visited.remove(stack.pop())
+                stack.append(ch)
+                visited.add(ch)
+        
+        return "".join(stack)
+                    
