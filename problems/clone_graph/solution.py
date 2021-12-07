d82d62209a0d2af1bc64a222bdb14a72a40e0460
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-12-07 04:34:00 +0000

    Sync LeetCode submission - Clone Graph (python3)

--- problems/clone_graph/solution.py
+++ problems/clone_graph/solution.py
@@ -11,18 +11,18 @@ class Solution:
         # BFS - QUEUE
         if not node: return None
         # map original nodes to their clones
-        d = {node : Node(node.val)}
+        d ={node : Node(node.val)} 
         q = [node]
         
         while q:
-            for i in range(len(q)):
-                curNode = q.pop()
-                for nei in curNode.neighbors:
-                    if nei not in d:
-                        # store copy of the neighboring node
-                        d[nei] = Node(nei.val)
-                        q.append(nei)
-                    # connect the node copy at hand to its neighboring nodes (also copies) 
-                    d[curNode].neighbors.append(d[nei])
+            currNode = q.pop()
+            for nei in currNode.neighbors:
+                if nei not in d:
+                    # store copy of the neighboring node
+                    d[nei] = Node(nei.val)
+                    q.append(nei)
+                # connect the node copy at hand to its neighboring nodes (also copies) 
+                d[currNode].neighbors.append(d[nei])
         # return copy of the starting node
         return d[node]
+        
