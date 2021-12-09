96b91a343d399b938627c7301d2ac6750b3c8caa
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-12-09 06:02:46 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (python3)

--- problems/maximum_depth_of_binary_tree/solution.py
+++ problems/maximum_depth_of_binary_tree/solution.py
@@ -7,5 +7,5 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root: return 0
-        return (1 + max(self.maxDepth(root.left), self.maxDepth(root.right)))
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
     
