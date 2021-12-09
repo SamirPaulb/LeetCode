e8cf8ad6274c6c7ecd085f4ff3ea58f3658b2093
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-12-09 05:57:54 +0000

    Sync LeetCode submission - Construct Binary Tree from Preorder and Inorder Traversal (python3)

--- problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.py
+++ problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.py
@@ -1,23 +1,21 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        self.preorder_index = 0  # nonelocal variable => self.preorder_index = nonlocal preorder_index
+        self.preorderIndex = 0
+        inorderIndex = {}
+        for i, ch in enumerate(inorder):
+            inorderIndex[ch] = i
+        
         def constractTree(left, right):
             if left > right: return None
-            
-            rootValue = preorder[self.preorder_index]
+            rootValue = preorder[self.preorderIndex]
             root = TreeNode(rootValue)
-            
-            self.preorder_index += 1
+            self.preorderIndex += 1
             
             if left == right: return root
             
-            root.left = constractTree(left, inorder_index[rootValue] - 1)
-            root.right = constractTree(inorder_index[rootValue] + 1, right)
+            root.left = constractTree(left, inorderIndex[rootValue] - 1)
+            root.right = constractTree(inorderIndex[rootValue] + 1, right)
             
             return root
-        
-        inorder_index = {}
-        for i, element in enumerate(inorder):
-            inorder_index[element] = i
         
         return constractTree(0, len(preorder) - 1)
