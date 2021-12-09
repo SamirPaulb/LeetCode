4abfd0866193fe0db7033e62060f3735d1e4c7a5
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-12-09 05:00:56 +0000

    Sync LeetCode submission - Convert Sorted List to Binary Search Tree (python3)

--- problems/convert_sorted_list_to_binary_search_tree/solution.py
+++ problems/convert_sorted_list_to_binary_search_tree/solution.py
@@ -1,6 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        if not head: return 
+        if not head: return
         if not head.next: return TreeNode(head.val)
         
         arr = []
@@ -8,12 +19,18 @@ class Solution:
             arr.append(head.val)
             head = head.next
         
-        def binaryTraversal(arr, left, right):
-            if left > right: return None
+        def binarysearch(arr, left, right):
+            if left > right: return 
             mid = left + (right - left) // 2
             root = TreeNode(arr[mid])
-            root.left = binaryTraversal(arr, left, mid - 1)
-            root.right = binaryTraversal(arr, mid + 1, right)
+            root.left = binarysearch(arr, left, mid - 1)
+            root.right = binarysearch(arr, mid + 1, right)
             return root
-        
-        return binaryTraversal(arr, 0, len(arr) - 1)
+    
+        return binarysearch(arr, 0, len(arr) - 1)
+            
+            
+            
+            
+            
+            
