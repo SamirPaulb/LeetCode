2d3c1b58feae6c1356bfe63134fec0f5a290b7bb
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-12-10 11:43:10 +0000

    Sync LeetCode submission - Reorder List (python3)

--- problems/reorder_list/solution.py
+++ problems/reorder_list/solution.py
@@ -1,8 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         arr = []
@@ -12,14 +7,16 @@ class Solution:
             a = a.next
         
         l, r = 0, len(arr) - 1
-        
         while l <= r and b:
             b.val = arr[l]
+            b = b.next
             l += 1
-            b = b.next
+            
             if not b: break
+            
             b.val = arr[r]
-            r -= 1
             b = b.next
+            r -= 1
         
         return head
+        
