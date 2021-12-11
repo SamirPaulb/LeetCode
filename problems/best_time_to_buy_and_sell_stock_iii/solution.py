86cd4f6aa0bd022bd072da967f218d3b18280d45
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-12-11 12:46:50 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock III (python3)

--- problems/best_time_to_buy_and_sell_stock_iii/solution.py
+++ problems/best_time_to_buy_and_sell_stock_iii/solution.py
@@ -7,29 +7,29 @@ class Solution:
         # Add both elements of the 2 array of same indexed
         # Make Firt array by forward traversal of Prices
         # Make Second array by Reversed traversal of Prices
-        n = len(prices)
-        profit1 = [0] * n
-        profit2 = [0] * n
         
+        n = len(prices)
+        profit1, profit2 = [0] * n, [0] * n 
         minSoFar = prices[0]
         maxProfitSoFar = 0
         for i in range(n):
-            maxProfitSoFar = max(maxProfitSoFar, prices[i] - minSoFar)
-            minSoFar = min(minSoFar, prices[i])
-            profit1[i] = maxProfitSoFar
-        
+        	maxProfitSoFar = max(maxProfitSoFar, prices[i] - minSoFar)
+        	minSoFar = min(minSoFar, prices[i])
+        	profit1[i] = maxProfitSoFar
+
         maxSoFar = prices[-1]
         maxProfitSoFar = 0
-        for i in range(n-1, -1, -1):
-            maxProfitSoFar = max(maxProfitSoFar, maxSoFar - prices[i])
-            maxSoFar = max(maxSoFar, prices[i])
-            profit2[i] = maxProfitSoFar
-        
-        ans = 0
+        for i in range(n - 1, -1, -1):
+        	maxProfitSoFar = max(maxProfitSoFar, maxSoFar - prices[i])
+        	maxSoFar = max(maxSoFar, prices[i])
+        	profit2[i] = maxProfitSoFar
+
+        res = 0
         for i in range(n):
-            ans = max(ans, profit1[i] + profit2[i])
-        
-        return ans
+        	res = max(res, profit1[i] + profit2[i])
+
+        return res
     
-    # Time Complexity = O(n)
-    # Space Complexity = O(n)
+        
+# Time Complexity = O(n)
+# Space Complexity = O(n)
