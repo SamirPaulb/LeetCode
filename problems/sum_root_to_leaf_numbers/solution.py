bcc222bad2aebf8094b09722a439af043feee18a
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-12-11 13:23:04 +0000

    Sync LeetCode submission - Sum Root to Leaf Numbers (python3)

--- problems/sum_root_to_leaf_numbers/solution.py
+++ problems/sum_root_to_leaf_numbers/solution.py
@@ -1,19 +1,14 @@
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         self.ans = 0
-        # https://leetcode.com/problems/binary-tree-paths/
-        def binaryTreePaths(root, s):
+        def binaryTreePath(root, s):
             if not root: return
-            if root.left == None and root.right == None:
+            if not root.left and not root.right:
                 s += str(root.val)
-                self.ans += (int(s))
-            if root.left:
-                binaryTreePaths(root.left, s = s + str(root.val))
-            if root.right:
-                binaryTreePaths(root.right, s = s + str(root.val))
-                
-        binaryTreePaths(root, "")
+                self.ans += int(s)
+                return
+            binaryTreePath(root.left, s + str(root.val))
+            binaryTreePath(root.right, s + str(root.val))
         
+        binaryTreePath(root, "")
         return self.ans
-
-    
