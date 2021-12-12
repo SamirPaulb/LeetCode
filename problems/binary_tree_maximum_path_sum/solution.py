624d17ec3dd6cc1926d92ac724bba80f99c79364
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-12-12 16:26:42 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (python3)

--- problems/binary_tree_maximum_path_sum/solution.py
+++ problems/binary_tree_maximum_path_sum/solution.py
@@ -1,19 +1,23 @@
-# https://www.youtube.com/watch?v=TO5zsKtc1Ic
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.result = float("-inf")
-        
-        def maxPath_util(root):
+        if not root: return
+        self.ans = root.val 
+        def pathSum(root):
             if not root: return 0
-            leftMax = maxPath_util(root.left)
-            rightMax = maxPath_util(root.right)
+            maxLeft = pathSum(root.left)
+            maxRight = pathSum(root.right)
             
-            max_stright = max(root.val + max(leftMax, rightMax), root.val)
-            max_case_LR = max(max_stright, leftMax + root.val + rightMax)
-            self.result = max(self.result, max_case_LR)
-            return max_stright
+            strightSum = max(root.val + max(maxLeft, maxRight), root.val)
+            maxSum = max(strightSum, maxLeft + root.val + maxRight)
+            self.ans = max(self.ans, maxSum)
+            
+            return strightSum
         
-        maxPath_util(root)
-        
-        return self.result
-    
+        pathSum(root)
+        return self.ans
