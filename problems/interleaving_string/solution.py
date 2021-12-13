cbb715c27f8e6dc150b698dc2e17eb65818e2394
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-12-13 08:48:54 +0000

    Sync LeetCode submission - Interleaving String (python3)

--- problems/interleaving_string/solution.py
+++ problems/interleaving_string/solution.py
@@ -1,16 +1,13 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        m = len(s1)
-        n = len(s2)
+        n = len(s1)
+        m = len(s2)
         @lru_cache(None)
-        
         def dfs(i, j, k):
-            if i > m - 1 and j > n - 1:
-                return True
-            if i < m and s1[i] == s3[k] and dfs(i + 1, j, k + 1):
-                return True
-            if j < n and s2[j]  == s3[k] and dfs(i, j + 1, k + 1):
-                return True
+            if i >= n and j >= m: return True
+            if i < n and s1[i] == s3[k] and dfs(i + 1, j, k + 1): return True
+            if j < m and s2[j] == s3[k] and dfs(i, j + 1, k + 1): return True
             return False
         
         return Counter(s1) + Counter(s2) == Counter(s3) and dfs(0, 0, 0)
+        
