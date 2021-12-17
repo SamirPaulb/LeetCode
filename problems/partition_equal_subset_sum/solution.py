7c64ef8ae1ec8e281777c28a2246237cdd96640a
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-12-17 06:07:43 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (python3)

--- problems/partition_equal_subset_sum/solution.py
+++ problems/partition_equal_subset_sum/solution.py
@@ -1,18 +1,23 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        nums.sort()
         s = sum(nums)
+        equalSum = s // 2
         if s % 2 != 0: return False
-        
-        dp = [[False]*(s//2 + 1) for i in range(len(nums)+1)]
-        
-        for i in range(len(nums)+1):
-            for j in range(s//2 + 1):
-                if j == 0:
-                    dp[i][j] = True
-                elif nums[i-1] > j:
-                    dp[i][j] = dp[i-1][j]
+        n = len(nums)
+        dp = [[False] * (equalSum + 1) for i in range(n + 1)]
+        # Change the first row => arr is empty => No subset is there for any sum > 0
+        for j in range(equalSum + 1):
+            dp[0][j] = False
+        # Change the first column => target sum is 0 => for any subset sum can be equal to 0
+        for i in range(n + 1):
+            dp[i][0] = True
+        # Change the value of remaining dp matrix
+        for i in range(1, n + 1):
+            for j in range(1, equalSum + 1):
+                if nums[i - 1] <= j:
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] or dp[i - 1][j]
                 else:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
-        #print(dp)
+                    dp[i][j] = dp[i - 1][j]
+                            
         return dp[-1][-1]
+        
