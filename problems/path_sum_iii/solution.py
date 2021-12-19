009134f7b14d3b84efda7dce2c4869002f892f35
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-12-19 07:04:22 +0000

    Sync LeetCode submission - Path Sum III (python3)

--- problems/path_sum_iii/solution.py
+++ problems/path_sum_iii/solution.py
@@ -1,13 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        
-        def includeCurrRoot(root, targetSum):
-            if not root: return 0
+        def incluideCurrNode(node, targetSum, res):
             res = 0
-            if root.val == targetSum: res += 1
-            res += includeCurrRoot(root.left, targetSum - root.val)               
-            res += includeCurrRoot(root.right, targetSum - root.val)
+            if not node: return 0
+            if node.val == targetSum: res += 1
+            res += incluideCurrNode(node.left, targetSum - node.val, res)
+            res += incluideCurrNode(node.right, targetSum - node.val, res)
             return res
-        
+                    
         if not root: return 0
-        return self.pathSum(root.left, targetSum) + includeCurrRoot(root, targetSum) + self.pathSum(root.right, targetSum)
+        return self.pathSum(root.left, targetSum) + incluideCurrNode(root, targetSum, 0) + self.pathSum(root.right, targetSum)
