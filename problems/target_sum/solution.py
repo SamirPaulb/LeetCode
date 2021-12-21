f7cf5504b0bd2d2d8d026354363fdb5fa536f8d6
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2021-12-21 18:07:13 +0000

    Sync LeetCode submission - Target Sum (python3)

--- problems/target_sum/solution.py
+++ problems/target_sum/solution.py
@@ -1,39 +1,35 @@
+# https://www.youtube.com/watch?v=Hw6Ygp3JBYw&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=12
+# https://leetcode.com/problems/target-sum/
+'''
+Assigning + and - before elements of nums of that sum of all elements is equal to target
+Basically we are again deviding the array into 2 subsets such that diffrence between that subsets equal to target
+s1 - s2 = target    ---(1)
+s1 + s2 = sum(nums) ---(2)
+(1) + (2)
+s1 = (target + sum(nums)) // 2
+Now we have to find number of possible subsets s1 in nums
+'''
+
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        '''
-        s1 - s2 = target     ----equation 1
-        s1 + s2 = sun(nums)  ----equation 2
-        solving eq1 + eq2, we get:
-        =>  2 * s1 = target + sum(nums)
-        =>  s1 = (target + sum(nums)) / 2
-        so, we have to find a subset s1 in nums.
-        # https://www.youtube.com/watch?v=MqYLmIzl8sQ
-        '''
-        sumOfNums = sum(nums)
-        n = len(nums)
-        
-        if abs(target) > sumOfNums:  # nums = [10], target = -100
-            return 0
-        
-        # 2 * s1 = (target + sum(nums))  =>  (target + sum(nums)) must be an even number
-        if (target + sumOfNums) % 2 != 0:
-            return 0
-        
-        s1 = (sumOfNums + target) // 2
-        
-        # DP Table
-        t = [[0] * (s1 + 1) for i in range(n + 1)]
-		
-        t[0][0] = 1
-        
-        for i in range(1, n + 1):
+        if abs(target) > sum(nums): return 0  # nums = [10], target = -100
+        s1 = (target + sum(nums)) // 2
+        # 2 * s1 = (target + sum(nums))  => as (target + sum(nums)) is a multiple of 2 so it must be an even number
+        if (target + sum(nums)) % 2 != 0: return 0
+        dp = [[0]*(s1+1) for i in range(len(nums) + 1)]
+        # 1st row of dp = 0; as in 1st row size of nums = [] so no subset s1 is possile for sum(s1) > 0
+        for j in range(s1 + 1):
+            dp[0][j] = 0
+        # 1st column of dp = 1; as in 1st column sum(s1) = 0; so empty nums is possibel so 1 possible subset s1 everytime
+        for i in range(len(nums)+1):
+            dp[i][0] = 1
+        # change values of remaining dp starting from (1, 1) to (len(nums), s1)
+        for i in range(1, len(nums) + 1):
             for j in range(s1 + 1):
-                
                 if nums[i - 1] <= j:
-                    t[i][j] = t[i - 1][j] + t[i - 1][j - nums[i - 1]]
-                
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j]
                 else:
-                    t[i][j] = t[i-1][j]
-            
-        return t[n][s1]
-            
+                    dp[i][j] = dp[i - 1][j]
+        
+        return dp[-1][-1] # dp[len(nums)][s1]
+    
