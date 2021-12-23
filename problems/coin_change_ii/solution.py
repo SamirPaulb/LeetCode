5702bed94b224da433a4aaf454ce522cc4c4aaee
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2021-12-23 18:23:00 +0000

    Sync LeetCode submission - Coin Change II (python3)

--- problems/coin_change_ii/solution.py
+++ problems/coin_change_ii/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[0] * (amount+1) for i in range(len(coins)+1)] 
+        dp = [[0]*(amount+1) for i in range(len(coins) +1)]
+        
         for i in range(1, len(coins)+1):
-            for j in range(0, amount+1):
-                if j == 0:
-                    dp[i][j] = 1
-                    
-                elif coins[i-1] > j:
-                    dp[i][j] = dp[i-1][j]
-
+            dp[i][0] = 1
+            
+        for i in range(1, len(coins)+1):
+            for j in range(1, amount + 1):
+                if coins[i-1] <= j:
+                    dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j]
                 else:
-                    dp[i][j] = (dp[i-1][j] + dp[i][j-coins[i-1]])
-                print(dp[i][j], end = " ")
-            print()
-        return dp[len(coins)][-1]
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[-1][-1]
+    
