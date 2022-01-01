399c13c04c66bbb7d0ee6d0c46309cec74ba08f2
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-01-01 09:25:44 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (python3)

--- problems/binary_tree_maximum_path_sum/solution.py
+++ problems/binary_tree_maximum_path_sum/solution.py
@@ -1,23 +1,30 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# https://www.youtube.com/watch?v=Osz-Vwer6rw&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=48
+# https://leetcode.com/problems/binary-tree-maximum-path-sum/
+
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         if not root: return
-        self.ans = root.val 
-        def pathSum(root):
-            if not root: return 0
-            maxLeft = pathSum(root.left)
-            maxRight = pathSum(root.right)
-            
-            strightSum = max(root.val + max(maxLeft, maxRight), root.val)
-            maxSum = max(strightSum, maxLeft + root.val + maxRight)
-            self.ans = max(self.ans, maxSum)
-            
-            return strightSum
+        self.res = root.val  
         
-        pathSum(root)
-        return self.ans
+        def solve(root):
+            # Base case
+            if not root: return 0
+            
+            # l and r store maximum path sum going through left and right child of root respectively
+            l = solve(root.left)
+            r = solve(root.right)
+            
+            # Max path for parent call of root. This path must include at most one child of root
+            temp = max(root.val + max(l, r), root.val)
+            # ans  represents the sum when the node under consideration is the root of the maxSum path and no ancestor of root are there in max sum path
+            ans = max(temp, root.val + l + r)
+            self.res = max(self.res, ans) # max across the whole tree
+            
+            return temp # for considering other subtrees also
+        
+        solve(root)
+        return self.res
+    
+    
+# Time Complexity: O(n) where n is number of nodes in Binary Tree.
+    
