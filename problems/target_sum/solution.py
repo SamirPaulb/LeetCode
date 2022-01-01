f5fd35960d0ddf588cf29a012c01fe77c903607d
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-01-01 18:22:42 +0000

    Sync LeetCode submission - Target Sum (python3)

--- problems/target_sum/solution.py
+++ problems/target_sum/solution.py
@@ -1,17 +1,18 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        if (sum(nums) + target) % 2 != 0: return 0
         if abs(target) > sum(nums): return 0
-        if (target + sum(nums)) % 2 != 0: return 0
-        s1 = (target + sum(nums)) // 2
-        dp = [[0]*(s1 + 1) for i in range(len(nums)+1)]
-        for i in range(len(nums) + 1):
-            dp[i][0] = 1
-        
+        tar = (sum(nums) + target) // 2
+        dp = [[0]*(tar+1) for i in range(len(nums)+1)]
+        for i in range(len(nums)+1):
+            dp[i][0] =  1
         for i in range(1, len(nums)+1):
-            for j in range(s1 + 1):
+            for j in range(tar+1):
                 if nums[i-1] <= j:
                     dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j]
                 else:
                     dp[i][j] = dp[i-1][j]
-        #print(dp)
+        
         return dp[-1][-1]
+        
+    
