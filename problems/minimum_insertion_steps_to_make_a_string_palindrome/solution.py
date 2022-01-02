c87bc1f8a11f94dc9c26b5292376063d770d6f68
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-01-02 18:43:03 +0000

    Sync LeetCode submission - Minimum Insertion Steps to Make a String Palindrome (python3)

--- problems/minimum_insertion_steps_to_make_a_string_palindrome/solution.py
+++ problems/minimum_insertion_steps_to_make_a_string_palindrome/solution.py
@@ -1,29 +1,17 @@
-# https://www.youtube.com/watch?v=CFwCCNbRuLY&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=27
-# https://practice.geeksforgeeks.org/problems/minimum-deletitions1648/1#
-# https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
-
-'''
-use LPS => LCS
-Minimum number of Deletion or Insertion  in a string to make it a palindrome => Maximum Length of Palindromic Subsequence(LPS) after Deletion or Minimum number of Deletion or Insertion.
-numebr of deletion = len(input) - LPS(input)
-LPS(X) = LCS(X, X[::-1)
-'''
 class Solution:
-    def minInsertions(self, S):  #or minimumNumberOfDeletions(self,S):
+    def minInsertions(self, s: str) -> int:
+        # Minimum Number of insertions = len(S) - LCS
         def LCS(X, Y):
             m = len(X); n = len(Y)
-            dp = [[0]*(n+1) for i in range(n+1)]
-            # 0th row and 0th coloum initialize as 0; as for size of any of X or Y equal 0 No common subsequence
-            for i in range(1, m+1):
-                for j in range(1, n+1):
+            dp = [[0]*(len(Y)+1) for i in range(len(X)+1)]
+            for i in range(1, len(X)+1):
+                for j in range(1, len(Y)+1):
                     if X[i-1] == Y[j-1]:
                         dp[i][j] = 1 + dp[i-1][j-1]
                     else:
-                        dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
             
             return dp[-1][-1]
         
-        LPS = LCS(S, S[::-1])
-        
-        return len(S) - LPS
-
+        return len(s) - LCS(s, s[::-1])
+    
