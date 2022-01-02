dbf035354417e0050247ea428bc80119f6bd9a46
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-01-02 14:58:16 +0000

    Sync LeetCode submission - Delete Operation for Two Strings (python3)

--- problems/delete_operation_for_two_strings/solution.py
+++ problems/delete_operation_for_two_strings/solution.py
@@ -1,26 +1,17 @@
-# https://www.youtube.com/watch?v=-fx6aDxcWyg&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=25
-# https://practice.geeksforgeeks.org/problems/minimum-number-of-deletions-and-insertions0209/1
-
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        X = word1; Y = word2; m = len(X); n = len(Y)
-		# Implement Longest Common Subsequence DP
-		# LCS of X and Y is ea [X = "heap", Y = "pea"]
-		# in X LCS will remain intact and remaining charecters will be removed
-		# in LCS add the charecters present in Y
+        x = word1; y = word2; m = len(x); n = len(y)
         dp = [[0]*(n+1) for i in range(m+1)]
-        # in 0th row and 0th column No common subsecuence can be so 0
         for i in range(1, m+1):
             for j in range(1, n+1):
-                if X[i-1] == Y[j-1]:
+                if x[i-1] == y[j-1]:
                     dp[i][j] = 1 + dp[i-1][j-1]
                 else:
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         
-        lenOfLCS = dp[-1][-1]
-        numberOfDeletion = len(X) - lenOfLCS
-        numberOfInsertion = len(Y) - lenOfLCS
+        lcs = dp[-1][-1]
         
-        totalSteps = numberOfDeletion + numberOfInsertion
+        d = m - lcs
+        i = n - lcs
         
-        return totalSteps
+        return d + i
