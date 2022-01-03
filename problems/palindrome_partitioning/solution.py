6f0bc3124ef71631a7eb7cb4780bfffb72525089
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-01-03 09:48:53 +0000

    Sync LeetCode submission - Palindrome Partitioning (python3)

--- problems/palindrome_partitioning/solution.py
+++ problems/palindrome_partitioning/solution.py
@@ -1,16 +1,14 @@
 class Solution:
-    def partition(self, s):
+    def partition(self, s: str) -> List[List[str]]:
         res = []
-        self.dfs(s, [], res)
+        def solve(s, path):
+            if not s: res.append(path); return
+            for i in range(1, len(s)+1):
+                if isPalindrome(s[:i]): 
+                    solve(s[i:], path + [s[:i]])
+        
+        def isPalindrome(s): return s == s[::-1]
+        
+        solve(s, [])
+        
         return res
-    
-    def dfs(self, s, path, res):
-        if not s:
-            res.append(path)
-            return
-        for i in range(1, len(s) + 1):
-            if self.isPalindrom(s[:i]):
-                self.dfs(s[i:], path + [s[:i]], res)
-                
-    def isPalindrom(self, s):
-        return s == s[::-1]
