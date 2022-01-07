5823debe7b2b11ec59b6413783b839863bcefa81
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-01-07 06:18:26 +0000

    Sync LeetCode submission - Minimum Path Sum (python3)

--- problems/minimum_path_sum/solution.py
+++ problems/minimum_path_sum/solution.py
@@ -1,16 +1,21 @@
+# https://www.youtube.com/watch?v=BzTIOsC0xWM
+# Traversing from bottom-right most to top-left most corner
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        r = len(grid)
-        c = len(grid[0])
-        for j in range(1, c):
-            grid[0][j] = grid[0][j-1] + grid[0][j]
+        r = len(grid); c = len(grid[0])
         
-        for i in range(1, r):
-            grid[i][0] = grid[i-1][0] + grid[i][0]
+        # in (c-1)th coloumn only 1 path available. ie, straight down. So bottom to up path along right most column is continuous sum of elements
+        for i in range(r-2, -1, -1):
+            grid[i][c-1] += grid[i+1][c-1]
         
-        for i in range(1, r):
-            for j in range(1, c):
-                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        # in (r-1)th row only 1 path is available => straight right. so right to left path along bottom most row is contigeuos sum of elements
+        for j in range(c-2, -1, -1):
+            grid[r-1][j] += grid[r-1][j+1]
         
-        return grid[-1][-1]
+        # from (r-2, c-2) to (0, 0) path => in any cell add minimum value of down or right cell 
+        for i in range(r-2, -1, -1):
+            for j in range(c-2, -1, -1):
+                grid[i][j] += min(grid[i+1][j], grid[i][j+1])
+        
+        return grid[0][0]
     
