84f22cdfd02e25ba835b720645931d3ade0268f5
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-01-08 06:46:25 +0000

    Sync LeetCode submission - Longest Palindromic Substring (python3)

---
+++ problems/longest_palindromic_substring/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            # Check for ODD length
+            l = i; r = i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > len(res):
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+                
+            # Check for EVEN length
+            l = i; r = i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > len(res):
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+                
+        
+        return res
