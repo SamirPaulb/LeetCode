66e2c47b05411838be561e1c15ac3f750a8900aa
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-01-10 13:30:23 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -1,34 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        Island = 0
-        q = []
+        island = 0
         
-        def dfs(i, j):
-            while q:
-                a = q.pop()
-                i, j = a[0], a[1]
-                if i+1 < len(grid) and grid[i+1][j] == '1':    # Down
-                    q.append([i+1, j])
-                    grid[i+1][j] = "0"
-                if i-1 >= 0 and grid[i-1][j] == '1':           # Up
-                    q.append([i-1, j])
-                    grid[i-1][j] = "0"
-                if j+1 < len(grid[0]) and grid[i][j+1] == '1': # Right
-                    q.append([i, j+1])
-                    grid[i][j+1] = "0"
-                if j-1 >= 0 and grid[i][j-1] == '1':           # Left
-                    q.append([i, j-1])
-                    grid[i][j-1] = "0"
-
+        def dfs(x, y):
+            if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]) or grid[x][y] == "0":
+                return
+            grid[x][y] = "0"
+            dfs(x+1, y)
+            dfs(x-1, y)
+            dfs(x, y-1)
+            dfs(x, y+1)
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1': 
-                    Island += 1
-                    grid[i][j] = "0"
-                    q.append([i, j])
+                if grid[i][j] == "1":
+                    island += 1
                     dfs(i, j)
 
-        return Island
-
-
-
+        return island
