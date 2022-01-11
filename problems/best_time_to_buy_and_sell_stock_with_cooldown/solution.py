717e1c75a1d34d09a13da0f87a670aa7f1791086
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-01-11 08:41:45 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock with Cooldown (python3)

--- problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.py
+++ problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.py
@@ -1,15 +1,13 @@
-# https://www.youtube.com/watch?v=GY0O57llkKQ
-# First Solve -->> 714. Best Time to Buy and Sell Stock with Transaction Fee -->> then solve this problem
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        obsp = -prices[0] # obsp = Old Brought state Profit
-        ossp = 0 # ossp = Old Sold State Profit
-        ocsp = 0 # ocsp = Old Cold State Profit
+        obsp = - prices[0]  # Old Brought State Profit
+        ossp = 0            # Old Sold State Profit
+        ocsp = 0            # Old Cold State Profit
+        
         for i in range(1, len(prices)):
-            nbsp = 0 # nbsp = New Brought State Profit
-            nssp = 0 # nssp = New Sold State Profit
-            ncsp = 0 # ncsp = New Cold State Profit
+            nbsp = 0        # New Brought State Profit
+            nssp = 0        # New Sold State Profit
+            ncsp = 0        # New Cold State Profit
             
             nbsp = max(ocsp - prices[i], obsp)
             nssp = max(obsp + prices[i], ossp)
