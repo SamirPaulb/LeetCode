4595a53527581a9934b3ca799cc11665bc89f875
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-01-12 05:37:23 +0000

    Sync LeetCode submission - Rotate Image (python3)

--- problems/rotate_image/solution.py
+++ problems/rotate_image/solution.py
@@ -1,24 +1,21 @@
-# https://www.youtube.com/watch?v=Y72QeX0Efxw
-'''
-First Transpose(flip matrix over its diagonal) the matrix then rotate every rows
-[[1,2,3],                [[1,4,7],                 [[7,4,1],
- [4,5,6],  =>Transpose=>  [2,5,8], =>Rotate rows=>  [8,5,2],
- [7,8,9]]                 [3,6,9]]                  [9,6,3]]
-'''
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         n = len(matrix)
-        #Transpose
+        
+        # Transpose
         for i in range(n):
-            for j in range(i + 1, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        #Rotate rows
-        for i in range(n):
-            l, r = 0, n - 1
-            while l < r:
+            for j in range(i+1, n):
+                if i != j:
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        # swap left columns with right  columns
+        l = 0; r = n-1
+        while l < r:
+            for i in range(n):
                 matrix[i][l], matrix[i][r] = matrix[i][r], matrix[i][l]
-                l += 1; r -= 1
-        return matrix
-
-# Time Complexity = O(n*n)
-# Space Complexity = O(1)
+            
+            l += 1
+            r -= 1
+        
+        
+        
