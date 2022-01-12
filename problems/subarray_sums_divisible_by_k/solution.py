d7a6b6ebe4756ffd223ddf27775648d6eed8d15b
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-01-12 13:45:52 +0000

    Sync LeetCode submission - Subarray Sums Divisible by K (python3)

--- problems/subarray_sums_divisible_by_k/solution.py
+++ problems/subarray_sums_divisible_by_k/solution.py
@@ -1,19 +1,23 @@
+# https://www.youtube.com/watch?v=QM0klnvTQzk
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        sumDic = {0 : 1}
-        curSum = 0
-        remainder = 0
+        # Use COncept of 523. Continuous Subarray Sum
+        
         ans = 0
-        for i, ch in enumerate(nums):
-            curSum += ch
-            remainder = curSum % k
-            if remainder < 0:
-                remainder += k
-            if remainder in sumDic:
-                ans += sumDic[remainder] 
-                sumDic[remainder] += 1
+        remainderCountDic = {0 : 1}
+        curSum = 0
+        
+        for i in range(len(nums)):
+            curSum += nums[i]
+            
+            if curSum < 0: curSum += k
+            
+            curSum = curSum % k
+            
+            if curSum in remainderCountDic:
+                ans += remainderCountDic[curSum]
+                remainderCountDic[curSum] += 1
             else:
-                sumDic[remainder] = 1
+                remainderCountDic[curSum] = 1
         
         return ans
-    
