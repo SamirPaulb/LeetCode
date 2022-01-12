1f349356a48ad59d2b5d6920a3c7227f3a02dbcd
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-01-12 18:48:42 +0000

    Sync LeetCode submission - Largest Number (python3)

--- problems/largest_number/solution.py
+++ problems/largest_number/solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        if not nums: return
         for i in range(len(nums)):
             nums[i] = str(nums[i])
-            
+        
+        # bubble sort
         for i in range(len(nums)):
             for j in range(len(nums) - 1):
                 if nums[j] + nums[j+1] < nums[j+1] + nums[j]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
-                    
-        res = "".join(nums)
-        if int(res) == 0: return "0"
-        return res
-    
+                    nums[j+1], nums[j] = nums[j], nums[j+1]
+        
+        ans = "".join(nums)
+        
+        return ans if int(ans) != 0 else "0"
