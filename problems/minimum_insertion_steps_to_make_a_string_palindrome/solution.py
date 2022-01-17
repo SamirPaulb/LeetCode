36e675ab7cd3452ec8dd166b315866bb84e3a09b
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-01-17 07:11:00 +0000

    Sync LeetCode submission - Minimum Insertion Steps to Make a String Palindrome (python3)

--- problems/minimum_insertion_steps_to_make_a_string_palindrome/solution.py
+++ problems/minimum_insertion_steps_to_make_a_string_palindrome/solution.py
@@ -1,17 +1,18 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        # Minimum Number of insertions = len(S) - LCS
-        def LCS(X, Y):
-            m = len(X); n = len(Y)
-            dp = [[0]*(len(Y)+1) for i in range(len(X)+1)]
-            for i in range(1, len(X)+1):
-                for j in range(1, len(Y)+1):
-                    if X[i-1] == Y[j-1]:
+        if s == s[::-1]: return 0
+        
+        def LCS(x, y):
+            n = len(x)
+            dp = [[0]*(n+1) for i in range(n+1)]
+            
+            for i in range(1, n+1):
+                for j in range(1, n+1):
+                    if x[i-1] == y[j-1]:
                         dp[i][j] = 1 + dp[i-1][j-1]
                     else:
-                        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                        dp[i][j] = max(dp[i][j-1], dp[i-1][j])
             
             return dp[-1][-1]
         
         return len(s) - LCS(s, s[::-1])
-    
