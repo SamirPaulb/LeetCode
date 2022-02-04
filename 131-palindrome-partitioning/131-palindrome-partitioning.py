81953bf91404a3a87befa58150c794f065b594bc
smrpb <89020961+smrpb@users.noreply.github.com>
2022-02-04 15:43:36 +0530

    Time: 1086 ms (27.97%), Space: 30.5 MB (41.15%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,17 @@
+class Solution:
+    def partition(self, s):
+        res = []
+        
+        def isPal(s):
+            return s == s[::-1]
+        
+        def dfs(s, path):
+            if not s:
+                res.append(path)
+                return
+            for i in range(1, len(s)+1):
+                if isPal(s[:i]): 
+                    dfs(s[i:], path + [s[:i]])
+        
+        dfs(s, [])
+        return res
