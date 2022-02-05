71592dd120e0997377d2dba4965a8742c4faba3d
smrpb <89020961+smrpb@users.noreply.github.com>
2022-02-05 19:28:31 +0530

    Time: 128 ms (50.33%), Space: 15.6 MB (69.42%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -33,4 +33,36 @@ class Solution:
             takenCourses.add(c)
             
         return True
+    
+    
+    
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if len(prerequisites) == 0: return True
+        adjacencyList = {i : [] for i in range(numCourses)} 
+        for num in prerequisites:
+            if num[0] not in adjacencyList: adjacencyList[num[0]] = [num[1]]
+            else: adjacencyList[num[0]].append(num[1])
+        # BFS
+        # outBound = number of elements dependent on the current element => course to be completed before 
+        # eg. for [[0,1], [0,2], [1,3], [1,4], [3,4]] outbound of 4 is 2, because 1 and 3 is dependent on 4
+        # outBound of 2 is 1, because 0 is dependent on 2 => to complete 0, 2 needs to complete first
+        outBound = [[i, 0] for i in range(numCourses)]
+        for num in prerequisites:
+            outBound[num[1]][1] += 1
+        # outbound = [[0,0], [1,1], [2,1], [3,1], [4,2]]
+        q = []
+        for i in outBound:
+            if i[1] == 0: q.append(i[0])
+        while q:
+            a = q.pop()
+            for i in adjacencyList[a]:
+                outBound[i][1] -= 1
+                if outBound[i][1] == 0: q.append(i)
+        
+        for i in outBound:
+            if i[1] != 0: return False
+        
+        return True
+    
         
