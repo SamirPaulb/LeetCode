fe0de31a5051438e0d4fc714e331a8fadf114814
smrpb <89020961+smrpb@users.noreply.github.com>
2022-02-07 00:38:19 +0530

    Time: 61 ms (6.13%), Space: 13.8 MB (94.69%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.py
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -1,5 +1,15 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        # (a + b)^2 = a*a + 2*a*b + a*b
         
-        return a + b
+        # 32 bit mask in hexadecimal
+        mask = 0xffffffff
+        
+        # works both as while loop and single value check 
+        while (b & mask) > 0:
+            
+            carry = ( a & b ) << 1
+            a = (a ^ b) 
+            b = carry
+        
+        # handles overflow
+        return (a & mask) if b > 0 else a
