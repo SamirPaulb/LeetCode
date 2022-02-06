e807646a3a4a6f2f65eb408934e7b15cc26fd303
smrpb <89020961+smrpb@users.noreply.github.com>
2022-02-06 13:32:25 +0530

    Time: 445 ms (18.04%), Space: 15.2 MB (96.03%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.py
@@ -1,10 +1,35 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
+        '''
         nums.sort()
-        # nums = [-10, -5, -1, 2, 4]  => maxproduct = 200 
-        a = nums[0]*nums[1]*nums[2]
-        b = nums[0]*nums[1]*nums[-1]
-        c = nums[0]*nums[-1]*nums[-2]
-        d = nums[-1]*nums[-2]*nums[-3]
+        # nums = [-10, -7, -5, -1, 2, 4]  => maxproduct = -10 * -7 * 4 = 280
+        a = nums[0]*nums[1]*nums[-1]
+        b = nums[-1]*nums[-2]*nums[-3]
         
-        return max(a, b, c, d)
+        return max(a, b)
+        
+        # Time: O(n log(n))
+        # space: O(1)
+        '''
+        
+        # Optimize the above solution
+        
+        smallestTwo = [float("inf")] * 2
+        largestThree = [float('-inf')] * 3
+        
+        for num in nums:
+            if num < smallestTwo[0]:
+                smallestTwo[0] = num
+                smallestTwo.sort(reverse = True)
+            if num > largestThree[0]:
+                largestThree[0] = num
+                largestThree.sort()
+        # smallestTwo[0] > smallestTwo[1]
+        # largestThree[0] < largestThree[1] < largestThree[2]
+        a = smallestTwo[1] * smallestTwo[0] * largestThree[2]
+        b = largestThree[2] * largestThree[1] * largestThree[0]
+        
+        return max(a, b)
+
+# Time: O(N)
+# Space: O(1)
