24a6fd1799b3e5e3b2e01b5aff8710c0c6f7d066
smrpb <89020961+smrpb@users.noreply.github.com>
2022-02-07 17:21:27 +0530

    Time: 68 ms (86.74%), Space: 13.9 MB (91.57%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -1,40 +1,24 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# https://www.youtube.com/watch?v=wgFPrzTjm7s
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        res = ListNode(-1)
-        node = res
-        a = l1; b = l2; carry = 0
-        while a or b or carry != 0:
-            if a and b:
-                v = a.val + b.val + carry
-                carry = v // 10
-                v = v % 10
-                node.next = ListNode(v)
-                node = node.next
-                a = a.next
-                b = b.next
-            elif a and not b:
-                v = a.val + carry
-                carry = v // 10
-                v = v % 10
-                node.next = ListNode(v)
-                node = node.next
-                a = a.next
-            elif not a and b:
-                v = b.val + carry
-                carry = v // 10
-                v = v % 10
-                node.next = ListNode(v)
-                node = node.next
-                b = b.next
-            elif not a and not b and carry != 0:
-                v = carry
-                carry = 0
-                node.next = ListNode(v)
-                node = node.next
+        dummy = ListNode()
+        cur = dummy
+        
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
             
-        return res.next
+            # new digit val
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)  # as in one place we have to put a single digit
+            
+            # update pointers
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return dummy.next
+        
