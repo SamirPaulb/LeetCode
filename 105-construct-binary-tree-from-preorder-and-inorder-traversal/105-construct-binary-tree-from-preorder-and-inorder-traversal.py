76de1a6b6f37a865289d73df88ed30b8dc4c3763
smrpb <89020961+smrpb@users.noreply.github.com>
2022-02-12 20:22:33 +0530

    Time: 74 ms (82.64%), Space: 18.8 MB (79.36%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,48 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-'''
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        self.pi = 0  # PreOrder Index
-        inInd = {}
-        for i, ch in enumerate(inorder):
-            inInd[ch] = i
+        n = len(inorder)
+        inorderIndx = {inorder[i] : i for i in range(n)}
+        self.pi = 0
         
         def solve(l, r):
             if l > r: return None
-            
-            root = TreeNode(preorder[self.pi])
+            v = preorder[self.pi]
             self.pi += 1
-            i = inInd[root.val]
-            
+            root = TreeNode(v)
+            i = inorderIndx[v]
             root.left = solve(l, i-1)
             root.right = solve(i+1, r)
-            
             return root
         
-        return solve(0, len(inorder)-1)
-    
-'''    
-    
-    
-class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        self.pi = 0  # PreOrder Index
-        inInd = {}
-        for i, ch in enumerate(inorder):
-            inInd[ch] = i
-        
-        def solve(preorder, inorder):
-            if not preorder or not inorder: return None
-            
-            root = TreeNode(preorder[0])
-            self.pi += 1
-            i = inorder.index(root.val)
-            
-            root.left = solve(preorder[1:i+1], inorder[:i])
-            root.right = solve(preorder[1+i:], inorder[i+1:])
-            
-            return root
-        
-        return solve(preorder, inorder)
+        return solve(0, n-1)
