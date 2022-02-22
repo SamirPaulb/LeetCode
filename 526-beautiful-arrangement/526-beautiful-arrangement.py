3724f2b3dbb516a92f61b315ff6aa9539187799b
smrpb <89020961+smrpb@users.noreply.github.com>
2022-02-22 18:42:08 +0530

    Time: 1500 ms (47.43%), Space: 13.8 MB (99.15%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.py
+++ 526-beautiful-arrangement/526-beautiful-arrangement.py
@@ -10,6 +10,6 @@ class Solution:
             for j, ch in enumerate(arr):
                 if ch % i == 0 or i % ch == 0:
                     solve(arr[:j] + arr[j+1:], i+1)
-        
+            
         solve(arr, 1)
         return self.res
