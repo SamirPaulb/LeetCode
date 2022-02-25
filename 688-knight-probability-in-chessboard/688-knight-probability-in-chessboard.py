d7452a26bd4de3c08c0d16c39fce3ecd6fdccbb1
smrpb <89020961+smrpb@users.noreply.github.com>
2022-02-26 00:46:48 +0530

    Time: 307 ms (77.81%), Space: 23.5 MB (35.77%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
@@ -1,25 +1,19 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        
-        move = [[2,1],[2,-1],[-2,1],[-2,-1],[1,-2],[1,2],[-1,-2],[-1,2]]  # all possible moves of knight in 8 directions.
-        
-        memo =  {}   # to store answer of each calls
-        
-        def dfs(r, c, k):
-            if (not 0 <= r < n) or (not 0 <= c < n):  # We're already outside the grid, so probability of staying inside is 0
-                return 0  
-            if k == 0:   # We're inside the grid and have no more moves to make
-                return 1  
-            
+        move = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]] 
+        memo = {}
+        def solve(r, c, k):
+            if not 0 <= r < n or not 0 <= c < n: return 0
+            if k == 0: return 1
             key = (r, c, k)
-            if key in memo:   # previously this call was made and calculated answer of this call is stored in memo
+            if key in memo:
                 return memo[key]
-            
             tmp = 0
-            for m in move:  # we make 8 possible moves and find the probability of staying inside after k - 1 more moves.
-                tmp += dfs(r+m[0], c+m[1], k-1)
-            
-            memo[key] = tmp / 8   # as each move is equally likely, we average all of these probabilities.
-            return memo[key]      # update memo to memozition to use this in future calls to avoid same calculation again.
+            for m in move:
+                i = r + m[0]
+                j = c + m[1]
+                tmp += solve(i,  j, k-1)
+            memo[key] = tmp / 8
+            return memo[key]
         
-        return dfs(row, column, k)
+        return solve(row, column, k)
