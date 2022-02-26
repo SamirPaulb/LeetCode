31f614cfaa4bf33af4f52e1b02205f73c3702497
smrpb <89020961+smrpb@users.noreply.github.com>
2022-02-27 02:11:05 +0530

    Time: 64 ms (93.39%), Space: 14.4 MB (80.16%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.py
+++ 841-keys-and-rooms/841-keys-and-rooms.py
@@ -1,18 +1,16 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        n = len(rooms)
-        visited = {i : False for i in range(n)}
-        visited[0] = True   # as room 0 is unlocked
+        visited = {i:False for i in range(len(rooms))}
+        visited[0] = True
         
-        q = [i for i in rooms[0]]
+        q = rooms[0]
         while q:
-            r = q.pop(0)
-            visited[r] = True
-            for key in rooms[r]:
-                if visited[key] == False:
-                    q.append(key)
-        
-        for i in range(n):
+            cur = q.pop()
+            if visited[cur] == True: continue
+            visited[cur] = True
+            q += rooms[cur]
+            
+        for i in visited:
             if visited[i] == False: return False
         
         return True
