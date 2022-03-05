ef449cef299bc63915fcfad8314d0b32291c25c7
smrpb <89020961+smrpb@users.noreply.github.com>
2022-03-05 17:57:45 +0530

    Time: 65 ms (66.17%), Space: 13.9 MB (91.80%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -1,15 +1,15 @@
 class Solution:
-    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        self.preIndex = 0
-        def dfs(postStart, postEnd):
-            if postStart > postEnd or self.preIndex >= len(preorder): return None
-            root = TreeNode(preorder[self.preIndex])
-            self.preIndex += 1
-            if postStart == postEnd or self.preIndex >= len(preorder): return root
-            postIndex = postorder.index(preorder[self.preIndex])
-            root.left = dfs(postStart, postIndex)
-            root.right = dfs(postIndex+1, postEnd-1)
-            
-            return root
+    def constructFromPrePost(self, preorder, postorder):
         
-        return dfs(0, len(preorder))
+        def dfs(preorder, postorder):
+            if postorder:
+                if len(postorder) == 1:
+                    return TreeNode(preorder.pop(0))
+                root = TreeNode(preorder.pop(0))
+                i = postorder.index(preorder[0])
+                root.left = dfs(preorder, postorder[:i+1])
+                root.right = dfs(preorder, postorder[i+1:-1])
+                return root
+        
+        return dfs(preorder, postorder)
+    
