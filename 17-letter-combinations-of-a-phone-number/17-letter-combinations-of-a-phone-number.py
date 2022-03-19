3812c95a90ad7fd5127a7368bad30991ea2176c8
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-03-19 21:13:30 +0530

    Time: 53 ms (27.19%), Space: 13.9 MB (45.90%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,38 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0:
-            return []
-        
-        d = {
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z']
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
         }
         
-        combinations = []
-        curr = [0] * len(digits)
-        letter_idx = [0] * len(digits)
-        i = 0
+        res = []
         
-        while True:
-            if letter_idx[i] < len(d[digits[i]]):
-                curr[i] = d[digits[i]][letter_idx[i]]
-                if i == len(digits) - 1:
-                    combinations.append(''.join(curr))
-                    letter_idx[i] += 1
-                else:
-                    i += 1
-            elif i > 0:
-                i -= 1
-                letter_idx[i] += 1
-                for j in range(i + 1, len(digits)):
-                    letter_idx[j] = 0
-            else:
-                break
-                
-        return combinations
+        def dfs(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            
+            for ch in digitToChar[digits[i]]:
+                dfs(i+1, curStr + ch)
+            
+        dfs(0, "")
+        return res if len(digits) != 0 else []
+        
