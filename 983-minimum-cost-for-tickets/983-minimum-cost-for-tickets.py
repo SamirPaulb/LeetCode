304a6615176da16590af447a1985f7e6924e18c2
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-04-04 16:22:06 +0530

    Time: 62 ms (60.41%), Space: 13.9 MB (92.94%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
@@ -1,18 +1,24 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        dp = [-1] * (max(days)+1)
-        days = set(days)
+        n = max(days) + 1
+        dp = [-1 for x in range(n)]
+        #dp = [-1]*(max(days)+1)
         dp[0] = 0
         
-        for i in range(1, len(dp)):
-            if i in days:
-                if i >= 30:
-                    dp[i] = min(dp[i-1] + costs[0], dp[i-7] + costs[1], dp[i-30] + costs[2])
-                elif 7 <= i < 30:
-                    dp[i] = min(dp[i-1] + costs[0], dp[i-7] + costs[1], costs[2])
+        for x in range(1,len(dp)):
+            if x in days:
+                if x>=30:
+                    #print(x,min(dp[x-1]+costs[0],dp[x-7]+costs[1],dp[x-30]+costs[2]))
+                    dp[x] = min(dp[x-1]+costs[0],dp[x-7]+costs[1],dp[x-30]+costs[2])
+                    
+                elif x>=7 and x<30:
+                    dp[x] = min(dp[x-1]+costs[0],dp[x-7]+costs[1],costs[2])
+                    
                 else:
-                    dp[i] = min(dp[i-1] + costs[0], costs[1], costs[2])
+                    dp[x] = min(dp[x-1]+costs[0],costs[1],costs[2])
+                    
             else:
-                dp[i] = dp[i-1]
-
+                dp[x] = dp[x-1]
+                
         return dp[max(days)]
+        
