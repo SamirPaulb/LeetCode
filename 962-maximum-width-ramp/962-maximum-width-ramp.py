6623175057e95193c906b08a3d0a8e4af2296fb5
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-04-20 11:36:34 +0530

    Time: 493 ms (44.45%), Space: 21.1 MB (39.43%) - LeetHub

--- 962-maximum-width-ramp/962-maximum-width-ramp.py
+++ 962-maximum-width-ramp/962-maximum-width-ramp.py
@@ -11,6 +11,9 @@ class Solution:
                     
         return res
         '''
+        
+        
+        '''
         # Method 1   # Time: O(N log(N))
         # Keep a decreasing stack.For each number, binary search the first smaller number in the stack. When the number is smaller the the last, push it into the stack.
         stack = []
@@ -21,5 +24,23 @@ class Solution:
             else:
                 j = stack[bisect.bisect(stack, [nums[i], i])][1]
                 res = max(res, j - i)
+        
+        return res
+        '''
+        
+        
+        # Method 2   # Time: O(N)
+        # make a stack in decreasing order
+        stack = []
+        res = 0
+        for i in range(len(nums)):
+            if not stack or nums[stack[-1]] > nums[i]:
+                stack.append(i)
+        
+        # Now traverse from end and check. 
+        # as we want max length so firstly popped element will give max length
+        for i in range(len(nums)-1, -1, -1):
+            while stack and nums[stack[-1]] <= nums[i]:
+                res = max(res, i - stack.pop())
         
         return res
