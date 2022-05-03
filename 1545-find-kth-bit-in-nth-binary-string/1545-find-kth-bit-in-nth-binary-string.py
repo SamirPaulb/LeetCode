6a89144973e77992576933d1210aab59fd385261
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-05-03 13:48:04 +0530

    Time: 35 ms (89.34%), Space: 13.8 MB (92.79%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,6 +1,10 @@
+# https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/
+
+
+''' 
+# Brute Force Solution
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        
         def revinv(s):
             s = list(s)
             for i in range(len(s)):
@@ -18,4 +22,17 @@ class Solution:
             dp[i] = dp[i-1] + '1' + revinv(dp[i-1])
         
         return dp[-1][k-1]
-        
+# Time: O(n^2)
+# Space: (n)
+''' 
+
+
+# Optimal Solution:
+# https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/discuss/785548/JavaC++Python-O(1)-Solutions
+
+class Solution:
+    def findKthBit(self, n, k):
+        return str(k // (k & -k) >> 1 & 1 ^ k & 1 ^ 1)
+
+# Time: O(1)
+# Space: O(1)
