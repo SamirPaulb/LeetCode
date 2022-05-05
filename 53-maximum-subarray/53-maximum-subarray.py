a84f381bf3c34c70b6fc9babb0812111aa82d7b1
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-05-05 15:40:08 +0530

    Time: 1290 ms (13.78%), Space: 28 MB (22.30%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # Kaden's Algorithms
-        cs = nums[0]
+        cs = 0
         ms = nums[0]
         
-        for i in nums[1:]:
-            cs += i
-            cs = max(cs, i)
+        for num in nums:
+            cs += num
+            cs = max(cs, num)
             ms = max(ms, cs)
         
         return ms
