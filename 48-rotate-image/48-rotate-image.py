33bc5642cca38767a4c091269b45bdffa234e849
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-05-06 07:52:31 +0530

    Time: 58 ms (29.10%), Space: 13.9 MB (30.67%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,17 +3,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # Transpose
         for i in range(len(matrix)):
             for j in range(i, len(matrix[0])):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
         for i in range(len(matrix)):
-            l = 0
-            r = len(matrix[0])-1
+            l = 0; r = len(matrix[0])-1
             while l < r:
-                matrix[i][l], matrix[i][r] = matrix[i][r], matrix[i][l]
+                matrix[i][l], matrix[i][r] = matrix[i][r], matrix[i][l] 
                 l += 1
                 r -= 1
-                
+        
         return matrix
