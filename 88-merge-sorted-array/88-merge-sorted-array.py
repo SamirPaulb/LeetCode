545b54301d171f5a6945fe29e27354b1012f3faf
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-05-06 09:46:25 +0530

    Time: 36 ms (92.39%), Space: 14 MB (6.91%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        
+        while n > 0:
+            nums1[n-1] = nums2[n-1]
+            n -= 1
+        
+        return nums1
+
+# Time: O(N)
+# Space: O(1)
