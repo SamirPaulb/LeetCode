4458fbfec0a9c2f9bd1ac0ec66dcfba72ad03206
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-05-12 20:31:25 +0530

    Time: 193 ms (68.66%), Space: 17.5 MB (36.15%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from collections import deque
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        if endWord not in wordList or not endWord or not beginWord or not wordList:
+            return 0
+        L = len(beginWord)
+        all_combo_dict = defaultdict(list)
+        for word in wordList:
+            for i in range(L):
+                all_combo_dict[word[:i] + "*" + word[i+1:]].append(word) 
+        queue = deque([(beginWord, 1)])
+        visited = set()
+        visited.add(beginWord)
+        while queue:
+            current_word, level = queue.popleft()
+            for i in range(L):
+                intermediate_word = current_word[:i] + "*" + current_word[i+1:]
+                for word in all_combo_dict[intermediate_word]:
+                    if word == endWord:
+                        return level + 1
+                    if word not in visited:
+                        visited.add(word)
+                        queue.append((word, level + 1))
+        return 0
