3b3c3bd54681af6ed76359ee612581367de475d9
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-05-15 19:24:59 +0530

    Time: 90 ms (10.56%), Space: 14.1 MB (35.71%) - LeetHub

--- 781-rabbits-in-forest/781-rabbits-in-forest.py
+++ 781-rabbits-in-forest/781-rabbits-in-forest.py
@@ -1,3 +1,13 @@
+# https://leetcode.com/problems/rabbits-in-forest/
+# https://youtu.be/9mEUIdP4ytw
+'''
+If answers[i] == v then the current rabbit must be from a group of (v+1) rabits.
+if there are more than (v+1) this coloured rabit then there is a another (v+1)
+number of rabit of same group.
+
+So find count of same coloured rabits.
+calculate no. of groups and extra group possible.
+'''
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
         dic = {}
@@ -16,3 +26,7 @@ class Solution:
                 res += key
         
         return res
+    
+    
+# Time: O(N)
+# Space: O(N)
