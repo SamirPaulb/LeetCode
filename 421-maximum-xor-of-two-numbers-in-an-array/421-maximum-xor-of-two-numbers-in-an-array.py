64773aa92cee321a8e18c386d7adbc197db3cd7f
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-05-24 20:35:16 +0530

    Time: 8734 ms (5.01%), Space: 142.6 MB (19.80%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
@@ -1,51 +1,44 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.fin_val = -1
-    
-    
+        self.val = -1  # used to store the value of number at the end
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-        
-        
-    def insert(self, val):
-        ptr = self.root
-        for x in range(31, -1, -1):
-            digit = 1 if (val & 1 << x) else 0
-            if digit not in ptr.children:
-                ptr.children[digit] = TrieNode()
-            ptr = ptr.children[digit]
-        ptr.fin_val = val
+    
+    def addNum(self, num):
+        cur = self.root
+        for i in range(31, -1, -1):
+            bit = 1 if num & (1 << i) else 0
+            if bit not in cur.children:
+                cur.children[bit] = TrieNode()
+            cur = cur.children[bit]
+        cur.val = num
+                
 
-
-    def max_xor(self, A):
-        g_max = 0
-        for x in range(len(A)):
-            ptr = self.root
-            curr = A[x]
-            for y in range(31, -1, -1):
-                digit = 1 if (curr & 1 << y) else 0
-                if digit == 0:
-                    if 1 in ptr.children:
-                        ptr = ptr.children[1]
-                    else:
-                        ptr = ptr.children[0]
-                else:
-                    if 0 in ptr.children:
-                        ptr = ptr.children[0]
-                    else:
-                        ptr = ptr.children[1]
-            g_max = max(g_max, ptr.fin_val ^ curr)
-        return g_max
-            
-        
-        
 class Solution:
-    # @param A : list of integers
-    # @return an integer
-    def findMaximumXOR(self, A):
-        t = Trie()
-        for x in A:
-            t.insert(x)
-        return t.max_xor(A)
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        trie = Trie()
+        for num in nums:
+            trie.addNum(num)
+        
+        res = 0
+        for num in nums:
+            cur = trie.root
+            for i in range(31, -1, -1):
+                bit = 1 if num & (1 << i) else 0
+                if bit == 1:  
+                    if 0 in cur.children:
+                        cur = cur.children[0]
+                    else:
+                        cur = cur.children[1]
+                else:     # bit == 0
+                    if 1 in cur.children:
+                        cur = cur.children[1]
+                    else:
+                        cur = cur.children[0]
+                        
+            res = max(res, cur.val ^ num)
+        
+        return res
