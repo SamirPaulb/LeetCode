3513445643acca8e662d59a69f37ebd83e7d297a
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-05-26 23:06:07 +0530

    Time: 18927 ms (5.21%), Space: 77.3 MB (63.67%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -18,23 +18,18 @@ class WordDictionary:
         cur.endOfWord = True
 
     def search(self, word: str) -> bool:
-        self.res = False
-        self.dfs(self.root, word)
-        return self.res
-                          
+        return self.dfs(self.root, word)
+        
     def dfs(self, node, word):
         if not word:
-            if node.endOfWord:
-                self.res = True
-            return
-        
-        if word[0] == '.':
-            for childNode in node.children.values():
-                self.dfs(childNode, word[1:])
-            return
-                
-        else:
-            if word[0] in node.children:
-                self.dfs(node.children[word[0]], word[1:])
-                
-            return
+            return node.endOfWord
+        for i in range(len(word)):
+            if word[i] == '.':
+                for child in node.children.values():
+                    if child and self.dfs(child, word[i+1:]): return True
+                else: return False
+            if word[i] not in node.children:
+                return False
+            else:
+                node = node.children[word[i]]
+        return node.endOfWord
