e56b890d54d5e4b4dedb9f4b385c76483ae00489
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-05-26 12:31:34 +0530

    Time: 282 ms (17.29%), Space: 15.3 MB (30.90%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
@@ -2,7 +2,7 @@ class TrieNode:
     def __init__(self):
         self.children = {}
         self.endOfWord = False
-        self.val = ''
+        self.val = ''           # to define the value of end node as word
 
 class Trie:
     def __init__(self):
@@ -15,25 +15,25 @@ class Trie:
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
         cur.endOfWord = True
-        cur.val = word
+        cur.val = word     # definging the end value as whole word
     
-    def bfs(self):
+    def bfs(self):         # as I am starting from the root itself, so bfs have only argument self for referring the root
         q = [self.root]
         res = ''
         while q:
             cur = q.pop(0)
-            for child in cur.children.values():
-                if child.endOfWord:
+            for child in cur.children.values():     # traversing all the nodes of cur not keys
+                if child.endOfWord:                 # ONLY go to the node which is of length 1 and end of a word also
                     q.append(child)
-                    if len(child.val) > len(res):
+                    if len(child.val) > len(res):   # for greater length as for lexicografical I have already sorted words 
                         res = child.val
         return res
         
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        words.sort()
-        trie = Trie()
-        for word in words:
+        words.sort()        # sort in lexicografical order
+        trie = Trie()       # making object of the Trie Class
+        for word in words:  # adding all words to trie structre
             trie.addWord(word)
         
-        return trie.bfs()
+        return trie.bfs()   # calling the bfs function 
