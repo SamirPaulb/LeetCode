db63fb77682c883211106a8342f7f63b304d63b2
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-05-26 17:46:56 +0530

    Time: 193 ms (43.00%), Space: 20 MB (91.92%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.py
+++ 386-lexicographical-numbers/386-lexicographical-numbers.py
@@ -1,21 +1,30 @@
+# https://leetcode.com/problems/lexicographical-numbers/
+''' 
+Input: n = 13
+Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+
+Basice idea: think about Trie. we append digits to the specific suffix until we reach the target number.
+'''
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        
-        def helper(start):
-            ans = []
-            for aux in range(10):
-                newStart = start * 10 + aux
-                if newStart > n: 
-                    break
-                ans += [newStart]
-                ans += helper(newStart)
-            return ans
-        
-        if n < 10:
-            return list([i for i in range(1, n+1)])
-        
+        # recursively use Trie
         res = []
+        if n < 10:
+            return list(range(1, n+1))
         for i in range(1, 10):
             res += [i]
-            res += helper(i)
+            res += self.helper(i, n)
         return res
+    
+    def helper(self, start, n):
+        res = []
+        for aux in range(10):
+            newStart = start*10+aux
+            if newStart > n:
+                break
+            res += [newStart]
+            res += self.helper(newStart, n)
+        return res
+    
+# Time: O(n)
+# Space: O(1)
