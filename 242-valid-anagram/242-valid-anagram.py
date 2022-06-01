2c015d5df374b7a810c4e0fc6b606c51290b3433
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-06-01 11:27:15 +0530

    Time: 88 ms (23.96%), Space: 14.5 MB (34.84%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,11 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dicS = {chr(i):0 for i in range(97, 123)}
+        dicS = {chr(i) : 0 for i in range(97, 123)}   # creating dictionary key as 'a' to 'z' and value as frequency of charecters in s
         for i in s:
-            dicS[i] += 1
+            dicS[i] += 1                              # increasing the count of current charecter i
         
-        dicT = {chr(i):0 for i in range(97, 123)}
+        dicT = {chr(i) : 0 for i in range(97, 123)}   # creating dictionary key as 'a' to 'z' and value as frequency of charecters in t
         for i in t:
-            dicT[i] += 1
+            dicT[i] += 1                              # increasing the count of current charecter i
         
-        return dicS == dicT
+        return dicS == dicT                           # frequency of charecters in s equal to frequency of charenters in t
+		
+# Time: O(N)   ; as traversing only once
+# Space: O(N)  ; for creating the dictionaries
