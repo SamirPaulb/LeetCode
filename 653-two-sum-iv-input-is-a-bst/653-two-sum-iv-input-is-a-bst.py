fff4c388f69e46a42dc032ca1603ae9e03764bca
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-06-04 00:58:31 +0530

    Time: 99 ms (67.63%), Space: 20.8 MB (6.03%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -6,24 +6,16 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        arr = []
+        visited = set()
+        self.res = False
+        
         def inorder(root):
             if not root: return
             inorder(root.left)
-            arr.append(root.val)
+            rem = k - root.val
+            if rem in visited: self.res = True
+            visited.add(root.val)
             inorder(root.right)
         
         inorder(root)
-        
-        l = 0
-        r = len(arr)-1
-        while l < r:
-            twoSum = arr[l] + arr[r]
-            if twoSum < k:
-                l += 1
-            elif twoSum > k:
-                r -= 1
-            else:
-                return True
-            
-        return False
+        return self.res
