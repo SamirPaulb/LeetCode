5e265fbebc6f382323dc4b02531706b8b99c8a82
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-06-07 19:33:35 +0530

    Time: 123 ms (38.79%), Space: 21.4 MB (87.03%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.py
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.py
@@ -1,5 +1,14 @@
 # https://leetcode.com/problems/count-complete-tree-nodes/
 # https://youtu.be/u-yWemKGWO0
+''' 
+Only traversing along the left and right boundary. 
+If both left and right height are equal then
+the bottom level would be full from left to right and total no. of nodes in that subtree
+is 2^h - 1. 
+
+If left and right height are not equal then add +1 for current root and go to left child 
+and right child. 
+'''
 
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
