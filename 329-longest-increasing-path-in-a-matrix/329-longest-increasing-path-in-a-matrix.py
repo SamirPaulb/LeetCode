073830f1a3149637d1fc9c0fdf97077dfa18a0cf
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-06-18 12:45:23 +0530

    Time: 987 ms (10.98%), Space: 19.4 MB (15.87%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -2,9 +2,7 @@ class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         row = len(matrix); col = len(matrix[0])
         res = 0
-        
         memo = {}
-        
         directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
         
         def helper(i, j):
@@ -13,7 +11,9 @@ class Solution:
             for move in directions:
                 r = i + move[0]
                 c = j + move[1]
-                if 0<=r<row and 0<=c<col and matrix[r][c] != '#' and matrix[r][c] > matrix[i][j]:
+                if (0<=r<row and 0<=c<col and
+                    matrix[r][c] != '#' and matrix[r][c] > matrix[i][j]):
+                    
                     tmp = matrix[i][j]
                     matrix[i][j] = '#'
                     path = max(path, helper(r, c))
