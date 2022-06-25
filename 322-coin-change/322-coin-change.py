46e421747bf116426cfac1066eb58954ddd20000
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-06-26 01:11:16 +0530

    Time: 1889 ms (61.70%), Space: 16.3 MB (26.20%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,21 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        row = len(coins); col = amount + 1
-        dp = [[2**31]*col for i in range(row)]
+        dp = [[2**31]*(amount+1) for i in range(len(coins)+1)]
         
-        for j in range(col):
-            if j % coins[0] == 0:
-                dp[0][j] = j // coins[0]
-        
-        for i in range(row):
+        for i in range(len(coins)+1):
             dp[i][0] = 0
         
-        for i in range(1, row):
-            for j in range(1, col):
-                if j < coins[i]:
-                    dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = min(1 + dp[i][j - coins[i]], dp[i-1][j])
+        for j in range(amount+1):
+            if j % coins[0] == 0:
+                dp[1][j] = j // coins[0]
         
-        res = dp[-1][-1]
-        return res if res != 2**31 else -1
+        for i in range(2, len(coins)+1):
+            for j in range(1, amount+1):
+                if coins[i-1] <= j:
+                    dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j])
+                else:
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[-1][-1] if dp[-1][-1] != 2**31 else -1
