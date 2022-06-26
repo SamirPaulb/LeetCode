ec5f09a1d407f3414b5bacc2b21ff83ceac086dc
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-06-26 14:22:44 +0530

    Time: 1988 ms (100.00%), Space: 32.3 MB (100.00%) - LeetHub

---
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumsSplicedArray(self, A, B):
+        def kadane(A, B):
+            # calculate how much B can earn by swaping a part of the array.
+            res = cur = 0
+            for i in range(len(A)):
+                # Once the element in A make the cur negative, reset cur to zero; else, update cur.
+                cur = max(0, cur + A[i] - B[i])
+                res = max(res, cur)
+            # return the sum after swapping a part of the array.
+            return res + sum(B)
+        
+        # We are not sure which array is bigger so that we have to do kadane() twice.
+        return max(kadane(A, B), kadane(B, A))
