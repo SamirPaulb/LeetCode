d17ec36f91bfd936803e057d2f2f7d246930f23f
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-06-28 18:47:53 +0530

    Time: 172 ms (35.91%), Space: 15.8 MB (53.89%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,41 +1,16 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        r = len(grid)
-        c = len(grid[0])
+        for i in range(len(grid)):
+            if i > 0: 
+                grid[i][0] += grid[i-1][0]
         
-        for i in range(r):
-            for j in range(c):
-                if i == j == 0: continue
-                    
-                elif i == 0: grid[i][j] += grid[i][j-1]
-                    
-                elif j == 0: grid[i][j] += grid[i-1][j]
-                    
-                else: grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        for j in range(len(grid[0])):
+            if j > 0:
+                grid[0][j] += grid[0][j-1]
+        
+        for i in range(1, len(grid)):
+            for j in range(1, len(grid[0])):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
         
         return grid[-1][-1]
     
-    
-    
-    
-    
-    
-    
-class Solution:
-    def minPathSum(self, grid):
-        # Bottom-Right to Top-Left Traversal
-
-        r = len(grid);  c = len(grid[0])
-        for i in range(r-1, -1, -1):
-            for j in range(c-1, -1, -1):
-                
-                if i == r-1 and j < c-1:
-                    grid[i][j] += grid[i][j+1]
-                    
-                elif i < r-1 and j == c-1:
-                    grid[i][j] += grid[i+1][j]
-                    
-                elif i < r-1 and j < c-1:
-                    grid[i][j] += min(grid[i+1][j], grid[i][j+1])
-        
-        return grid[0][0]
