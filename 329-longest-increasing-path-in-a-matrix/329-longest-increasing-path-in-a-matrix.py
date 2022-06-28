9a4215f6e8d5c41ec4c58669d5aed0e127751f6e
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-06-28 22:28:54 +0530

    Time: 651 ms (47.81%), Space: 19.4 MB (15.94%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,29 +1,26 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        row = len(matrix); col = len(matrix[0])
-        res = 0
+        row = len(matrix)
+        col = len(matrix[0])
         memo = {}
-        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-        
-        def helper(i, j):
+        move = [(-1,0), (1,0), (0,-1), (0,1)]
+
+        def dfs(i, j):
             if (i,j) in memo: return memo[(i,j)]
-            path = 1
-            for move in directions:
-                r = i + move[0]
-                c = j + move[1]
-                if (0<=r<row and 0<=c<col and
-                    matrix[r][c] != '#' and matrix[r][c] > matrix[i][j]):
-                    
-                    tmp = matrix[i][j]
+            ans = 1
+            tmp = matrix[i][j]
+            for m in move:
+                r = i + m[0]
+                c = j + m[1]
+                if 0<=r<row and 0<=c<col and matrix[r][c] != '#' and matrix[r][c] > matrix[i][j]: 
                     matrix[i][j] = '#'
-                    path = max(path, helper(r, c) + 1)
+                    ans = max(ans, 1 + dfs(r,c))
                     matrix[i][j] = tmp
-                    
-            memo[(i,j)] = path 
-            return memo[(i,j)]
+            memo[(i,j)] = ans
+            return ans 
         
+        res = 0
         for i in range(row):
             for j in range(col):
-                res = max(res, helper(i, j))
-    
+                res = max(res, dfs(i,j))
         return res
