e92c16bb41312091a95348d2129db5f65ddf43ac
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-06-29 11:22:52 +0530

    Time: 163 ms (84.06%), Space: 16.6 MB (85.51%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,36 +1,20 @@
-'''
-class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        memo = {}
-        
-        def dfs(i, j):
-            if i == 0 or j == 0: return j or i
-                        
-            if (i,j) in memo:
-                return memo[(i,j)]
-            
-            if word1[i-1] == word2[j-1]:
-                ans = dfs(i-1, j-1)
-            else: 
-                ans = 1 + min(dfs(i, j-1), dfs(i-1, j), dfs(i-1, j-1))
-                
-            memo[(i,j)] = ans
-            return memo[(i,j)]
-        
-        return dfs(len(word1), len(word2))
-'''
-
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         n1 = len(word1)
         n2 = len(word2)
-        dp = [[0]*(n2+1) for i in range(n1+1)]
+        self.res = 0
+        dp = [[-1]*(n2+1) for i in range(n1+1)]
         
-        for i in range(n1+1):
-            for j in range(n2+1):
-                if i == 0: dp[i][j] = j
-                elif j == 0: dp[i][j] = i
-                elif word1[i-1] == word2[j-1]: dp[i][j] = dp[i-1][j-1]
-                else: dp[i][j] = 1 + min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
+        def solve(i, j):
+            if i == j == 0: return 0
+            if i <= 0 or j <= 0: return j or i
+            if dp[i][j] != -1: return dp[i][j]
+            if word1[i-1] == word2[j-1]:
+                ans = solve(i-1, j-1)
+            else:
+                ans = 1 + min(solve(i, j-1), solve(i-1, j-1), solve(i-1, j))
+
+            dp[i][j] = ans
+            return ans
         
-        return dp[-1][-1]
+        return solve(n1, n2)
