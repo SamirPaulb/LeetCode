f460d2680c3d01b7c468697d61fa2af7740a8629
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-07-04 00:32:01 +0530

    Time: 1607 ms (20.52%), Space: 76.6 MB (23.07%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,52 +1,50 @@
+# https://youtu.be/7ABFKPK2hD4
+
 class ListNode:
-    def __init__(self, key, value):
+    def __init__(self, key, val):
         self.key = key
-        self.val = value
+        self.val = val
         self.next = None
         self.prev = None
-    
+
+
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.dic = {}
-        self.head = None
-        self.tail = None
+        self.cache = {}
+        self.left = ListNode(0, 0) 
+        self.right = ListNode(0, 0)
+        self.left.next = self.right
+        self.right.prev = self.left    
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev   
+    
+    def insert(self, node):
+        self.right.prev.next = node
+        node.prev = self.right.prev
+        node.next = self.right
+        self.right.prev = node
 
     def get(self, key: int) -> int:
-        if key in self.dic:
-            self.remove(self.dic[key])
-            self.makeRecent(self.dic[key])
-            return self.dic[key].val
+        if key not in self.cache: return -1
         else:
-            return -1
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val
         
     def put(self, key: int, value: int) -> None:
-        if key in self.dic:
-            self.remove(self.dic[key])
-            self.dic[key] = ListNode(key, value)
-            self.makeRecent(self.dic[key])
-        else:
-            if len(self.dic) == self.capacity:
-                del self.dic[self.head.key]
-                self.remove(self.head)
-            self.dic[key] = ListNode(key, value)
-            self.makeRecent(self.dic[key])
+        if key in self.cache:
+            self.remove(self.cache[key])
+        newNode = ListNode(key, value)
+        self.insert(newNode)
+        self.cache[key] = newNode
+            
+        if len(self.cache) > self.capacity:
+            lruNode = self.left.next
+            self.remove(lruNode)
+            del self.cache[lruNode.key]
         
-    def remove(self, node):
-        if self.head.key == node.key:
-            self.head = self.head.next
-        elif self.tail and self.tail.key == node.key:
-            self.tail = self.tail.prev
-        else:
-            node.prev.next = node.next
-            node.next.prev = node.prev
-        
-    def makeRecent(self, node):
-        if not self.head:
-            self.head = node
-            self.tail = node
-        else:
-            node.prev = self.tail
-            node.next = None
-            self.tail.next = node
-            self.tail = node
