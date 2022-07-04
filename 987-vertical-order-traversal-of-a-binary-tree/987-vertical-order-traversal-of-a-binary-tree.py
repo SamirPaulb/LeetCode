a162b5236ff0aa99eadcacd9c202b53ab7d11d4b
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-07-04 16:35:29 +0530

    Time: 47 ms (62.13%), Space: 14.1 MB (69.66%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -1,26 +1,22 @@
-# If some nodes of same horizontal level comes in same vertical level then
-# we should only sort those nodes.
+
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        self.dic = {}
-        def solve(root, d, l):
+        rootDict = {}
+        
+        def solve(root, l, d):
             if not root: return
-            
-            if d not in self.dic:
-                self.dic[d] = [(l, root.val)]
+            if d not in rootDict: 
+                rootDict[d] = [(l, root.val)]
             else:
-                self.dic[d].append((l, root.val))
-                
-            solve(root.left, d-1, l+1)
-            solve(root.right, d+1, l+1)
+                rootDict[d].append((l, root.val))
+            solve(root.left, l+1, d-1)
+            solve(root.right, l+1, d+1)
         
         solve(root, 0, 0)
-        keys = sorted(list(self.dic.keys()))
+        keys = sorted(list(rootDict.keys()))
         
         res = []
         for key in keys:
-            values = self.dic[key]
-            values.sort()  # sorting is done based on level then for same level nodes based on values
-            res.append([r[1] for r in values])
-            
+            arr = sorted(rootDict[key])
+            res.append([r[1] for r in arr])
         return res
