17acdb2e345201555c31130fd6ce99fdfb1990c4
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-07-05 02:04:34 +0530

    Time: 188 ms (71.61%), Space: 15.3 MB (51.01%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
@@ -1,26 +1,25 @@
-'''
-Traverse from bottom to top (Postorder Traversal) and keep track of the distance of the leaf nodes to each node. Once those leaf nodes meet a Lowest Common Ancestor, we can immediately check whether they are good pairs.
-'''
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def countPairs(self, root: TreeNode, distance: int) -> int:
         count = 0
-        
-        def dfs(node):
+        def solve(root):
             nonlocal count
-            
-            if not node: return []
-            if not node.left and not node.right: return [1]
-            
-            left = dfs(node.left)
-            right = dfs(node.right)
-            
-            count += sum(l + r <= distance for l in left for r in right)
-            return [n + 1 for n in left + right if n + 1 < distance]
-        
-        dfs(root)
+            if not root: return []
+            if not root.left and not root.right: return [1]
+            left = solve(root.left)
+            right = solve(root.right)
+            for i in left:
+                for j in right:
+                    if i + j <= distance: count += 1
+            arr = []
+            for i in (left + right):
+                if 1 + i < distance:
+                    arr.append(1 + i)
+            return arr
+        solve(root)
         return count
-    
-# Time: O(N)
-# Space: O(N)
-    
