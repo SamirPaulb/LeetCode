578f352f7edb6345ea617838b5676c3cc7c41f63
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-07-04 23:31:56 +0530

    Time: 144 ms (30.44%), Space: 21.4 MB (86.41%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.py
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.py
@@ -1,43 +1,22 @@
-# https://leetcode.com/problems/count-complete-tree-nodes/
-# https://youtu.be/u-yWemKGWO0
-''' 
-Only traversing along the left and right boundary. 
-If both left and right height are equal then
-the bottom level would be full from left to right and total no. of nodes in that subtree
-is 2^h - 1. 
-
-If left and right height are not equal then add +1 for current root and go to left child 
-and right child. 
-'''
-
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        if not root: return 0
-        
-        leftHeight = self.getLeftHeight(root)
-        rightHeight = self.getRightHeight(root)
-        
-        if leftHeight == rightHeight: 
-            return 2 ** leftHeight - 1
-        else:
-            return 1 + self.countNodes(root.left) + self.countNodes(root.right)         
-        
+        lh = self.leftHeight(root)
+        rh = self.rightHeight(root)
+        if lh == rh:
+            return 2 ** lh - 1
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
     
-    def getLeftHeight(self, node):
+    
+    def leftHeight(self, root):
         height = 0
-        while node:
+        while root:
             height += 1
-            node = node.left
+            root = root.left
         return height
     
-    def getRightHeight(self, node):
+    def rightHeight(self, root):
         height = 0
-        while node:
+        while root:
             height += 1
-            node = node.right
+            root = root.right
         return height
-    
-# Height = H = log(n) where n = total number of nodes
-# Time: O(H * H) = O(log(n)^2) = O(Log^2 n)
-# Auxiliary Space: O(H) = O(log(n))
-    
