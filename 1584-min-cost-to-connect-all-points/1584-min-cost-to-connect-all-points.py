46cd9ab66638480428687a79575c7db7b4293db6
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-07-05 22:52:32 +0530

    Time: 8284 ms (5.01%), Space: 281 MB (5.00%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,39 +1,25 @@
-# https://leetcode.com/problems/min-cost-to-connect-all-points/
-# https://youtu.be/f7JOBJIC-NA
-
-import heapq
 class Solution:
-    def minCostConnectPoints(self, points):
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-        # for simplicity, marking the points with a node value from 0 to (n-1)
-        adjList = {i:[] for i in range(n)}  # key = curr node, value = (dist, other nodes)
+        adjList = {i:[] for i in range(n)}
         for i in range(n):
             x1 = points[i][0]; y1 = points[i][1]
-            for j in range(i+1, n):
+            for j in range(n):
                 x2 = points[j][0]; y2 = points[j][1]
-                dist = abs(x1 - x2) + abs(y1 - y2)
-                # as it is a undirected graph, we need to add both i and j to adjList
+                dist = abs(x2 - x1) + abs(y2 - y1)
                 adjList[i].append((dist, j))
                 adjList[j].append((dist, i))
         
-        # Prim's Algorithm - Minimum Spanning Tree (MST) 
-        # (In the Dijkstra's Algorithm if we introduce a visited data structure to 
-        # stop cycle in graph then it converts to Prim's Algorithm)
-        visited = set()
         res = 0
-        minHeap = [(0, 0)]  # (dist, node)
-        while len(visited) < n: # until all nodes a visited
-            dist, node = heapq.heappop(minHeap)
-            if node in visited: continue
-            visited.add(node)
+        visited = set()
+        minHeap = [(0, 0)]
+        while len(visited) < n:
+            dist, point = heapq.heappop(minHeap)
+            if point in visited: continue
+            visited.add(point)
             res += dist
-            for dn in adjList[node]:  # dn = (dist, node)
-                if dn[1] not in visited:
-                    heapq.heappush(minHeap, dn)
+            for d, p in adjList[point]:
+                if p not in visited:
+                    heapq.heappush(minHeap, (d, p))
         
         return res
-
-
-# V = number of umber of verices and E = numver of Edges in the graph.
-# Time: O(V^2) + O((V+E)logV) = O(V^2)
-# Space: O(V^2)  In worst cases all connections of undirected graph can be in minheap 
