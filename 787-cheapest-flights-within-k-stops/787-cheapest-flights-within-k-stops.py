8d079f0fef4e3d3bf85169fc035abebd71313f53
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-07-05 18:03:48 +0530

    Time: 211 ms (58.66%), Space: 15.6 MB (15.01%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,31 +1,18 @@
-# https://leetcode.com/problems/cheapest-flights-within-k-stops/
-
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        #Make graph
-        adj_list = {i:[] for i in range(n)}
-        for frm, to, price in flights:
-            adj_list[frm].append((to, price))
+        adjList = {i:[] for i in range(n)}
+        for u, v, w in flights:
+            adjList[u].append((w, v))
+        min_stops = [2**31] * n
+        heap = [(0, src, -1)]
+        while heap:
+            w, v, t = heapq.heappop(heap)
+            if min_stops[v] < t: continue
+            if t > k: continue
+            if v == dst and t <= k: return w
+            min_stops[v] = t
+            for w1, v1 in adjList[v]:
+                if t+1 <= k:
+                    heapq.heappush(heap, (w+w1, v1, t+1))
         
-        best_visited = [2**31]*n # Initialized to maximum
-        
-        prior_queue = [ (0, -1, src) ]  # weight, steps, node
-
-        while prior_queue:
-            cost, steps, node = heapq.heappop(prior_queue)
-            
-            if best_visited[node] <= steps:  # Have seen the node already, and the current steps are more than last time
-                continue
-
-            if steps > k:  # More than k stops, invalid
-                continue
-
-            if node==dst:  # reach the destination # as priority_queue is a minHeap so this cost is the most minimum cost.
-                return cost
-            
-            best_visited[node] = steps # Update steps
-
-            for neighb, weight in adj_list[node]:
-                heapq.heappush(prior_queue, (cost + weight, steps + 1, neighb))
-
         return -1
