56b48d6dd090ed6593cd38b20fd0690a610e3b71
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-07-15 01:27:53 +0530

    Time: 53 ms (36.55%), Space: 13.9 MB (81.09%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -3,10 +3,10 @@ class Solution:
         res = 0
         while a or b or c:
             if (a & 1) | (b & 1) != (c & 1):
-                if (c & 1) == 1:
-                    res += 1
-                else:
+                if (c & 1) == 1:  # (a & 1) | (b & 1) should be == 1 ; so changing any of a, b we can get 1
+                    res += 1      
+                else:             # (a & 1) | (b & 1) should be == 0 ; is (a & 1) == 1 and (b & 1) == 1 we need to change both to 0 so res += 1; if any of them is 1 then change only 1 i.e. res += 1
                     res += (a & 1) + (b & 1) 
-            a, b, c = a>>1, b>>1, c>>1
+            a, b, c = a>>1, b>>1, c>>1   # left-shift by 1
         
         return res
