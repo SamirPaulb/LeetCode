6398de70eb4215b341e737a5a6648298e4fe406b
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-07-18 18:23:12 +0530

    Time: 87 ms (41.85%), Space: 14 MB (47.08%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,36 +1,29 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = []
-        countOfFresh = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        row = len(grid); col = len(grid[0])
+        q = collections.deque()
+        fresh_count = 0
+        for i in range(row):
+            for j in range(col):
                 if grid[i][j] == 2:
-                    q.append((i, j))
-                if grid[i][j] == 1:
-                    countOfFresh += 1
-                    
-        if countOfFresh == 0: return 0                                       
-        move = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+                    q.append((i,j))
+                if grid[i][j] == 1: fresh_count += 1
         
-        res = 0
+        if fresh_count == 0: return 0
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        print(q)
+        time = 0
         while q:
-            res += 1
             n = len(q)
-            for i in range(n):
-                r, c = q.pop(0)
-                for m in move:
-                    nr = r + m[0]
-                    nc = c + m[1]
-                    
-                    if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2
-                        q.append((nr, nc))
+            time += 1
+            for _ in range(n):
+                i, j = q.popleft()
+                for move in directions:
+                    r = i + move[0]; c = j + move[1]
+                    if 0 <= r < row and 0 <= c < col and grid[r][c] == 1:
+                        grid[r][c] = 2
+                        q.append((r, c))
                         
-        countOfFresh = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    countOfFresh += 1
-                    
-        if countOfFresh == 0: return res-1
-        return -1
+        for row in grid:
+            if 1 in row: return -1
+        return time - 1
