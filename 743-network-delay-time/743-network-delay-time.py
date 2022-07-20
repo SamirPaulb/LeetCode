e0e899ebd9b6079a082b64807b20f08aea9c07a6
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-07-20 12:35:00 +0530

    Time: 873 ms (21.99%), Space: 16.1 MB (57.31%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,30 +1,19 @@
-# Implement Dijkstra's Shortest Path Algorithm
-
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # create Adjacency List 
-        adjList = {i:[] for i in range(1, n+1)}
+        adjList = {i:[] for i in range(n+1)}
         for u, v, w in times:
             adjList[u].append((v, w))
         
-        minHeap = [(0, k)]  # taking min-heap so that pop() will give node with lowest distance
-        t = 0   # total time require
-        visited = set()  # to keep a track of visited nodes
-        
+        res = 0
+        visited = set()
+        minHeap = [(0, k)]
         while minHeap:
-            w1, n1 = heapq.heappop(minHeap)  # w1 = time to reach from source k(weight); n1 = value of node
-            if n1 in visited: continue
-            visited.add(n1)
-            t = max(t, w1)  # as w1 is the total time to reach to n1 from k
-            for n2, w2 in adjList[n1]:
-                if n2 not in visited:
-                    heapq.heappush(minHeap, (w1 + w2, n2)) # added current time(w1) + time between n1 and n2
+            time, node = heapq.heappop(minHeap)
+            if node in visited: continue
+            visited.add(node)
+            res = max(res, time)
+            for v, w in adjList[node]:
+                if v not in visited:
+                    heapq.heappush(minHeap, (time + w, v))
         
-        # if we can visit all nodes then only return total time t
-        return t if len(visited) == n else -1 
-
-# number of edges, e = len(times)
-# n = total no. of nodes
-
-# Time: O(e * log(n)) 
-# Space: O(n)
+        return res if len(visited) == n else -1
