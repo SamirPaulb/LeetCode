5ed5a9de135cc470edffd55fce99551274bfa789
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-07-31 23:16:34 +0530

    Time: 135 ms (16.95%), Space: 14 MB (29.16%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.py
+++ 686-repeated-string-match/686-repeated-string-match.py
@@ -1,25 +1,6 @@
-class Solution:
-    def repeatedStringMatch(self, a: str, b: str) -> int:
-        n = len(b) // len(a)
-        if self.subStr(a*n, b): return n
-        if self.subStr(a*(n+1), b): return n+1
-        if self.subStr(a*(n+2), b): return n+2    
-        return -1
-    
-    ''' 
-    def subStr(self, a, b):
-        return b in a
-    '''
-    def subStr(self, a, b):  # check if b is substring of a or not
-        for i in range(len(a)):
-            if a[i] == b[0] and i+len(b) <= len(a) and a[i:i+len(b)] == b:
-                return True
-        return False
+# https://leetcode.com/problems/repeated-string-match/
 
-
-    
-
-'''
+# Method 1 ---------------------------------------  
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
         res = 1
@@ -39,5 +20,26 @@ class Solution:
             if a[i] == b[0] and i+len(b) <= len(a) and a[i:i+len(b)] == b:
                 return True
         return False
-'''
+
+    
+    
+# Method 2 ---------------------------------------  
+
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        n = len(b) // len(a)
+        if self.subStr(a*n, b): return n
+        if self.subStr(a*(n+1), b): return n+1
+        if self.subStr(a*(n+2), b): return n+2    
+        return -1
+    
+    ''' 
+    def subStr(self, a, b):
+        return b in a
+    '''
+    def subStr(self, a, b):  # check if b is substring of a or not
+        for i in range(len(a)):
+            if a[i] == b[0] and i+len(b) <= len(a) and a[i:i+len(b)] == b:
+                return True
+        return False
     
