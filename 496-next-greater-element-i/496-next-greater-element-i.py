a68ce436622f44729132f8611aa1609033b66871
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-08-02 13:48:50 +0530

    Time: 75 ms (67.68%), Space: 14.3 MB (14.92%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,21 +1,18 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ngeDict = {}
         stack = []
-        nums1Set = set(nums1)
-        nextGreater = [-1] * len(nums2)
-        indexDict = {}
-        
-        for i in range(len(nums2)-1, -1, -1):
-            if nums2[i] in nums1Set:
-                indexDict[nums2[i]] = i
-            while stack and stack[-1] <= nums2[i]:
-                stack.pop()
-            if stack:
-                nextGreater[i] = stack[-1]
-            stack.append(nums2[i])
-        
+        for i in nums2:
+            while stack and stack[-1] < i:
+                a = stack.pop()
+                ngeDict[a] = i
+            stack.append(i)
+
         res = []
-        for num in nums1:
-            res.append(nextGreater[indexDict[num]])
+        for i in nums1:
+            if i in ngeDict:
+                res.append(ngeDict[i])
+            else:
+                res.append(-1)
         
         return res
