b1fd9750306dccd1c248392183353baa94c098fc
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-08-11 11:21:05 +0530

    Time: 2019 ms (56.40%), Space: 60.1 MB (7.78%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,22 +1,12 @@
-# https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
-'''
-Approch:
-1. Sort the points by the end
-2. Put an arrow at the end of the 1-st points
-3. From the 2-nd points, we check whether the current arrow pass through the current points, if not add an arrow, put it at the end of the current points
-
-'''
-
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         points.sort(key = lambda x:x[1])
-        res = 0
-        arrow = 0
-        for start, end in points:
-            if res == 0 or start > arrow:
+        print(points)
+        arrow = points[0][1]
+        res = 1
+        for p in points[1:]:
+            if p[0] > arrow:
                 res += 1
-                arrow = end
+                arrow = p[1]
         
         return res
-    
-    
