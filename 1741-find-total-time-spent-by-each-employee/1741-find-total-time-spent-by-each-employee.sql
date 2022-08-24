a566ebee903c61cb409062d6893806e23f714ded
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-08-24 22:33:15 +0530

    Time: 425 ms (94.66%), Space: 0B (100.00%) - LeetHub

--- 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
+++ 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
@@ -5,3 +5,6 @@ SELECT
     SUM(out_time - in_time) AS total_time
     FROM Employees
     GROUP BY day, emp_id;
+    
+
+-- Group by statement is used to group the rows that have the same value. Whereas Order by statement sort the result-set either in ascending or in descending order.
