25e7ba66c9d16ebc6c7a0f41213f68db7bfe5f44
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-08-30 20:52:17 +0530

    Time: 128 ms (70.34%), Space: 14.1 MB (6.47%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,19 +1,19 @@
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+    def isValidSudoku(self, board):
         row = {i:set() for i in range(9)}
         col = {j:set() for j in range(9)}
-        sub_box = {(i, j):set() for j in range(3) for i in range(3)}
+        subbox = {(i//3, j//3): set() for j in range(9) for i in range(9)}
         
         for i in range(9):
             for j in range(9):
-                if board[i][j] == ".": continue
-                num = board[i][j] 
-                if num in row[i]: return False
-                row[i].add(num)
-                if num in col[j]: return False
-                col[j].add(num)
+                cur = board[i][j]
+                if cur == '.': continue
+                if cur in row[i]: return False
+                row[i].add(cur)
+                if cur in col[j]: return False
+                col[j].add(cur)
                 key = (i//3, j//3)
-                if num in sub_box[key]: return False
-                sub_box[key].add(num)
+                if cur in subbox[key]: return False
+                subbox[key].add(cur)
         
         return True
