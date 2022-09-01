3f5dd138dd16b938cba3dc8795f066cd8f039077
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-09-01 17:47:19 +0530

    Time: 967 ms (82.56%), Space: 75.9 MB (39.76%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -8,38 +8,37 @@ class ListNode:
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.dict = {}
+        self.capacity = capacity
+        self.cache = {}
         self.left = ListNode(-1, -1)
         self.right = ListNode(-1, -1)
         self.left.next = self.right
         self.right.prev = self.left
-    
-    def remove(self, node):
+
+    def delete(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
     
     def makeMRU(self, node):
         self.right.prev.next = node
         node.prev = self.right.prev
-        self.right.prev = node
         node.next = self.right
+        self.right.prev = node
     
     def get(self, key: int) -> int:
-        if key not in self.dict: return -1
-        node = self.dict[key]
-        self.remove(node)
+        if key not in self.cache: return -1
+        node = self.cache[key]
+        self.delete(node)
         self.makeMRU(node)
         return node.val
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dict:
-            self.remove(self.dict[key])
-        node = ListNode(key, value)
-        self.dict[key] = node
-        self.makeMRU(node)
-        if len(self.dict) > self.cap:
-            lru = self.left.next
-            del self.dict[lru.key]
-            self.remove(lru)
+        if key in self.cache:
+            self.delete(self.cache[key])
+        self.cache[key] = ListNode(key, value)
+        self.makeMRU(self.cache[key])
+        if len(self.cache) > self.capacity:
+            del self.cache[self.left.next.key]
+            self.delete(self.left.next)
+        
 
