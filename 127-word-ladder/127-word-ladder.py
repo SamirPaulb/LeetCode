17b8222c2e19c54353ecf5350189551c17f1c2a0
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-09-04 02:19:10 +0530

    Time: 136 ms (95.16%), Space: 17.6 MB (36.87%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,31 +1,27 @@
-from collections import defaultdict
-from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        """
-        :type beginWord: str
-        :type endWord: str
-        :type wordList: List[str]
-        :rtype: int
-        """
-        if endWord not in wordList or not endWord or not beginWord or not wordList:
-            return 0
-        L = len(beginWord)
-        all_combo_dict = defaultdict(list)
+        if endWord not in wordList or not endWord or not beginWord or not wordList: return 0
+        wordList.append(beginWord)
+        dct = {}
         for word in wordList:
-            for i in range(L):
-                all_combo_dict[word[:i] + "*" + word[i+1:]].append(word) 
-        queue = deque([(beginWord, 1)])
-        visited = set()
-        visited.add(beginWord)
-        while queue:
-            current_word, level = queue.popleft()
-            for i in range(L):
-                intermediate_word = current_word[:i] + "*" + current_word[i+1:]
-                for word in all_combo_dict[intermediate_word]:
-                    if word == endWord:
-                        return level + 1
+            for i in range(len(word)):
+                cur = word[:i] + '*' + word[i+1:]
+                if cur not in dct:
+                    dct[cur] = [word]
+                else:
+                    dct[cur].append(word)
+        
+        q = collections.deque()
+        visited = {beginWord}
+        q.append((beginWord, 1))
+        while q:
+            cur, level = q.popleft()
+            for i in range(len(cur)):
+                key = cur[:i] + '*' + cur[i+1:]
+                for word in dct[key]:
+                    if word == endWord: return level + 1
                     if word not in visited:
+                        q.append((word, level+1))
                         visited.add(word)
-                        queue.append((word, level + 1))
+                    
         return 0
