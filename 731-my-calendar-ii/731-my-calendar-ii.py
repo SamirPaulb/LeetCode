4681163770b73f787dc3031f746f693111dc0ec7
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-09-07 18:44:37 +0530

    Time: 744 ms (56.56%), Space: 14.7 MB (48.29%) - LeetHub

--- 731-my-calendar-ii/731-my-calendar-ii.py
+++ 731-my-calendar-ii/731-my-calendar-ii.py
@@ -1,22 +1,20 @@
 class MyCalendarTwo:
-    
+
     def __init__(self):
-        self.dct = {}
-    
+        self.calendar = []
+        self.overlaps = []
+
     def book(self, start: int, end: int) -> bool:
-        self.dct[start] = self.dct[start] + 1 if start in self.dct else 1
-        self.dct[end] = self.dct[end] - 1 if end in self.dct else -1
+        for s, e in self.overlaps:
+            if s < end and start < e: return False
         
-        arr = sorted(self.dct.keys())
-        s = 0
-        for i in arr:
-            s += self.dct[i]
-            
-            if s >= 3:
-                self.dct[start] -= 1
-                self.dct[end] += 1
-                return False
-            
+        for s, e in self.calendar:
+            if s < end and start < e:
+                self.overlaps.append([max(s, start), min(e, end)])
+        
+        self.calendar.append([start, end])
         return True
 
 
+# Time: O(N)
+# Space: O(N)
