0bcdfea452456af6231abf312d976c9aa71b387b
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-10-10 19:55:03 +0530

    Time: 112 ms (33.54%), Space: 14.1 MB (5.61%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.py
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -3,14 +3,13 @@ class Solution:
         a_area = abs(ax1 - ax2) * abs(ay1 - ay2)
         b_area = abs(bx1 - bx2) * abs(by1 - by2)
         
-        if (bx1 < ax2 and ax1 < bx2) and (by1 < ay2 and ay1 < by2): # if intersection
+        if (bx1 < ax2 and ax1 < bx2) and (by1 < ay2 and ay1 < by2): # Intersection
             rx1 = max(ax1, bx1)
             rx2 = min(ax2, bx2)
             ry1 = max(ay1, by1)
             ry2 = min(ay2, by2)
             return a_area + b_area - abs(rx1 - rx2) * abs(ry1 - ry2)
         
-        else:
-            return a_area + b_area
+        return a_area + b_area  # No Intersection
             
         
