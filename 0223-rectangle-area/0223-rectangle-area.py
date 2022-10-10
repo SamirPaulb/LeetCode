9723157902edd806caa558c999f96284a9f55adf
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-10-10 19:53:56 +0530

    Time: 68 ms (82.04%), Space: 14.1 MB (26.06%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.py
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -3,7 +3,7 @@ class Solution:
         a_area = abs(ax1 - ax2) * abs(ay1 - ay2)
         b_area = abs(bx1 - bx2) * abs(by1 - by2)
         
-        if (bx1 < ax2 and ax1 < bx2) and (by1 < ay2 and ay1 < by2): # for inter section
+        if (bx1 < ax2 and ax1 < bx2) and (by1 < ay2 and ay1 < by2): # if intersection
             rx1 = max(ax1, bx1)
             rx2 = min(ax2, bx2)
             ry1 = max(ay1, by1)
