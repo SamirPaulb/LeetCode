1f8c5dcc32d4c884eb603fb7f9cab0bb47a97482
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-10-17 23:32:58 +0530

    Time: 76 ms (29.01%), Space: 14 MB (10.75%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -11,10 +11,10 @@ class Solution:
             
         for num in nums[i+1:]:
             if num == stack[-1]: continue
-            if flag == 1:
+            if flag == 1:   # Increasing
                 if num > stack[-1]: stack.pop()
                 else: flag *= -1
-            else:
+            else:           # flag = -1 => Decreasing
                 if num < stack[-1]: stack.pop()
                 else: flag *= -1
             stack.append(num)
