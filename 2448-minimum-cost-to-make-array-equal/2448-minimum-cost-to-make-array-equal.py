85c4fb5442797fdfc3e57c1bf75554e822edb476
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-10-23 23:18:08 +0530

    Time: 6161 ms (7.69%), Space: 33 MB (100.00%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -1,18 +1,20 @@
-class Solution:  
+class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
-        low, high = min(nums), max(nums)
-        while (low <= high):
-            target = (low+high)//2
-            cost_curr = self.cal_cost(nums, cost, target)
-            cost_right = self.cal_cost(nums, cost, target+1)
-            if cost_right < cost_curr:
-                low = target+1
-            else:   # cost_right < cost:
-                high = target-1
-        return self.cal_cost(nums, cost, low)
-    
-    def cal_cost(self, nums, cost, target):
-        ans = 0
-        for n, c in zip(nums, cost):
-            ans += abs(n-target) * c
-        return ans
+        def cal_cost(target):
+            ans = 0
+            for n, c in zip(nums, cost):
+                ans += abs(target - n) * c
+            return ans
+        
+        low = min(nums); high = max(nums)
+        while low <= high:
+            mid = low + (high - low) // 2
+            cur_cost = cal_cost(mid)
+            right_cost = cal_cost(mid + 1)
+            
+            if cur_cost < right_cost:
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return cal_cost(low)
