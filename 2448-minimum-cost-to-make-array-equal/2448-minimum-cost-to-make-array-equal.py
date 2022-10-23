b29c2feb3df83cc083438a15183a4fcebd5b69cc
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-10-23 23:14:00 +0530

    Time: 6367 ms (7.69%), Space: 32.8 MB (100.00%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -4,15 +4,12 @@ class Solution:
         while (low <= high):
             target = (low+high)//2
             cost_curr = self.cal_cost(nums, cost, target)
-            cost_left = self.cal_cost(nums, cost, target-1)
             cost_right = self.cal_cost(nums, cost, target+1)
-            if cost_left >= cost_curr <= cost_right:
-                return cost_curr
-            elif cost_left < cost_curr:
-                high = target-1
-            else:   # cost_right < cost:
+            if cost_right < cost_curr:
                 low = target+1
-        return cal_cost(low)
+            else:   # cost_right < cost:
+                high = target-1
+        return self.cal_cost(nums, cost, low)
     
     def cal_cost(self, nums, cost, target):
         ans = 0
