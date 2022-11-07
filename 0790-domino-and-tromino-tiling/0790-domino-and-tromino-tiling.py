63689ec67ab68cdc19289c23cdc606ef073c2565
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-11-08 01:04:21 +0530

    Time: 75 ms (39.74%), Space: 13.9 MB (59.97%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -3,8 +3,6 @@ class Solution:
         MOD = 10**9 + 7
         dp = [0, 1, 2, 5] + [1] * n
         if n <= 3: return dp[n]
-        
         for i in range(4, n+1):
-            dp[i] = ((2 * dp[i-1]) % MOD + dp[i-3])% MOD
-        
-        return dp[n]
+            dp[i] = (2 * dp[i-1] + dp[i-3]) % MOD 
+        return dp[n] % MOD
