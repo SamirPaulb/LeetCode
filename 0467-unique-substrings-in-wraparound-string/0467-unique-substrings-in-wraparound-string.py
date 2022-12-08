8c3223258cf73e4ba9a252d50715fe3cc0bc58eb
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-12-08 17:23:32 +0530

    Time: 290 ms (25.60%), Space: 14.7 MB (10.12%) - LeetHub

--- 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.py
+++ 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.py
@@ -12,3 +12,14 @@ class Solution:
             dp[p[i]] = max(dp[p[i]], streak)
         # print(dp)
         return sum(dp.values())
+    
+    
+    
+class Solution(object):
+    def findSubstringInWraproundString(self, p):
+        p, d, lo = '0'+p, collections.defaultdict(int), 0
+        for hi in range(1, len(p)):
+            if p[hi-1]+p[hi] not in 'abcdefghijklmnopqrstuvwxyza':
+                lo = hi
+            d[p[hi]] = max(d[p[hi]], hi+1-lo)
+        return sum(d.values())
