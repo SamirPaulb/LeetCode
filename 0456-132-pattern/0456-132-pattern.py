7603df5c3149813d1b496f9e159e44c73e037ec0
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-12-08 21:35:07 +0530

    Time: 390 ms (89.28%), Space: 32.2 MB (37.22%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.py
@@ -0,0 +1,13 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        curMin = nums[0]
+        stack = []  # stack contains [nums[i], nums[j]] and n = nums[k] 
+        
+        for n in nums[1:]:
+            while stack and n >= stack[-1][1]:
+                stack.pop()
+            if stack and n > stack[-1][0]: return True
+            stack.append([curMin, n])
+            curMin = min(curMin, n)
+        
+        return False
