7c2147a35a8818bc729ec317cc483c2082dea6f2
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-12-14 20:13:41 +0530

    Time: 551 ms (86.32%), Space: 28.4 MB (31.42%) - LeetHub

---
+++ 2488-count-subarrays-with-median-k/2488-count-subarrays-with-median-k.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/count-subarrays-with-median-k/
+# https://youtu.be/QZzDioqkRhU
+
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        pos = nums.index(k)
+        cnt = Counter()
+        cnt[0] = 1
+        c = 0
+        for i in range(pos+1, len(nums)):
+            c += 1 if nums[i] > k else -1
+            cnt[c] += 1
+        res = cnt[0] + cnt[1]
+        c = 0
+        for i in range(pos-1, -1, -1):
+            c += 1 if nums[i] < k else -1
+            res += cnt[c] + cnt[c+1]
+        print(cnt)
+        return res
