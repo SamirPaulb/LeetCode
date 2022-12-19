ea94f39fd29c4a0a903ba307c14b859782cbde83
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-12-19 19:10:56 +0530

    Time: 2357 ms (69.23%), Space: 72.5 MB (53.85%) - LeetHub

--- 2508-add-edges-to-make-degrees-of-all-nodes-even/2508-add-edges-to-make-degrees-of-all-nodes-even.py
+++ 2508-add-edges-to-make-degrees-of-all-nodes-even/2508-add-edges-to-make-degrees-of-all-nodes-even.py
@@ -1,3 +1,31 @@
+# https://leetcode.com/problems/add-edges-to-make-degrees-of-all-nodes-even/
+'''
+Intuition
+The number of odd degree node must be even: 0,2,4,...
+if odd.size() == 0, return True
+if odd.size() == 2, TODO
+if odd.size() == 4, TODO
+if odd.size() > 4, return false
+
+
+Explanation
+find the adjList where adjList[i] is the neighbour set of node i.
+Find out the list of nodes with odd degree.
+
+In case if we have 2 odds:
+option1:
+we can link them together, if they are not neighbour.
+option2:
+we can link both them to node i, if node i is not neighbour of either of them.
+otherwise return false.
+
+In case if we have 4 odds a,b,c,d
+option1: We can link (a,b) and (c,d)
+option2: We can link (a,c) and (b,d)
+option3: We can link (a,d) and (c,b)
+otherwise return false.
+'''
+
 class Solution:
     def isPossible(self, n: int, edges: List[List[int]]) -> bool:
         adjList = {i:set() for i in range(1, n+1)}
@@ -5,20 +33,20 @@ class Solution:
             adjList[a].add(b)
             adjList[b].add(a)
         
-        oddedges = [node for node in adjList if len(adjList[node]) % 2]
+        odd = [i for i in adjList if len(adjList[i]) % 2]
         
-        if len(oddedges) % 2 == 1: return False
+        def noLink(a, b):
+            # return True if a not in adjList[b] else False
+            return (a not in adjList[b]) 
+                
+        if len(odd) == 2:
+            a, b = odd
+            return any(noLink(a, i) and noLink(b, i) for i in adjList)
         
-        if len(oddedges) == 2:
-            a, b = oddedges
-            return True if any(a not in adjList[i] and b not in adjList[i] for i in adjList) else False
+        if len(odd) == 4:
+            a, b, c, d = odd
+            return noLink(a, b) and noLink(c, d) or \
+                   noLink(a, c) and noLink(b, d) or \
+                   noLink(a, d) and noLink(b, c)
         
-        if len(oddedges) == 4:
-            a, b, c, d = oddedges
-            # try connecting (a,b)and(b,c) or (a,c)and(b,d) or (a,d)and(b,c) 
-            if a not in adjList[b] and c not in adjList[d]: return True
-            if a not in adjList[c] and b not in adjList[d]: return True
-            if a not in adjList[d] and b not in adjList[c]: return True
-            return False
-        
-        return len(oddedges) == 0
+        return len(odd) == 0
