56c8e241063e1c187406b14ac87e5a7d47773483
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-12-22 19:32:59 +0530

    Time: 31 ms (95.14%), Space: 14 MB (56.46%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -5,11 +5,9 @@ class Solution:
         flag = -1
         for i in range(0, n, k):
             if flag == -1:
-                if i+k > n: res += s[i:][::-1]
-                else: res += s[i:i+k][::-1]
+                res += s[i:i+k][::-1]
             else:
-                if i+k > n: res += s[i:]
-                else: res += s[i:i+k]
+                res += s[i:i+k]
             flag *= -1
         
         return res
