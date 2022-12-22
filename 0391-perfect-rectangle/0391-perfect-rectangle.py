11d729963f2ed6c729cf41d549d42cd9caf1ddbd
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-12-22 21:44:36 +0530

    Time: 380 ms (85.23%), Space: 20 MB (37.50%) - LeetHub

---
+++ 0391-perfect-rectangle/0391-perfect-rectangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isRectangleCover(self, rectangles: List[List[int]]) -> bool:
+        X1 = Y1 = float("inf")
+        X2 = Y2 = float("-inf")
+        corners = set()
+        area = 0
+        for x1, y1, x2, y2 in rectangles:
+            area += (x2-x1) * (y2-y1)
+            X1 = min(X1, x1)
+            Y1 = min(Y1, y1)
+            X2 = max(X2, x2)
+            Y2 = max(Y2, y2)
+            corners ^= {(x1,y1), (x2,y2), (x1,y2), (x2,y1)}
+
+        return area == (X2-X1) * (Y2-Y1) and corners == {(X1,Y1), (X2,Y2), (X1,Y2), (X2,Y1)}
