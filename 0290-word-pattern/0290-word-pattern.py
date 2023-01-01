0a87ff97259b933c580b3894d7b54317086676fa
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-01 11:45:46 +0530

    Time: 34 ms (77.71%), Space: 13.9 MB (74.33%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -2,25 +2,17 @@ class Solution:
     def wordPattern(self, p: str, s: str) -> bool:
         s = list(s.split(' '))
         if len(s) != len(p): return False
-        sd = {}
-        for i, ch in enumerate(p):
-            if ch not in sd:
-                sd[ch] = {s[i]}
+        dp = {}
+        ds = {}
+        for ep, es in zip(p, s):
+            if ep not in dp:
+                dp[ep] = es
             else:
-                sd[ch].add(s[i])
-                
-        for ch in sd:
-            if len(sd[ch]) != 1: return False
-        
-        pd = {}
-        for i, ch in enumerate(s):
-            if ch not in pd:
-                pd[ch] = {p[i]}
+                if dp[ep] != es: return False
+            
+            if es not in ds:
+                ds[es] = ep
             else:
-                pd[ch].add(p[i])
-        
-        for ch in pd:
-            if len(pd[ch]) != 1: return False
+                if ds[es] != ep: return False
         
         return True
-                
