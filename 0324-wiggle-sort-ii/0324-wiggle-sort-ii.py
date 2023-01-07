7ba511ab370597a557ad5bd29ee18257b32ef41d
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-07 20:09:25 +0530

    Time: 180 ms (78.42%), Space: 17.1 MB (52.19%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        arr = sorted(nums)
+        for i in range(1, len(nums), 2):
+            nums[i] = arr.pop()
+        for i in range(0, len(nums), 2):
+            nums[i] = arr.pop()
+        
