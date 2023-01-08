f3376698ab425d54421a06c8bf9229228ef81786
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-08 18:03:21 +0530

    Time: 81 ms (92.46%), Space: 14.2 MB (92.55%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,22 +1,35 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        adj = {}
-        for scr, dst in tickets:
-            if scr not in adj:
-                adj[scr] = [dst]
+        graph = {}
+        # Create a graph for each airport and keep list of airport reachable from it
+        for src, dst in tickets:
+            if src in graph:
+                graph[src].append(dst)
             else:
-                adj[scr].append(dst)
-        
-        for scr in adj:
-            adj[scr].sort(reverse = True)
-        
-        stack = ['JFK']
+                graph[src] = [dst]
+
+        for src in graph.keys():
+            graph[src].sort(reverse=True)
+            # Sort children list in descending order so that we can pop last element 
+            # instead of pop out first element which is costly operation
+        stack = []
         res = []
-        while stack:
-            elm = stack[-1]
-            if elm in adj and len(adj[elm]) > 0:
-                stack.append(adj[elm].pop())
+        stack.append("JFK")
+        # Start with JFK as starting airport and keep adding the next child to traverse 
+        # for the last airport at the top of the stack. If we reach to an airport from where 
+        # we can't go further then add it to the result. This airport should be the last to go 
+        # since we can't go anywhere from here. That's why we return the reverse of the result
+        # After this backtrack to the top airport in the stack and continue to traaverse it's children
+
+        while len(stack) > 0:
+            elem = stack[-1]
+            if elem in graph and len(graph[elem]) > 0: 
+                # Check if elem in graph as there may be a case when there is no out edge from an airport 
+                # In that case it won't be present as a key in graph
+                stack.append(graph[elem].pop())
             else:
                 res.append(stack.pop())
-        
+                # If there is no further children to traverse then add that airport to res
+                # This airport should be the last to go since we can't anywhere from this
+                # That's why we return the reverse of the result
         return res[::-1]
