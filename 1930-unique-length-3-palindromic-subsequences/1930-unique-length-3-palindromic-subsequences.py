25855aae5b977be6611e2e8708b116b3f874c61c
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-10 00:18:59 +0530

    Time: 175 ms (94.18%), Space: 14.8 MB (97.76%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,13 +1,23 @@
+# https://leetcode.com/problems/unique-length-3-palindromic-subsequences/
+
 '''
 Just notice we need to return the count of distinct palindromes having length 3(point to be noticed).
-Special thing about these odd length palindromes are that they have first and last character same so we only need to check how many unique characters are there between first and last character(this can be done using set)
+Special thing about these odd length palindromes are that they have first and last character same 
+so we only need to check how many unique characters are there between first and last character(this can be done using set)
 '''
+
 class Solution:
     def countPalindromicSubsequence(self, s):
         res = 0
-        for i in set(s):
-            l = s.find(i)   # first occurance
-            r = s.rfind(i)  # last occurance
-            res += len(set(s[l+1:r]))  # count of unique element in middle
+        for c in string.ascii_lowercase:
+            l = s.find(c)   # first occurance
+            r = s.rfind(c)  # last occurance
+            if l > -1:
+                res += len(set(s[l+1:r]))  # count of unique element in middle
         
         return res
+      
+      
+# Time: O(26*N)
+# Space: O(26*N)
+
