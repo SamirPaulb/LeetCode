552f044f15e743b3ce835054832e113b1c533988
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-10 00:10:39 +0530

    Time: 191 ms (92.84%), Space: 14.8 MB (97.76%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -3,7 +3,7 @@ Just notice we need to return the count of distinct palindromes having length 3(
 Special thing about these odd length palindromes are that they have first and last character same so we only need to check how many unique characters are there between first and last character(this can be done using set)
 '''
 class Solution:
-    def countPalindromicSubsequence(self, s: str) -> int:
+    def countPalindromicSubsequence(self, s):
         res = 0
         for i in set(s):
             l = s.find(i)   # first occurance
