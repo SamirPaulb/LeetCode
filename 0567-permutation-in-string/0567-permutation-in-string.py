617a59421bc35458ffedff42246145688258b688
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-10 21:14:55 +0530

    Time: 87 ms (77.18%), Space: 13.9 MB (95.89%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,9 +2,11 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         k = len(s1)
         s1d = collections.Counter(s1)
-        for i in range(k-1, len(s2)):
-            sb = s2[i-k+1:i+1]
-            sbd = collections.Counter(sb)
-            if sbd == s1d: return True
+        s2d = collections.Counter(s2[:k])
+        if s1d == s2d: return True
+        for i in range(k, len(s2)):
+            s2d[s2[i]] += 1
+            s2d[s2[i-k]] -= 1
+            if s1d == s2d: return True
         
         return False
