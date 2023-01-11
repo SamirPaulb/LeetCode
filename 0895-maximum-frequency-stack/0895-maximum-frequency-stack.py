864fa39e622a8209354e0a9fea657f42d54ffb8f
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-11 21:09:15 +0530

    Time: 336 ms (91.54%), Space: 22.6 MB (79.43%) - LeetHub

--- 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
@@ -1,3 +1,7 @@
+# https://leetcode.com/problems/maximum-frequency-stack/
+# https://youtu.be/Z6idIicFDOE
+
+'''
 import heapq
 class FreqStack:
 
@@ -15,4 +19,32 @@ class FreqStack:
         val = self.maxHeap[0][2]
         self.cnt[val] -= 1
         heapq.heappop(self.maxHeap)
+        return val
+        
+# Time: O(log(N))
+# Space: O(N)
+'''
+
+class FreqStack:
+
+    def __init__(self):
+        self.cnt = collections.Counter()
+        self.stackDict = {}
+        self.maxcnt = 0
+
+    def push(self, val):
+        self.cnt[val] += 1
+        if self.maxcnt < self.cnt[val]:
+            self.maxcnt = self.cnt[val]
+            self.stackDict[self.maxcnt] = [val]
+        else:
+            self.stackDict[self.cnt[val]].append(val)
+
+    def pop(self):
+        # print(self.stackDict)
+        if not self.stackDict[self.maxcnt]:
+            del self.stackDict[self.maxcnt]
+            self.maxcnt -= 1
+        val = self.stackDict[self.maxcnt].pop()
+        self.cnt[val] -= 1
         return val
