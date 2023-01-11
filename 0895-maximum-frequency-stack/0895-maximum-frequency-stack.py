7c4b69470067292c13409487ff93eff8c79cd75b
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-11 19:41:07 +0530

    Time: 442 ms (57.32%), Space: 23 MB (23.09%) - LeetHub

--- 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
@@ -4,14 +4,14 @@ class FreqStack:
     def __init__(self):
         self.cnt = collections.Counter()
         self.maxHeap = []
-        self.i = 0
+        self.indx = 0
 
-    def push(self, val: int) -> None:
+    def push(self, val):
         self.cnt[val] += 1
-        self.i += 1
-        heapq.heappush(self.maxHeap, (-self.cnt[val], -self.i, val))
+        self.indx += 1
+        heapq.heappush(self.maxHeap, (-self.cnt[val], -self.indx, val))
         
-    def pop(self) -> int:
+    def pop(self):
         val = self.maxHeap[0][2]
         self.cnt[val] -= 1
         heapq.heappop(self.maxHeap)
