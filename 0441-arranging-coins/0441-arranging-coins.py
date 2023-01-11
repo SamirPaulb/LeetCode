24888d3465369ea4b26dc9745948dd5c58a88c83
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-11 22:15:23 +0530

    Time: 34 ms (94.83%), Space: 13.8 MB (96.34%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,4 +1,21 @@
+# https://leetcode.com/problems/arranging-coins/
+# https://samirpaulb.github.io/assets/arranging-coins-leetcode.png
+
+
+# class Solution:
+#     def arrangeCoins(self, n):
+#         return int((2*n + 1/4)**(1/2) - 1/2)
+
 class Solution:
-    def arrangeCoins(self, n: int) -> int:
-        return int((2*n + 1/4)**(1/2) - 1/2)
-    
+    def arrangeCoins(self, n):
+        res = 0
+        l, r = 0, (n+1)//2
+        while l <= r:
+            mid = l + (r-l)//2
+            if mid*(mid+1)//2 <= n:
+                res = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return res
