a6bf9b993d051a4e996e47e4d65ba5f7c62bbc96
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-12 19:31:42 +0530

    Time: 74 ms (77.87%), Space: 14.6 MB (58.52%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -41,11 +41,5 @@ class MyCircularQueue:
         return self.curSize == self.maxSize
 
 
-# Your MyCircularQueue object will be instantiated and called as such:
-# obj = MyCircularQueue(k)
-# param_1 = obj.enQueue(value)
-# param_2 = obj.deQueue()
-# param_3 = obj.Front()
-# param_4 = obj.Rear()
-# param_5 = obj.isEmpty()
-# param_6 = obj.isFull()
+# Time: O(1)
+# Space: O(k)
