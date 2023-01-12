547c51a5b054b8127da6bdefddcfea12e1c82af6
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-12 19:53:04 +0530

    Time: 479 ms (84.29%), Space: 15.5 MB (72.59%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,6 +1,6 @@
 class Solution:
     def minEatingSpeed(self, piles, h):
-        def valid(speed):
+        def isValid(speed):
             time = 0
             for num in piles:
                 time += math.ceil(num/speed)
@@ -9,10 +9,14 @@ class Solution:
         l, r, res = 1, max(piles), max(piles)
         while l <= r:
             mid = l + (r-l)//2
-            if valid(mid):
+            if isValid(mid):
                 res = min(res, mid)
                 r = mid - 1
             else:
                 l = mid + 1
         
         return res
+    
+    
+# Time: O(N * log(M))      where M = max(piles)
+# Space: O(1)
