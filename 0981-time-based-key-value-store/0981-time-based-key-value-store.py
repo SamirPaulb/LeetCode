64ee0c9e22238fab25f247765bc023ad6b2ac048
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-12 22:22:12 +0530

    Time: 765 ms (85.15%), Space: 71.2 MB (82.90%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -15,14 +15,12 @@ class TimeMap:
         # print(arr)
         l, r = 0, len(arr)-1
         i = 0
-        while l <= r:
-            mid = l + (r-l)//2
-            if timestamp > arr[mid][0]:
-                l = mid+1
-            elif timestamp < arr[mid][0]:
+        while l < r:
+            mid = (l+r+1)//2
+            if timestamp < arr[mid][0]:
                 r = mid-1
             else:
-                return arr[mid][1]
+                l = mid
                 
-        curTime = arr[r][0]
-        return arr[r][1] if timestamp >= curTime else ""
+        curTime = arr[l][0]
+        return arr[l][1] if timestamp >= curTime else ""
