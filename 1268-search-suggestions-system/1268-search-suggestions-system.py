31e4bcad35fa42b848ad724f64c2a74f421aac3c
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-14 17:10:51 +0530

    Time: 430 ms (39.88%), Space: 22.8 MB (10.04%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,3 +1,68 @@
+# https://leetcode.com/problems/search-suggestions-system/
+
+# Method 1: Sorting
+class Solution:
+    def suggestedProducts(self, products, searchWord):
+        products.sort()
+        res = []
+        for i, c in enumerate(searchWord):
+            products = [p for p in products if len(p) > i and p[i] == c]
+            res.append(products[:3])
+        
+        return res
+
+# Time: O(N log(N)) + O(N * K) ; where N = len(products), K = len(searchWord)
+
+
+# ---------------------------------------------------------------
+# Method 2: Trie
+class Node:
+    def __init__(self):
+        self.val = ""
+        self.children = {}
+        self.suggestions  = []
+
+        
+class Trie:
+    def __init__(self):
+        self.root = Node()
+    
+    def addWord(self, word):
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = Node()
+            cur = cur.children[c]
+            cur.val = c
+            if len(cur.suggestions) < 3:
+                cur.suggestions.append(word)
+    
+    def getSuggestions(self, searchWord):
+        cur = self.root
+        res = []
+        for c in searchWord:
+            if c in cur.children:
+                cur = cur.children[c]
+                res.append(cur.suggestions)
+            else: 
+                break
+        res += [[] for i in range(len(searchWord)-len(res))]
+        return res
+                
+
+class Solution:
+    def suggestedProducts(self, products, searchWord):
+        products.sort()
+        trie = Trie()
+        for word in products:
+            trie.addWord(word)
+        return trie.getSuggestions(searchWord)
+
+# Time: O(N log(N)) + O(N * K)
+
+    
+# ---------------------------------------------------------------
+# Method 3: Trie + DFS
 class TrieNode:
     def __init__(self):
         self.children = {}
@@ -43,3 +108,5 @@ class Solution:
             
         res += [[] for i in range(len(searchWord) - len(res))]
         return res
+    
+# Time: O(N log(N)) + O(N * K)
