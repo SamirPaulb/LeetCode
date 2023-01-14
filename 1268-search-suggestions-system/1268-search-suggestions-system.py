56c765290554938f6e4ca30e4c289cca4f791bda
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-14 16:55:48 +0530

    Time: 184 ms (69.53%), Space: 21.2 MB (34.76%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,9 +1,43 @@
-class Solution:
-    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products.sort()
+class Node:
+    def __init__(self):
+        self.val = ""
+        self.children = {}
+        self.suggestions  = []
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+    
+    def addWord(self, word):
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = Node()
+            cur = cur.children[c]
+            cur.val = c
+            if len(cur.suggestions) < 3:
+                cur.suggestions.append(word)
+    
+    def getSuggestions(self, searchWord):
+        cur = self.root
         res = []
-        for i, c in enumerate(searchWord):
-            products = [p for p in products if len(p) > i and p[i] == c]
-            res.append(products[:3])
-        
+        for c in searchWord:
+            if c not in cur.children:
+                break
+            else:
+                cur = cur.children[c]
+                res.append(cur.suggestions)
+        res += [[] for i in range(len(searchWord)-len(res))]
         return res
+                
+
+class Solution:
+    def suggestedProducts(self, products, searchWord):
+        # edge cases:
+        # if len(products) == 1 and products[0] == searchWord:
+        #     return [[searchWord] for i in range(len(searchWord))]
+        products.sort()
+        trie = Trie()
+        for word in products:
+            trie.addWord(word)
+        return trie.getSuggestions(searchWord)
