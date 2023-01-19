30b6a7239858372a439340177bfe90991bdd4353
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-20 00:06:02 +0530

    Time: 2139 ms (52.24%), Space: 39.4 MB (5.99%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -0,0 +1,32 @@
+'''
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        q = collections.deque()
+        res = []
+        
+        for i, ch in enumerate(nums):
+            while q and nums[q[-1]] <= ch:
+                q.pop()
+            if q and i - q[0] >= k:
+                q.popleft()
+            q.append(i)
+            if i >= k-1: res.append(nums[q[0]])
+        
+        return res
+    
+'''
+import heapq
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        maxHeap = []
+        res = []
+        l = r = 0
+        for r, ch in enumerate(nums):
+            heapq.heappush(maxHeap, (-ch, r))
+            if r-l+1 > k: l += 1
+            while maxHeap and maxHeap[0][1] < l:
+                heapq.heappop(maxHeap)
+            if r >= k-1 and maxHeap:
+                res.append(-maxHeap[0][0])
+        
+        return res
