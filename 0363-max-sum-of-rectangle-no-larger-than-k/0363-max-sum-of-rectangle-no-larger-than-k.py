ea48fbddccb56b34633d9cbd0d8e0224e65f9449
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-21 01:46:10 +0530

    Time: 1953 ms (66.15%), Space: 14.6 MB (99.48%) - LeetHub

--- 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.py
+++ 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.py
@@ -1,19 +1,25 @@
 class Solution:
     def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
-        row = len(matrix); col = len(matrix[0])
-        res = -2**31
-        for i in range(col):
-            arr = [0] * row
-            for j in range(i, col):
-                sortedSum = [0]
-                s = 0
-                for r in range(row):
-                    arr[r] += matrix[r][j]
-                    s += arr[r]
-                    idx = bisect.bisect_left(sortedSum, s-k)
-                    if 0<= idx < len(sortedSum):
-                        res = max(res, s - sortedSum[idx])
-                    bisect.insort(sortedSum, s)
-                        
-                                            
-        return res
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        ans = -2**31 - 1
+        for i in range(cols):
+            colsum = [0] * rows
+            for j in range(i, cols):
+                curlist = [0]
+                rowsum = 0
+                for r in range(rows):
+                    # sum up columns in each row from i to j, use prefix sum to speed up
+                    colsum[r] += matrix[r][j]
+                    # sum up rows up to r
+                    rowsum += colsum[r]
+                    # find the smallest element larger than rowsum-k if exists
+                    idx = bisect.bisect_left(curlist, rowsum - k)
+                    # update final answer
+                    if idx >= 0 and idx < len(curlist):
+                        ans = max(ans, rowsum - curlist[idx])
+                    # update the set of cums(or ordered list here)
+                    bisect.insort(curlist, rowsum)
+
+        return ans              
