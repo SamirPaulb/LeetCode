21eb6eb352231bd67be54af41e1fa8538244c2d1
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-27 12:27:40 +0530

    Time: 2035 ms (17.08%), Space: 15.3 MB (81.13%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -3,25 +3,23 @@ class Solution:
         deadends = set(deadends)
         if '0000' in deadends: 
             return -1
-        visited = set()
-        q = collections.deque()
-        q.append(['0000', 0])
         
         def children(lock):
             arr = []
             for i in range(4):
                 cur = int(lock[i])
-                q.append([lock[:i] + str((cur+1)%10) + lock[i+1:], turns + 1])
-                q.append([lock[:i] + str((cur-1)%10) + lock[i+1:], turns + 1])
+                arr.append(lock[:i] + str((cur+1)%10) + lock[i+1:])
+                arr.append(lock[:i] + str((cur-1)%10) + lock[i+1:])
             return arr
             
+        visited = set()
+        q = collections.deque()
+        q.append(['0000', 0])
             
         while q:
             lock, turns = q.popleft()
-            if lock == target: return turns
-            if lock in deadends or lock in visited: continue
-            visited.add(lock)
-            n = len(q)
+            if lock == target: 
+                return turns
             for child in children(lock):
                 if child not in deadends and child not in visited:
                     visited.add(child)
