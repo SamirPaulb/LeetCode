660ff0082cd1bf75c17ba1ccfa11e63bc00d81dc
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-27 18:05:37 +0530

    Time: 662 ms (89.20%), Space: 21.7 MB (55.89%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -2,14 +2,14 @@ class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         safe = {}
         def dfs(node):
-            if node in safe: 
+            if node in safe:        # Cycle found or previously checked
                 return safe[node]
-            safe[node] = False
+            safe[node] = False      # NOTE: FOR CYCLE DETECTION
             for i in graph[node]:
-                if not dfs(i):
-                    return safe[i]
-            safe[node] = True
-            return True
+                if not dfs(i):      # visited i and not safe
+                    return safe[i]  # stop further check
+            safe[node] = True       # all outgoing edges had been checked so safe node
+            return safe[node]
         
         res = []
         for i in range(len(graph)):
@@ -17,3 +17,12 @@ class Solution:
                 res.append(i)
         
         return res
+
+'''
+NOTE:  
+initializing safe[node] = False, so before traversing all outgoing edges 
+if we again reach node i.e, loop detected then return safe[node].
+'''
+
+
+# Time: O(N)
