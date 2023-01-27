b5edd18d889895f0184cee4628b4165df17b0158
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-28 01:41:41 +0530

    Time: 305 ms (67.90%), Space: 14 MB (83.39%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,13 +1,9 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        rules = {'a':'e', 'e':'ai', 'i':'aeou', 'o':'iu', 'u':'a'}
-        dp = {'a':1, 'e':1, 'i':1, 'o':1, 'u':1}
-        for i in range(1, n):
-            new_dp = {'a':0, 'e':0, 'i':0, 'o':0, 'u':0}
-            for key, values in rules.items():
-                for v in values:
-                    new_dp[key] += dp[v]
-            dp = new_dp
-        return sum(dp.values()) % 1000000007
+        a, e, i, o, u = 1, 1, 1, 1, 1
+        for _ in range(1, n):
+            a, e, i, o, u = e, a+i, a+e+o+u, i+u, a
+            # print(a, e, i, o, u)
+        return (a + e + i + o + u) % 1000000007
                     
             
