c74fc96e4551fb96db1a4953c7b840a25ce86e7d
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-29 19:57:00 +0530

    Time: 98 ms (85.09%), Space: 55.2 MB (7.18%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,13 +1,29 @@
+#----------------------- Method 1 -----------------------
 class Solution:
-    def maxLength(self, arr: List[str]) -> int:
-        dp = [set()]  # added an empty set
+    def maxLength(self, arr):
+        dp = [set()]  # adding an empty set
         for a in arr:
-            if len(set(a)) < len(a): continue
+            if len(set(a)) < len(a): continue  # repeated character is string
             a = set(a)
             for c in dp:
-                if a & c: # intersection of 2 sets is not empty
-                    continue 
-                dp.append(a | c)  # added union of set a and c
+                if a & c: continue # intersection of 2 sets is not empty 
+                dp.append(a | c)   # adding union of set a and c
         
         return max(len(a) for a in dp)
-                
+    
+    '''
+#----------------------- Method 2 -----------------------
+class Solution:
+    def maxLength(self, arr):
+        unique = ['']
+        for a in arr:
+            for b in unique:
+                if len(a+b) == len(set(a+b)):  # a and b are Mutual Exclusivity
+                    unique.append(a+b)
+        
+        return max(len(a) for a in unique)
+    
+    
+#----------------------- Method 3 -----------------------
+
+            '''
