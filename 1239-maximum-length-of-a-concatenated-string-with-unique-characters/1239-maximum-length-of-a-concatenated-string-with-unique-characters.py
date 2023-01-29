e7a1613b933f1666f4306930d6a438777b401df0
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-29 20:08:15 +0530

    Time: 92 ms (88.68%), Space: 14 MB (61.04%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,3 +1,9 @@
+# https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+'''
+In this we will store each unique lenght substring separetly and will compare with all the possible permutation.
+'''
+
+
 #----------------------- Method 1 -----------------------
 class Solution:
     def maxLength(self, arr):
@@ -11,7 +17,7 @@ class Solution:
         
         return max(len(a) for a in dp)
     
-    '''
+    
 #----------------------- Method 2 -----------------------
 class Solution:
     def maxLength(self, arr):
@@ -25,5 +31,19 @@ class Solution:
     
     
 #----------------------- Method 3 -----------------------
-
-            '''
+class Solution:
+    def maxLength(self, arr):
+        self.res = 0
+        def dfs(i, s):
+            if len(set(s)) < len(s): 
+                return
+            self.res = max(self.res, len(s))
+            for j in range(i, len(arr)):
+                dfs(j+1, s + arr[j])
+        
+        dfs(0, "")
+        return self.res
+            
+        
+        
+# Time: O(N^2)  # For All Methods 1, 2, 3
