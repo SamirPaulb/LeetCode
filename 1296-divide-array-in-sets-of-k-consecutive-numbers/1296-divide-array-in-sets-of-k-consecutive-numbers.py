7958999b32292fd5773b445ea235ced403d9e4be
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-29 10:18:10 +0530

    Time: 485 ms (60.17%), Space: 31.5 MB (54.32%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        cnt = collections.Counter(nums)
+        numset = sorted(list(cnt.keys()))
+        i = 0
+        while i < len(numset):
+            c = numset[i]
+            if cnt[c] > 0:
+                for j in range(k):
+                    cnt[c+j] -= 1
+                    if cnt[c+j] < 0: return False
+            else:
+                i += 1
+        
+        return True
