e150b91efd032b58f572003b6cc40337c5297cd0
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-30 19:10:13 +0530

    Time: 43 ms (74.26%), Space: 13.9 MB (76.08%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,5 +1,27 @@
+# https://leetcode.com/problems/two-city-scheduling/
+# https://youtu.be/d-B_gk_gJtQ
+
+# ------------------ Greedy Solution ------------------
 class Solution:
-    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+    def twoCitySchedCost(self, costs):
+        diffs = []
+        for ac, bc in costs:
+            diffs.append((bc-ac, ac, bc))
+        diffs.sort()
+        res = 0
+        for i in range(len(costs)):
+            if i < len(costs)//2:
+                res += diffs[i][2]    # sending to city B     
+            else:
+                res += diffs[i][1]    # sending to city A
+                
+        return res
+
+# Time: O(n log(n))
+'''
+# ------------------ Dynamic Programming ------------------
+class Solution:
+    def twoCitySchedCost(self, costs):
         n = len(costs)//2
         dp = [[-1]*(n+1) for _ in range(n+1)]
         
@@ -13,8 +35,12 @@ class Solution:
             
             aCost = costs[i][0] + dfs(i+1, a+1, b)
             bCost = costs[i][1] + dfs(i+1, a, b+1)
-            dp[a][b] = min(aCost, bCost)
             
+            dp[a][b] = min(aCost, bCost)
             return dp[a][b]
         
         return dfs(0, 0, 0)
+    
+    
+# Time: O(n^2)
+'''
