214022fa610338f223560a50fb1be7a90eec7372
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-30 22:32:54 +0530

    Time: 33 ms (89.66%), Space: 13.8 MB (68.97%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/splitting-a-string-into-descending-consecutive-values/
+
 class Solution:
     def splitString(self, s: str) -> bool:
         i = 0
@@ -14,9 +16,11 @@ class Solution:
                 elif num == n:
                     n -= 1
                     l = r+1
-                    print(l)
                     if l == len(s): return True
                 r += 1
             i += 1
         
         return False
+      
+      
+# Time: O(N^2)
