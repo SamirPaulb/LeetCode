9fcea24e9b13dcf01a4a3f33d7cb330c8e67744d
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-31 20:18:21 +0530

    Time: 2292 ms (61.54%), Space: 55.3 MB (82.05%) - LeetHub

---
+++ 1882-process-tasks-using-servers/1882-process-tasks-using-servers.py
@@ -0,0 +1,23 @@
+class Solution:
+    def assignTasks(self, servers: List[int], tasks: List[int]) -> List[int]:
+        res = [0] * len(tasks)
+        
+        available = [(servers[i], i) for i in range(len(servers))]
+        heapq.heapify(available)
+        unavailable = []
+        
+        t = 0
+        for i in range(len(tasks)):
+            t = max(t, i)
+            
+            if len(available) == 0:
+                t = unavailable[0][0]
+            while unavailable and t >= unavailable[0][0]:
+                timefree, weight, index = heapq.heappop(unavailable)
+                heapq.heappush(available, (weight, index))
+            
+            weight, index = heapq.heappop(available)
+            res[i] = index
+            heapq.heappush(unavailable, (t + tasks[i], weight, index))
+        
+        return res
