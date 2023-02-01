a6fd92c6d91f727e48176d8900b02f5f840fd51f
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-02-01 18:16:16 +0530

    Time: 31 ms (95.66%), Space: 13.9 MB (77.74%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,14 +1,33 @@
+# https://leetcode.com/problems/verifying-an-alien-dictionary/
+
+''' 
+If a sequence of words is sorted, then each adjacent word of the sequence must also be sorted.
+
+\U0001f449 Lexicographically Sorted when -
+    ✦ If at first mismatch, mp[a[i]] < mp[b[i]], or
+    ✦ If each letters of both words match and length(a) <= length(b)
+
+\U0001f449 Not Lexicographically Sorted when -
+    ✦ If at first mismatch, mp[a[i]] > mp[b[i]], or
+    ✦ If each letters of both words match and length(a) > length(b).
+
+'''
+
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         ind = {c:i for i,c in enumerate(order)}
-        for a, b in zip(words, words[1:]):
-            if len(a) > len(b) and a[:len(b)] == b: 
-                return False
-            for s1, s2 in zip(a, b):
-                if ind[s1] < ind[s2]:
-                    break
-                if ind[s1] > ind[s2]:
-                    return False
+        def check(w1, w2):
+            for s1, s2 in zip(w1, w2):
+                if ind[s1] != ind[s2]:
+                    return ind[s1] < ind[s2]
+            return len(w1) <= len(w2)
         
-        return True
+        return all(check(w1, w2) for w1, w2 in zip(words, words[1:]))
+
                 
+        
+'''
+✦ Time Complexity : O(N),   where N is the total number of characters in words.
+
+✦ Space Complexity : O(1),  we only need constant amount of space to store the mapping of letters to index which does not depend on the size of input. Hence we get constant space complexity.
+'''
