6fda31ff216c41d9747b6e72d9f04c24913d3f32
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-02-01 19:42:31 +0530

    Time: 27 ms (95.70%), Space: 13.7 MB (98.52%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,5 +1,23 @@
+# https://leetcode.com/problems/greatest-common-divisor-of-strings/
+
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
+    def gcdOfStrings(self, str1, str2):
+        if str1 + str2 != str2 + str1: 
+            return ""
+        len1, len2 = len(str1), len(str2)
+        
+        def gcd(a, b):
+            if b == 0: return a
+            return gcd(b, a%b)
+        
+        i = gcd(len1, len2)
+        return str1[:i]
+
+
+
+'''
+class Solution:
+    def gcdOfStrings(self, str1, str2):
         len1, len2 = len(str1), len(str2)
         
         def isDivisor(i):
@@ -13,3 +31,7 @@ class Solution:
                 return str1[:i]
         
         return ""
+    
+    
+# Time: O(N^2)  # as time to take a substring is O(N)
+'''
