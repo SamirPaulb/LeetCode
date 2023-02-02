17a2bd35203a6ccb2ca3178068402b230c92f30b
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-02-02 19:05:42 +0530

    Time: 1182 ms (91.53%), Space: 28.4 MB (98.31%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -1,3 +1,5 @@
+'''# https://leetcode.com/problems/maximum-alternating-subsequence-sum/
+
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         stack = []
@@ -14,7 +16,7 @@ class Solution:
         if not len(stack)%2:
             stack.pop()
 
-        print(stack)
+        # print(stack)
         res = 0
         for i in range(1, len(stack)+1):
             if i%2:
@@ -23,3 +25,28 @@ class Solution:
                 res -= stack[i-1]
         
         return res
+    
+    
+# Time: O(N)
+# Space: O(N)
+'''
+
+
+'''
+Given an alternating sequence (a0, a1... ak), the change in value after appending an element x depends only on whether we have an even or odd number of elements so far:
+
+If we have even # of elements, we add x; otherwise, we subtract x. So, tracking the best subsequences of odd and even sizes gives an extremely simple update formula.
+
+'''
+class Solution:
+    def maxAlternatingSum(self, nums):
+        odd, even = nums[0], 0
+        for num in nums[1:]:
+            odd = max(even + num, odd)
+            even = max(odd - num, even)
+        
+        return max(odd, even)
+    
+    
+# Time: O(N)
+# Space: O(1)
