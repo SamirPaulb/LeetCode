6d068efe1edc8083ab518b27bb29a0201b4b2a83
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-02-04 15:10:10 +0530

    Time: 121 ms (72.28%), Space: 14 MB (74.25%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -2,7 +2,7 @@
 
 class Solution:
     def canPartitionKSubsets(self, nums, k):
-        nums.sort(reverse=True) # Game Changer 1
+        nums.sort(reverse=True)
         subsets, target = [0] * k, sum(nums)//k
         
         def backtrack(idx):
@@ -18,9 +18,42 @@ class Solution:
             return False
         
         return backtrack(0)
-        
+    
+# Time: O(n * 2^k)
+# Space: O(n * 2^k)
 
+
+'''
+# https://youtu.be/h_6MldQ8vB8?t=121
+
+class Solution:
+    def canPartitionKSubsets(self, nums, k):
+        if sum(nums) % k: 
+            return False
+        nums.sort(reverse = True)
+        target = sum(nums)//k
+        used = [False] * len(nums)
+        
+        def backtrack(i, k, subsetSum):
+            if k == 0: 
+                return True
+            if subsetSum == target:
+                return backtrack(0, k-1, 0)
+            if i >= len(nums) or subsetSum > target:
+                return False
+            if used[i]:
+                return backtrack(i+1, k, subsetSum)
+            # Pick the urrent element
+            used[i] = True
+            pick = backtrack(i+1, k, subsetSum + nums[i])
+            used[i] = False
+            # Skip the current element
+            skip = backtrack(i+1, k, subsetSum)
+            return pick or skip
+        
+        return backtrack(0, k, 0)
     
     
 # Time: O(k * 2^n)
 # Space: O(k * 2^n)
+'''
