1eb7bf82c26b26462e02b4dd698b6acb65058408
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-02-04 15:03:47 +0530

    Time: 113 ms (76.31%), Space: 13.9 MB (93.48%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,19 +1,24 @@
 # https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
-# https://youtu.be/h_6MldQ8vB8?t=121
 
 class Solution:
     def canPartitionKSubsets(self, nums, k):
         nums.sort(reverse=True) # Game Changer 1
-        buck, kSum = [0] * k, sum(nums) // k
-        def dfs(idx):
-            if idx == len(nums): return len(set(buck)) == 1
+        subsets, target = [0] * k, sum(nums)//k
+        
+        def backtrack(idx):
+            if idx == len(nums):
+                return True
             for i in range(k):
-                buck[i] += nums[idx]
-                if buck[i] <= kSum and dfs(idx + 1): return True
-                buck[i] -= nums[idx]
-                if buck[i] == 0: break # Game Changer 2
+                subsets[i] += nums[idx]
+                if subsets[i] <= target and backtrack(idx+1): 
+                    return True
+                subsets[i] -= nums[idx]
+                if subsets[i] == 0: 
+                    break
             return False
-        return dfs(0)
+        
+        return backtrack(0)
+        
 
     
     
