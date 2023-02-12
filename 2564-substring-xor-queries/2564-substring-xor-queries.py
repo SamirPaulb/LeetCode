431fb2c02820bcc2bf84842366ed78990cd4a323
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-02-12 11:56:11 +0530

    Time: 1505 ms (38.89%), Space: 90.6 MB (5.56%) - LeetHub

--- 2564-substring-xor-queries/2564-substring-xor-queries.py
+++ 2564-substring-xor-queries/2564-substring-xor-queries.py
@@ -2,7 +2,7 @@ class Solution:
     def substringXorQueries(self, s, queries):
         seen = defaultdict(lambda: [-1, -1])
         for i in range(len(s)-1, -1, -1):
-            if s[i] == '0':
+            if s[i] == '0': # prevents picking "011" instead of "11" for value = 3. So basically we need to exclude subslices starts from "0", except for "0" itself
                 seen[0] = [i, i]
                 continue
             v = 0
@@ -11,3 +11,7 @@ class Solution:
                 if v > 2**32: break
                 seen[v] = [i, j]
         return [seen[fst ^ snd] for fst, snd in queries]
+    
+    
+# Time: O(30N + Q)
+# Space: O(30N)
