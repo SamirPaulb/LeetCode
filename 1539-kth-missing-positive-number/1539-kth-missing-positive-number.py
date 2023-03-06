b91fb9301ea82e0e12de6c0969306acf8338d808
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-06 20:40:08 +0530

    Time: 59 ms (43.62%), Space: 14.1 MB (44.89%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/kth-missing-positive-number/
+# https://youtu.be/88k8xa-pSrM
+
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
         l, r = 0, len(arr)-1
@@ -9,3 +12,7 @@ class Solution:
                 r = mid - 1
         
         return l + k
+    
+    
+# Time: O(log(n))
+# Space: O(1)
