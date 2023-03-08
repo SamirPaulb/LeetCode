9ab0f951bdadae8bd8b48de1417bf365d143d137
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-09 01:53:55 +0530

    Time: 73 ms (90.03%), Space: 14.2 MB (79.75%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,11 +1,22 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         LIS = [nums[0]]
+        
+        def bis_left(target):
+            l, r = 0, len(LIS)-1
+            while l <= r:
+                mid = l + (r-l)//2
+                if LIS[mid] >= target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return l
+        
         for i in range(1, len(nums)):
             if nums[i] > LIS[-1]:
                 LIS.append(nums[i])
             else:
-                idx = bisect.bisect_left(LIS, nums[i])
+                idx = bis_left(nums[i])
                 LIS[idx] = nums[i]
         
         return len(LIS)
