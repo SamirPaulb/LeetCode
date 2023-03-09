6a99c377d050234f644e0d0e2d6fafbe8cda93b6
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-10 02:40:08 +0530

    Added solution - LeetHub

---
+++ Maximum sum Rectangle - GFG/maximum-sum-rectangle.py
@@ -0,0 +1,46 @@
+#User function Template for python3
+
+
+class Solution:
+    def maximumSumRectangle(self,R,C,M):
+        #code here
+        def kadanes(arr):
+            cs = 0; ms = arr[0]
+            for a in arr:
+                cs = max(a, cs + a)
+                ms = max(ms, cs)
+            return ms
+        
+        res = kadanes(M[0])
+        for i in range(R):
+            arr = [0]*C
+            for r in range(i, R):
+                for c in range(C):
+                    arr[c] += M[r][c]
+                res = max(res, kadanes(arr))
+        
+        return res
+        
+        
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+import sys
+
+if __name__=='__main__':
+    t=int(sys.stdin.readline().strip())
+    for _ in range(t):
+        N,M=map(int,sys.stdin.readline().strip().split())
+        a=[]
+        for i in range(N):
+            s=list(map(int,sys.stdin.readline().strip().split()))
+            a.append(s)
+        ob=Solution()
+        print(ob.maximumSumRectangle(N,M,a))
+# } Driver Code Ends
