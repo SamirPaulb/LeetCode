52a109c17e50ab8e4b5114084f4903d782ff7e9f
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-14 01:28:50 +0530

    Time: 31 ms (71.96%), Space: 13.9 MB (25.30%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,17 +1,25 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         n = len(s)
-        dp = [[""] for _ in range(n+1)]
-        for i in range(1, n+1):
-            arr = []
+        dp = [[] for _ in range(n)]
+        # dp = {i : [] for i in range(len(s))}
+        for i in range(n-1, -1, -1):
             for word in wordDict:
-                if i - len(word) >= 0 and s[i-len(word):i] == word:
-                    if i-len(word) == 0:
-                        arr.append(word)
-                    elif dp[i-len(word)] != [""]:
-                        for st in dp[i-len(word)]:
-                            arr.append(st + " " + word)
-            dp[i] = arr
-            
-        # print(dp)
-        return dp[-1]
+                if i + len(word) <= len(s) and s[i:i+len(word)] == word:
+                    dp[i].append(word)
+        
+        res = []
+        if not dp[0]: return res
+        
+        def solve(i, path):
+            if i >= len(s): 
+                res.append(path)
+                return
+            for st in dp[i]:
+                if not path:
+                    solve(i+len(st), st)
+                else:
+                    solve(i+len(st), path + " " + st)
+        
+        solve(0, "")
+        return res
