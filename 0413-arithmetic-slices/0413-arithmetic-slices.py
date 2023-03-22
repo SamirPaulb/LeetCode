ac21ba626faae2156516eac8d61b73196d97c832
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-22 19:52:11 +0530

    Time: 42 ms (48.18%), Space: 14.3 MB (9.50%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.py
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+    def numberOfArithmeticSlices(self, nums):
         n = len(nums)
         dp = [0] * n
         for i in range(2, n):
@@ -7,3 +7,23 @@ class Solution:
                 dp[i] = dp[i-1] + 1
         
         return sum(dp)
+    
+# Time: O(n)
+# Space: O(n)
+    
+    
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums):
+        dp = 0; n = len(nums); res = 0
+        for i in range(2, n):
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                dp += 1
+                res += dp
+            else:
+                dp = 0
+                
+        return res
+    
+# Time: O(n)
+# Space: O(1)
