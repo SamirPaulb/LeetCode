17adc560037ce3903057b5f50778096f075372ec
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-30 03:23:08 +0530

    Time: 44 ms (64.17%), Space: 13.9 MB (77.26%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,13 +1,8 @@
 class Solution:
-    def maxSatisfaction(self, sati: List[int]) -> int:
-        sati.sort()
-        dp = {}
-        # [-9, -8, -1, 0, 5]
-        def dfs(i, j):
-            if i >= len(sati): return 0
-            if (i,j) in dp: return dp[(i,j)]
-            ans = max(dfs(i+1, j), j * sati[i] + dfs(i+1, j+1))
-            dp[(i,j)] = ans
-            return ans
-        
-        return dfs(0, 1)
+    def maxSatisfaction(self, A: List[int]) -> int:
+        A.sort()
+        res = total = 0
+        while A and A[-1] + total >= 0:
+            total += A.pop()
+            res += total
+        return res
