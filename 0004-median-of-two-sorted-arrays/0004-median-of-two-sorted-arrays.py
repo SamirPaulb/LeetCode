97beeecb4d3998f2038bf26bde89242dc87d7895
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-06-15 21:48:12 +0530

    Time: 103 ms (65.94%), Space: 16.7 MB (7.92%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,26 +1,23 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        n1, n2 = len(nums1), len(nums2)
-        mid = (n1 + n2) // 2
-        prev = cur = 0
-        i = j = 0
-        k = 0
-        while k <= mid:
-            if i < n1 and j < n2:
-                if nums1[i] <= nums2[j]:
-                    val = nums1[i]
-                    i += 1
+        n1,n2 = len(nums1), len(nums2)
+        if n1 > n2: return self.findMedianSortedArrays(nums2, nums1)
+        low, high = 0, n1
+        while low <= high:
+            cut1 = low + (high-low)//2
+            cut2 = (n1 + n2 + 1) // 2 - cut1
+            left1 = nums1[cut1-1] if cut1 > 0 else -2**31
+            right1 = nums1[cut1] if cut1 < n1 else 2**31
+            left2 = nums2[cut2-1] if cut2 > 0 else -2**31
+            right2 = nums2[cut2] if cut2 < n2 else 2**31
+            if left1 <= right2 and left2 <= right1:
+                if (n1 + n2)%2 == 0:
+                    return (max(left1, left2) + min(right1, right2)) / 2
                 else:
-                    val = nums2[j]
-                    j += 1
-            elif j < n2:
-                val = nums2[j]
-                j += 1
+                    return max(left1, left2)
+            elif left1 > right2:
+                high = cut1 - 1
             else:
-                val = nums1[i]
-                i += 1
-            k += 1
-            prev = cur
-            cur = val
-        
-        return cur if (n1+n2)%2 else (prev+cur)/2
+                low = cut1 + 1
+                
+        return 0
