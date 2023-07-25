49d705ba7aa073be24673853499f0c6e71803fe0
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-07-25 18:11:02 +0530

    Time: 151 ms (16.64%), Space: 18 MB (79.39%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        negCount = 0
+        for i in range(32):
+            s = 0
+            for num in nums:
+                if num < 0: 
+                    negCount += 1
+                if (abs(num) >> i) & 1:
+                    s += 1
+                    s %= 3
+            res |= (s << i)
+
+        return -res if negCount%3 else res
