25b5c1ee9d917621b7488f7955d5b6330acbdbbc
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-07-25 08:07:21 +0530

    Added solution - LeetHub

---
+++ Column name from a given column number - GFG/column-name-from-a-given-column-number.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    def colName (self, n):
+        # your code here
+        res = ''
+        while n > 0:
+            r = n%26
+            if r == 0: 
+                res += 'Z'
+                n = (n-1)//26
+            else:
+                res += chr(64 + r)
+                n = n//26
+        
+        return res[::-1]
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+t = int (input ())
+for tc in range (t):
+    n = int (input ())
+    ob = Solution()
+    print (ob.colName (n))
+    
+
+# } Driver Code Ends
