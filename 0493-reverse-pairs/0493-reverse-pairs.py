28df9761897569442d129cb5d660411bdfd51008
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-07-28 09:32:35 +0530

    Time: 1697 ms (64.50%), Space: 23.3 MB (97.40%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,39 +1,39 @@
 class Solution:
-    def reversePairs(self, nums: List[int]) -> int:
+    def __init__(self):
         self.res = 0
-        def mergesort(nums):
-            if not nums or len(nums) == 1: return nums
-            m = len(nums)//2
-            left = mergesort(nums[:m])
-            right = mergesort(nums[m:])
-            
+    
+    def mergeSort(self, nums):
+        if len(nums) > 1:
+            mid = len(nums)//2
+            left = nums[:mid]
+            right = nums[mid:]
+            self.mergeSort(left)
+            self.mergeSort(right)
             j = 0
             for i in range(len(left)):
                 while j < len(right) and left[i] > 2*right[j]:
                     j += 1
                 self.res += j
-            
-            i, j, k = 0, 0, 0
+            i = j = k = 0
             while i < len(left) and j < len(right):
                 if left[i] <= right[j]:
                     nums[k] = left[i]
                     i += 1
+                    k += 1
                 else:
                     nums[k] = right[j]
                     j += 1
-                k += 1
-            
+                    k += 1
             while i < len(left):
                 nums[k] = left[i]
                 i += 1
                 k += 1
-            
             while j < len(right):
                 nums[k] = right[j]
                 j += 1
-                k += 1
-                
-            return nums
-        
-        mergesort(nums)
+                k += 1            
+    
+    def reversePairs(self, nums: List[int]) -> int:
+        self.mergeSort(nums)
         return self.res
+        
