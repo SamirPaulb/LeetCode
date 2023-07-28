b1745046103061ac77502b7d8f24811e8b83a38d
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-07-28 20:07:29 +0530

    Time: 102 ms (97.97%), Space: 21.2 MB (57.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_cnt = {}
+        num_cnt = collections.Counter()
         for num in nums:
-            if num not in num_cnt: num_cnt[num] = 1
-            else: num_cnt[num] += 1
+            num_cnt[num] += 1
         
         cnt_num = {}
         for num in num_cnt:
             cnt = num_cnt[num]
-            if cnt not in cnt_num: cnt_num[cnt] = [num]
-            else: cnt_num[cnt].append(num)
+            if cnt not in cnt_num: 
+                cnt_num[cnt] = [num]
+            else: 
+                cnt_num[cnt].append(num)
         
         res = []
         n = max(num_cnt.values())
