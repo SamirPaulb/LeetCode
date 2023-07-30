e5d68174e6f29de8ef082158796139b71ab65bd4
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-07-31 01:43:38 +0530

    Time: 60 ms (100%), Space: 17.4 MB (75.21%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def calculateMinimumHP(self, dungeon):
+        m, n = len(dungeon), len(dungeon[0])
+        dp = [[float("inf")]*(n+1) for _ in range(m+1)]
+        dp[m-1][n], dp[m][n-1] = 1, 1
+            
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                dp[i][j] = max(min(dp[i+1][j],dp[i][j+1])-dungeon[i][j],1)
+        
+        return dp[0][0]
