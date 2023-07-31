1ced82087fac1bd5e6475c5adbcbd01438643cda
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-07-31 16:36:48 +0530

    Time: 183 ms (30.1%), Space: 32.5 MB (5.1%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,16 +1,34 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.val = 0
+
 class Solution:
+    def __init__(self):
+        self.root = Node()
+    
+    def addNode(self, s):
+        cur = self.root
+        for c in s:
+            if c not in cur.children:
+                cur.children[c] = Node()
+            cur = cur.children[c]
+        cur.val = int(s)
+
     def lexicalOrder(self, n: int) -> List[int]:
+        for i in range(1, n+1):
+            self.addNode(str(i))
         res = []
-        def dfs(start):
-            arr = []
-            for i in range(10):
-                newStart = start*10 + i
-                if newStart > n: break
-                res.append(newStart)
-                dfs(newStart)
-        if n < 10: return range(1, n+1)
-        for i in range(1, 10):
-            res.append(i)
-            dfs(i)
-        
+        def dfs(root):
+            for c in root.children:
+                node = root.children[c]
+                res.append(node.val)
+                dfs(node)
+        dfs(self.root)
         return res
+
+
+
+
+
+
