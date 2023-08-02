fe08f2b47f7e8a320e263860ce175e7b2faa7061
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-08-02 12:18:03 +0530

    Time: 1220 ms (5.02%), Space: 17.1 MB (94.71%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        tcnt = collections.Counter(t)
+        scnt = collections.Counter()
+        stMatch = collections.Counter()
+        print(tcnt)
+        res = ""
+        l = 0
+        res = ""
+        maxLen = len(s)
+        for r in range(len(s)):
+            if s[r] in tcnt:
+                scnt[s[r]] += 1
+                if scnt[s[r]] >= tcnt[s[r]]:
+                    stMatch[s[r]] = tcnt[s[r]]
+            # print(stMatch)
+            while stMatch == tcnt and l <= r:
+                if r-l+1 <= maxLen:
+                    maxLen = r-l+1
+                    res = s[l:r+1]
+                if s[l] in tcnt: 
+                    scnt[s[l]] -= 1
+                    if scnt[s[l]] < tcnt[s[l]]:
+                        stMatch[s[l]] = scnt[s[l]]
+                l += 1
+            # print(l,r,scnt)
+        return res
