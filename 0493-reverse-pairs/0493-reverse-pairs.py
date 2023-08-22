941c0c8d9fd3f7d09274fa8b48225912f84d7b5a
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-08-22 21:59:28 +0530

    Time: 1771 ms (60.54%), Space: 22.8 MB (99.89%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,39 +1,42 @@
 class Solution:
     def __init__(self):
-        self.res = 0
-    
-    def mergeSort(self, nums):
-        if len(nums) > 1:
-            mid = len(nums)//2
-            left = self.mergeSort(nums[:mid])
-            right = self.mergeSort(nums[mid:])
+        self.count = 0
+
+    def mergeSort(self, arr):
+
+        if len(arr) > 1:
+            mid = len(arr)//2
+            left = self.mergeSort(arr[:mid])
+            right = self.mergeSort(arr[mid:])
+            
             j = 0
             for i in range(len(left)):
                 while j < len(right) and left[i] > 2*right[j]:
                     j += 1
-                self.res += j
+                self.count += j
+            
             i = j = k = 0
             while i < len(left) and j < len(right):
                 if left[i] <= right[j]:
-                    nums[k] = left[i]
+                    arr[k] = left[i]
                     i += 1
-                    k += 1
                 else:
-                    nums[k] = right[j]
+                    arr[k] = right[j]
                     j += 1
-                    k += 1
+                k += 1
+
             while i < len(left):
-                nums[k] = left[i]
+                arr[k] = left[i]
                 i += 1
                 k += 1
+
             while j < len(right):
-                nums[k] = right[j]
+                arr[k] = right[j]
                 j += 1
                 k += 1
-        return nums
-            
-    
+
+        return arr
+
     def reversePairs(self, nums: List[int]) -> int:
         self.mergeSort(nums)
-        return self.res
-        
+        return self.count
