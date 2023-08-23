06f5d26d309537dbaddcf5d344e7b26854520deb
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-08-23 22:54:51 +0530

    Time: 68 ms (70.57%), Space: 16.5 MB (27.15%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,37 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        n1 = self.getLen(l1); n2 = self.getLen(l2)
-        if n1 >= n2:
-            return self.solve(l1, l2)
-        return self.solve(l2, l1)
-    
-    
-    
-    
-    def solve(self, l1, l2):
-        head = prev = l1
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        res = ListNode(-1)
+        cur = res
         carry = 0
-        while l1:
-            v = l1.val + carry
-            v += l2.val if l2 else 0
-            carry = v // 10
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            v = v1 + v2 + carry
+            carry = v//10
             v = v % 10
-            l1.val = v
-            prev = l1
-            l1 = l1.next
+            cur.next = ListNode(v)
+            cur = cur.next
+            if l1: l1 = l1.next
             if l2: l2 = l2.next
-        if carry:
-            prev.next = ListNode(carry)
-        return head
         
-        
-    def getLen(self, head):
-        n = 0
-        while head:
-            n += 1
-            head = head.next
-        return n
+        return res.next
