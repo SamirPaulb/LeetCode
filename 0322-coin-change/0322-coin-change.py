01c9eacbc01b7e638df6d788b933ccff831da500
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-08-27 02:22:57 +0530

    Time: 1206 ms (51.13%), Space: 18.5 MB (27.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
-        def solve(i, amount):
-            if amount == 0: return 0
-            if amount < 0: return 2**31
-            if i >= len(coins): return 2**31
-            if (i, amount) in memo: return memo[(i, amount)]
-            ans = min(1 + min(solve(i, amount - coins[i]), solve(i+1, amount - coins[i])), solve(i+1, amount))
-            memo[(i, amount)] = ans
-            return ans
+        dp = [[2**31]*(amount+1) for i in range(len(coins)+1)]
+
+        for i in range(len(coins)+1):
+            dp[i][0] = 0
         
-        res = solve(0, amount)
-        return res if res < 2**31 else -1
+        for j in range(1, amount+1):
+            if j % coins[0] == 0:
+                dp[1][j] = j//coins[0]
+        
+        for i in range(2, len(coins)+1):
+            for j in range(1, amount+1):
+                if coins[i-1] <= amount:
+                    dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j])
+                else:
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[-1][-1] if dp[-1][-1] < 2**31 else -1
