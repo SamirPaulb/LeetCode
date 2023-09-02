c64b374b724f9b0809fc2d86d621331d872a2241
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-09-03 01:17:39 +0530

    Time: 127 ms (51.27%), Space: 16.9 MB (65.72%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,12 +1,30 @@
+class trieNode:
+    def __init__(self):
+        self.val = ""
+        self.children = {}
+        self.endWord = False
+
 class Solution:
+    def __init__(self):
+        self.root = trieNode()
+        self.root.endWord = True
+        self.res = ""
+    
+    def addWord(self, word):
+        cur = self.root
+        pre = cur.endWord
+        for c in word:
+            if c not in cur.children:
+                if not pre: break
+                cur.children[c] = trieNode()
+            pre = cur.endWord
+            cur = cur.children[c]
+        cur.endWord = True
+        if pre and len(word) > len(self.res):
+            self.res = word
+
     def longestWord(self, words: List[str]) -> str:
         words.sort()
-        visited = set()
-        visited.add("")
-        res = ""
         for word in words:
-            if word[:-1] in visited:
-                if len(word) > len(res):
-                    res = word
-                visited.add(word)
-        return res
+            self.addWord(word)
+        return self.res
