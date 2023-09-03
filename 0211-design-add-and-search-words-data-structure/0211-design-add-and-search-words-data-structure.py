735355c1fa8ae95be741ed6e880ba278eb45f549
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-09-03 15:19:22 +0530

    Time: 4863 ms (6.19%), Space: 80.4 MB (55.54%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,35 +1,30 @@
-class TrieNode:
+class trieNode:
     def __init__(self):
         self.children = {}
-        self.isWord = False
-
+        self.endWord = False
 
 class WordDictionary:
 
     def __init__(self):
-        self.root = TrieNode()
+        self.root = trieNode()
 
     def addWord(self, word: str) -> None:
         cur = self.root
         for c in word:
             if c not in cur.children:
-                cur.children[c] = TrieNode()
+                cur.children[c] = trieNode()
             cur = cur.children[c]
-        cur.isWord = True
+        cur.endWord = True
 
     def search(self, word: str) -> bool:
-        self.res = False
-        def dfs(cur, word):
-            if not word:
-                return True if cur.isWord else False
-            elif word[0] in cur.children:
-                cur = cur.children[word[0]]
-                return dfs(cur, word[1:])
-            elif word[0] == '.':
-                tmp = False
+        q = collections.deque()
+        q.append(self.root)
+        for i in range(len(word)):
+            n = len(q)
+            for _ in range(n):
+                cur = q.popleft()
                 for c in cur.children:
-                    tmp |= dfs(cur.children[c], word[1:])
-                return tmp
-            return False
-        
-        return dfs(self.root, word)
+                    if word[i] == '.' or c == word[i]:
+                        q.append(cur.children[c])
+                        if i == len(word)-1 and cur.children[c].endWord: return True
+        return False
