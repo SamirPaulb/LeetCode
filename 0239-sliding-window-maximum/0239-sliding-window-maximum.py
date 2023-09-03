65744610dc261e5b97d840a15ceeca69032a2ec4
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-09-03 20:14:04 +0530

    Time: 1298 ms (84.55%), Space: 33.2 MB (47.96%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,33 +1,14 @@
-### Solution 1 # Using Queue
 class Solution:
-    def maxSlidingWindow(self, nums, k):
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
         q = collections.deque()
-        res = []
-        
-        for i, ch in enumerate(nums):
-            while q and nums[q[-1]] <= ch:
+        for i, num in enumerate(nums):
+            while q and nums[q[-1]] <= num:
                 q.pop()
-            if q and i - q[0] >= k:
-                q.popleft()
             q.append(i)
-            if i >= k-1: res.append(nums[q[0]])
-        
-        return res
-    
-    
-### Solution 2 # Using MaxHeap
-import heapq
-class Solution:
-    def maxSlidingWindow(self, nums, k):
-        maxHeap = []
-        res = []
-        l = r = 0
-        for r, ch in enumerate(nums):
-            heapq.heappush(maxHeap, (-ch, r))
-            if r-l+1 > k: l += 1
-            while maxHeap and maxHeap[0][1] < l:
-                heapq.heappop(maxHeap)
-            if r >= k-1 and maxHeap:
-                res.append(-maxHeap[0][0])
-        
+            if i-q[0] >= k:
+                q.popleft()
+            if i >= k-1:
+                res.append(nums[q[0]])
+
         return res
