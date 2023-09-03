93fff1a9657f5469e2aa72ac0cbb16f8f453f55b
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-09-03 22:28:32 +0530

    Updated Solution - GfG to GitHub

--- Easy/Sort a stack/sort-a-stack.py
+++ Easy/Sort a stack/sort-a-stack.py
@@ -3,45 +3,52 @@ class Solution:
     # function sort the stack such that top element is max
     # funciton should return nothing
     # s is a stack
-    def sorted(self, s):
-        if not s: return []
-        top = s.pop()
-        self.sorted(s)
-        self.insert(s, top)
+    def Sorted(self, s):
+        # Code here
+        if not s: return 
+        tmp = s.pop()
+        self.Sorted(s)  
+        self.helper(s, tmp)
         return s
     
-    def insert(self, s, top):
-        if not s: 
-            s.append(top)
-            return
-        elif top >= s[-1]:
-            s.append(top)
-            return
+    def helper(self, stack, tmp):
+        if stack and stack[-1] > tmp:
+            top = stack.pop()
+            self.helper(stack, tmp)
+            stack.append(top)
         else:
-            tmp = s.pop()
-            self.insert(s, top)
-            s.append(tmp)
-            return
-            
-            
-            
+            stack.append(tmp)
+        return
         
         
         
         
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
 
 
 #{ 
  # Driver Code Starts
+
 if __name__=='__main__':
     t = int(input())
     for i in range(t):
         n = int(input())
         arr = list(map(int, input().strip().split()))
         ob = Solution()
-        ob.sorted(arr)
+        ob.Sorted(arr)
         for e in range(len(arr)):
             print(arr.pop(), end=" ")
         print()
