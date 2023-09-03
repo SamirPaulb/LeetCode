6fb520d393aa114b2f1da083e673b72fa9766d3b
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-09-03 19:40:58 +0530

    Time: 66 ms (56.05%), Space: 20.9 MB (8.36%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(val, self.stack[-1][1])))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+        
